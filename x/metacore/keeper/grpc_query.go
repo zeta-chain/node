31cb5ee74dd6aefa1a6570c5fcaabc82fcb7cd7c
Developer Experience team at Tendermint <hello@tendermint.com>
2021-09-24 02:09:52 -0500

    Initialized with Starport

---
+++ x/metacore/keeper/grpc_query.go
@@ -0,0 +1,7 @@
+package keeper
+
+import (
+	"github.com/Meta-Protocol/metacore/x/metacore/types"
+)
+
+var _ types.QueryServer = Keeper{}
---
+++ x/metacore/keeper/msg_server_test.go
@@ -0,0 +1,14 @@
+package keeper
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Meta-Protocol/metacore/x/metacore/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
+	keeper, ctx := setupKeeper(t)
+	return NewMsgServerImpl(*keeper), sdk.WrapSDKContext(ctx)
+}
