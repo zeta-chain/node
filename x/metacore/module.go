34799ca944ba2465dd7c7b5192085206a3f5ca51
brewmaster012 <panruo@metaprotocol.xyz>
2021-09-30 22:38:21 -0500

    fix lint error check

--- x/metacore/module.go
+++ x/metacore/module.go
@@ -79,7 +79,8 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
-	types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
+	err := types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
+	fmt.Println("RegisterQueryHandlerClient err: %w", err)
 }
 
 // GetTxCmd returns the capability module's root tx command.
