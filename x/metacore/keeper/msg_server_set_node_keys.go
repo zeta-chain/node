b5f374e8e710aed090f56232a6b9fc7c4d3fab6e
brewmaster012 <panruo@metaprotocol.xyz>
2021-10-04 00:04:26 -0500

    setNodeKeys: create NodeAccount
    In the future, bond needs to be checked.

--- x/metacore/keeper/msg_server_set_node_keys.go
+++ x/metacore/keeper/msg_server_set_node_keys.go
@@ -2,6 +2,8 @@ package keeper
 
 import (
 	"context"
+	"fmt"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	"github.com/Meta-Protocol/metacore/x/metacore/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -9,9 +11,24 @@ import (
 
 func (k msgServer) SetNodeKeys(goCtx context.Context, msg *types.MsgSetNodeKeys) (*types.MsgSetNodeKeysResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	addr, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, fmt.Sprintf("msg creator %s not valid", msg.Creator))
+	}
+	na, found := k.GetNodeAccount(ctx, msg.Creator)
+	if !found {
+		na = types.NodeAccount{
+			Creator:     msg.Creator,
+			Index:       msg.Creator,
+			NodeAddress: addr,
+			PubkeySet:   msg.PubkeySet,
+			NodeStatus:  types.NodeStatus_Unknown,
+		}
+	} else {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, fmt.Sprintf("msg creator %s already has a node account", msg.Creator))
+	}
 
-	// TODO: Handling the message
-	_ = ctx
+	k.SetNodeAccount(ctx, na)
 
 	return &types.MsgSetNodeKeysResponse{}, nil
 }
