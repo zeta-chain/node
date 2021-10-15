e2fe5b894bf89a33e7515426454c77de642abf5e
brewmaster012 <panruo@metaprotocol.xyz>
2021-10-15 03:58:28 -0500

    SendVoter logic

--- x/metacore/keeper/msg_server_send_voter.go
+++ x/metacore/keeper/msg_server_send_voter.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"context"
 	"fmt"
+	"github.com/ethereum/go-ethereum/crypto"
 
 	"github.com/Meta-Protocol/metacore/x/metacore/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -35,5 +36,38 @@ func (k msgServer) CreateSendVoter(goCtx context.Context, msg *types.MsgCreateSe
 		ctx,
 		sendVoter,
 	)
+
+	sendVoter.Index = ""
+	sendVoter.Creator = ""
+	hashSend := crypto.Keccak256Hash([]byte(sendVoter.String()))
+
+	send, isFound := k.GetSend(ctx, hashSend.Hex());
+	if isFound {
+		for _, s := range send.Signers {
+			if s == msg.Creator {
+				return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, fmt.Sprintf("send index %s already set from signer %s", msg.TxHash, msg.Creator))
+			}
+		}
+		send.Signers = append(send.Signers, msg.Creator)
+	} else {
+		send = types.Send{
+			Index:           msg.Index,
+			Creator:         msg.Creator,
+			Sender:          msg.Sender,
+			SenderChainId:   msg.SenderChainId,
+			Receiver:        msg.Receiver,
+			ReceiverChainId: msg.ReceiverChainId,
+			MBurnt:          msg.MBurnt,
+			Message:         msg.Message,
+			TxHash:          msg.TxHash,
+			Signers: 		 []string{msg.Creator},
+			FinalizedHeight: 0,
+		}
+	}
+
+	if len(send.Signers) == 2 {
+		send.FinalizedHeight = uint64(ctx.BlockHeader().Height)
+		//TODO: generate RECV item.
+	}
 	return &types.MsgCreateSendVoterResponse{}, nil
 }
