2365faa39eb25212f2f734b688b36d7cef95c840
brewmaster012 <panruo@metaprotocol.xyz>
2021-10-22 14:44:01 -0500

    receive type: for outbound consumption and also voter
    MetaClient watch receive type for outbound tx;
    once the outbound tx is confirmed, it sends
    receiveConfirm msg (to be introduced in later commit)
    Once enough voters votes on receiveConfirm, it's
    considered confirmed.
    
    MetaClient needs to make sure not to double sign
    and not to drop and "receive" outbound.

---
+++ x/metacore/client/cli/query_receive.go
@@ -0,0 +1,71 @@
+package cli
+
+import (
+    "context"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+    "github.com/Meta-Protocol/metacore/x/metacore/types"
+)
+
+func CmdListReceive() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list-receive",
+		Short: "list all receive",
+		RunE: func(cmd *cobra.Command, args []string) error {
+            clientCtx := client.GetClientContextFromCmd(cmd)
+
+            pageReq, err := client.ReadPageRequest(cmd.Flags())
+            if err != nil {
+                return err
+            }
+
+            queryClient := types.NewQueryClient(clientCtx)
+
+            params := &types.QueryAllReceiveRequest{
+                Pagination: pageReq,
+            }
+
+            res, err := queryClient.ReceiveAll(context.Background(), params)
+            if err != nil {
+                return err
+            }
+
+            return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddPaginationFlagsToCmd(cmd, cmd.Use)
+	flags.AddQueryFlagsToCmd(cmd)
+
+    return cmd
+}
+
+func CmdShowReceive() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "show-receive [index]",
+		Short: "shows a receive",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+            clientCtx := client.GetClientContextFromCmd(cmd)
+
+            queryClient := types.NewQueryClient(clientCtx)
+
+            params := &types.QueryGetReceiveRequest{
+                Index: args[0],
+            }
+
+            res, err := queryClient.Receive(context.Background(), params)
+            if err != nil {
+                return err
+            }
+
+            return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+    return cmd
+}
