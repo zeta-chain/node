2edcd8c1fd50a09c2e991b206320ffd182752529
brewmaster012 <panruo@metaprotocol.xyz>
2021-10-22 04:44:18 -0500

    remove txin/txout voter logic, in favor of the new one

--- cmd/metaclientd/voter_test.go
+++ cmd/metaclientd/voter_test.go
@@ -74,48 +74,6 @@ func (s *VoterSuite) SetUpTest(c *C) {
 	}
 }
 
-func (s *VoterSuite) TestObservedTxIn(c *C) {
-	b1 := s.bridge1
-	b2 := s.bridge2
-	//err := b.PostTxIn("ETH.ETH", 2, 4, "ETH.BSC", "0xdeadbeef", "0x1234", 2345)
-	metaHash, err := b1.PostTxIn("0xfrom", "0xto", "0xsource.ETH", 123456, 23245, "0xdest.BSC",
-		"0xtxhash", 123123)
-
-	c.Assert(err, IsNil)
-	log.Info().Msgf("PostTxIn metaHash %s", metaHash)
-
-	// wait for the next block
-	timer1 := time.NewTimer(2 * time.Second)
-	<-timer1.C
-
-	metaHash, err = b2.PostTxIn("0xfrom", "0xto", "0xsource.ETH", 123456, 23245, "0xdest.BSC",
-		"0xtxhash", 123123)
-	c.Assert(err, IsNil)
-	log.Info().Msgf("Second PostTxIn metaHash %s", metaHash)
-
-	// wait for the next block
-	timer2 := time.NewTimer(2 * time.Second)
-	<-timer2.C
-
-	txouts, err := b1.GetAllTxout()
-	c.Assert(err, IsNil)
-	log.Info().Msgf("txouts: %v", txouts)
-	c.Assert(len(txouts) >= 1, Equals, true)
-
-	txout := txouts[0]
-	tid := txout.Id
-	metaHash, err = b1.PostTxoutConfirmation(tid, "0xhashtxout", 1337, "0xnicetoken", 1773, "0xmywallet", 12345)
-	timer3 := time.NewTimer(2 * time.Second)
-	<-timer3.C
-	metaHash, err = b2.PostTxoutConfirmation(tid, "0xhashtxout", 1337, "0xnicetoken", 1773, "0xmywallet", 12345)
-
-	timer4 := time.NewTimer(2 * time.Second)
-	<-timer4.C
-
-	txouts, err = b1.GetAllTxout()
-	c.Assert(err, IsNil)
-	log.Info().Msgf("txouts: %v", txouts)
-}
 
 
 func (s *VoterSuite) TestSendVoter(c *C) {
