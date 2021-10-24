ab4074e63d27153e5a879f3117fe5586b40dbce2
brewmaster012 <panruo@metaprotocol.xyz>
2021-10-23 23:23:26 -0500

    added lastBlockHeight for tracking external chains
    the structure will keep track of the block heights
    that last reported to MetaCore by MetaClient

--- x/metacore/client/cli/query.go
+++ x/metacore/client/cli/query.go
@@ -26,6 +26,9 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 
 	// this line is used by starport scaffolding # 1
 
+	cmd.AddCommand(CmdListLastBlockHeight())
+	cmd.AddCommand(CmdShowLastBlockHeight())
+
 	cmd.AddCommand(CmdListReceive())
 	cmd.AddCommand(CmdShowReceive())
 
