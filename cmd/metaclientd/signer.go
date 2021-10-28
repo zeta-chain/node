08a823fd7138982241fc4f5138950283e04673c2
brewmaster012 <panruo@metaprotocol.xyz>
2021-10-27 22:02:13 -0500

    signer: add meta token contract call mint

--- cmd/metaclientd/signer.go
+++ cmd/metaclientd/signer.go
@@ -3,10 +3,12 @@ package metaclientd
 import (
 	"context"
 	"github.com/Meta-Protocol/metacore/common"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	ethcommon "github.com/ethereum/go-ethereum/common"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"math/big"
+	"strings"
 )
 
 type TSSSigner interface {
@@ -16,15 +18,17 @@ type TSSSigner interface {
 }
 
 type Signer struct {
-	client    *ethclient.Client
-	nonce     uint64
-	chain     common.Chain
-	chainID   *big.Int
-	tssSigner TSSSigner
-	ethSigner ethtypes.Signer
+	client              *ethclient.Client
+	nonce               uint64
+	chain               common.Chain
+	chainID             *big.Int
+	tssSigner           TSSSigner
+	ethSigner           ethtypes.Signer
+	abi                 abi.ABI
+	metaContractAddress ethcommon.Address
 }
 
-func NewSigner(chain common.Chain, endpoint string, tssAddress ethcommon.Address, tssSigner TSSSigner) (*Signer, error) {
+func NewSigner(chain common.Chain, endpoint string, tssAddress ethcommon.Address, tssSigner TSSSigner, abiString string, metaContract ethcommon.Address) (*Signer, error) {
 	client, err := ethclient.Dial(endpoint)
 	if err != nil {
 		return nil, err
@@ -38,20 +42,26 @@ func NewSigner(chain common.Chain, endpoint string, tssAddress ethcommon.Address
 		return nil, err
 	}
 	ethSigner := ethtypes.LatestSignerForChainID(chainID)
+	abi, err := abi.JSON(strings.NewReader(abiString))
+	if err != nil {
+		return nil, err
+	}
 
 	return &Signer{
-		client:    client,
-		nonce:     nonce,
-		chain:     chain,
-		tssSigner: tssSigner,
-		chainID:   chainID,
-		ethSigner: ethSigner,
+		client:              client,
+		nonce:               nonce,
+		chain:               chain,
+		tssSigner:           tssSigner,
+		chainID:             chainID,
+		ethSigner:           ethSigner,
+		abi:                 abi,
+		metaContractAddress: metaContract,
 	}, nil
 }
 
 // given data, and metadata (gas, nonce, etc)
 // returns a signed transaction, sig bytes, hash bytes, and error
-func (signer *Signer) Sign(data []byte, to ethcommon.Address, gasLimit uint64, gasPrice *big.Int) (*ethtypes.Transaction, []byte,[]byte,  error) {
+func (signer *Signer) Sign(data []byte, to ethcommon.Address, gasLimit uint64, gasPrice *big.Int) (*ethtypes.Transaction, []byte, []byte, error) {
 	tx := ethtypes.NewTransaction(signer.nonce, to, big.NewInt(0), gasLimit, gasPrice, data)
 	hashBytes := signer.ethSigner.Hash(tx).Bytes()
 	sig := signer.tssSigner.Sign(hashBytes)
@@ -68,3 +78,21 @@ func (signer *Signer) Broadcast(tx *ethtypes.Transaction) error {
 	return signer.client.SendTransaction(context.TODO(), tx)
 }
 
+// send outbound tx to smart contract
+func (signer *Signer) MMint(amount *big.Int, to ethcommon.Address, gasLimit uint64, message []byte) (string, error) {
+	data, err := signer.abi.Pack("mint", to, amount)
+	if err != nil {
+		return "", err
+	}
+	gasPrice, err := signer.client.SuggestGasPrice(context.Background())
+
+	tx, _, _, err := signer.Sign(data, signer.metaContractAddress, gasLimit, gasPrice)
+	if err != nil {
+		return "", err
+	}
+	err = signer.Broadcast(tx)
+	if err != nil {
+		return "", err
+	}
+	return tx.Hash().Hex(), nil
+}
