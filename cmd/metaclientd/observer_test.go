5d8239ba87583850648eb9792087b00643758c4f
Chase Zimmerman <chaseklvk@icloud.com>
2021-10-28 21:48:16 -0700

    streamline observer and core monitor

--- cmd/metaclientd/observer_test.go
+++ cmd/metaclientd/observer_test.go
@@ -1,11 +1,40 @@
 package metaclientd
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/rs/zerolog/log"
 )
 
 func TestWatchRouter(t *testing.T) {
+	t.Logf("Settting up test...")
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		t.Logf("UserHomeDir error")
+		t.Fail()
+	}
+	t.Logf("user home dir: %s", homeDir)
+	chainHomeFoler := filepath.Join(homeDir, ".metacore")
+	t.Logf("chain home dir: %s", chainHomeFoler)
+
+	signerName := "alice"
+	signerPass := "password"
+	kb, _, err := GetKeyringKeybase(chainHomeFoler, signerName, signerPass)
+	if err != nil {
+		log.Fatal().Err(err).Msg("fail to get keyring keybase")
+	}
+
+	k := NewKeysWithKeybase(kb, signerName, signerPass)
+
+	chainIP := "127.0.0.1"
+	bridge, err := NewMetachainBridge(k, chainIP, "alice")
+	if err != nil {
+		t.Fail()
+	}
+
 	EthObserver := &MetaObserver{}
-	EthObserver.InitMetaObserver("Ethereum")
-	EthObserver.WatchRouter()
+	EthObserver.InitMetaObserver("Ethereum", bridge)
+	//EthObserver.WatchRouter()
 }
