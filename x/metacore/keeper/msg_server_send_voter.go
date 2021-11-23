6eb6304161b26a4b1e8d0f57f2f450b77e2410e1
brewmaster012 <pwu@zetachain.com>
2021-11-22 23:18:43 -0600

    fix linter complaints

--- x/metacore/keeper/msg_server_send_voter.go
+++ x/metacore/keeper/msg_server_send_voter.go
@@ -16,7 +16,7 @@ func (k msgServer) SendVoter(goCtx context.Context, msg *types.MsgSendVoter) (*t
 
 	validators := k.StakingKeeper.GetAllValidators(ctx)
 	if !isBondedValidator(msg.Creator, validators) {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrorInvalidSigner, fmt.Sprintf("signer %s is not a bonded validator"))
+		return nil, sdkerrors.Wrap(sdkerrors.ErrorInvalidSigner, fmt.Sprintf("signer %s is not a bonded validator", msg.Creator))
 	}
 
 	index := msg.Digest()
