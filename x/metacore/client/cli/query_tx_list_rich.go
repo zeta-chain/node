98490d2d065fd80573702b22a104ec0d241cc400
brewmaster012 <pwu@zetachain.com>
2021-12-10 01:03:39 -0600

    created txListRich API for recent txs

---
+++ x/metacore/client/cli/query_tx_list_rich.go
@@ -0,0 +1,45 @@
+package cli
+
+import (
+	"github.com/spf13/cobra"
+	"strconv"
+
+	"github.com/Meta-Protocol/metacore/x/metacore/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+)
+
+var _ = strconv.Itoa(0)
+
+func CmdTxListRich() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "tx-list-rich [last]",
+		Short: "Query txListRich",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			reqLast := string(args[0])
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+			last, _ := strconv.ParseUint(reqLast, 10, 64)
+			params := &types.QueryTxListRichRequest{
+				Last: last,
+			}
+
+			res, err := queryClient.TxListRich(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
---
+++ x/metacore/keeper/grpc_query_tx_list_rich.go
@@ -0,0 +1,41 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/Meta-Protocol/metacore/x/metacore/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (k Keeper) TxListRich(goCtx context.Context, req *types.QueryTxListRichRequest) (*types.QueryTxListRichResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	val, found := k.GetTxList(ctx)
+	if !found {
+		return nil, status.Error(codes.InvalidArgument, "not found")
+	}
+	txlist := val.Tx
+	len := len(txlist)
+	from := len - int(req.Last)
+	if from < 0 {
+		from = 0
+	}
+
+	var sendlist []*types.Send
+	for i := from; i < len; i++ {
+		tx := txlist[i]
+		sendHash := tx.SendHash
+		send, found := k.GetSend(ctx, sendHash)
+		if found {
+			sendlist = append(sendlist, &send)
+		}
+	}
+
+	return &types.QueryTxListRichResponse{Tx: sendlist, Length: uint64(len)}, nil
+}
