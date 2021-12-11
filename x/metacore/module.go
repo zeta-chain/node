e0df4d2a1380f07ed925a7055e7a5dd547f18dc0
brewmaster012 <pwu@zetachain.com>
2021-12-11 02:31:17 -0600

    remove migration for restarting blockchain

--- x/metacore/module.go
+++ x/metacore/module.go
@@ -167,6 +167,5 @@ func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}
 // EndBlock executes all ABCI EndBlock logic respective to the capability module. It
 // returns no validator updates.
 func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
-	am.keeper.CleanupEthNonce44Mess(sdk.WrapSDKContext(ctx))
 	return []abci.ValidatorUpdate{}
 }
