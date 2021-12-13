841f0e8ec206ad32e1d1f207885e8a83c8d8e4ad
brewmaster012 <pwu@zetachain.com>
2021-12-13 00:37:43 -0600

    update reset-testnet.sh due to rename

--- build/reset-testnet.sh
+++ build/reset-testnet.sh
@@ -1,49 +1,50 @@
 set -e
 set -x
-METACORED=/home/ubuntu/go/bin/metacored
-NODES="10.0.0.22"
-rm -rf ~/.metacore
+ZETACORED=/home/ubuntu/go/bin/zetacored
+NODES="3.137.46.147"
+
+rm -rf ~/.zetacore
 for NODE in $NODES; do
-	ssh -i ~/.ssh/meta.pem $NODE rm -rf ~/.metacore
+	ssh -i ~/.ssh/meta.pem $NODE rm -rf ~/.zetacore
 done
 
-$METACORED init --chain-id testing zetachain
-$METACORED keys add val --keyring-backend=test
-$METACORED add-genesis-account $($METACORED keys show val -a --keyring-backend=test) 1000000000stake
+$ZETACORED init --chain-id testing zetachain
+$ZETACORED keys add val --keyring-backend=test
+$ZETACORED add-genesis-account $($ZETACORED keys show val -a --keyring-backend=test) 1000000000stake
 
 
 for NODE in $NODES; do
-	ssh -i ~/.ssh/meta.pem $NODE $METACORED keys add val --keyring-backend=test
-	ADDR=$(ssh -i ~/.ssh/meta.pem $NODE $METACORED keys show val -a --keyring-backend=test)
-	$METACORED add-genesis-account $ADDR 1000000000stake --keyring-backend=test
-done
-
-
-for NODE in $NODES; do
-	scp -i ~/.ssh/meta.pem ~/.metacore/config/genesis.json $NODE:~/.metacore/config/
+	ssh -i ~/.ssh/meta.pem $NODE $ZETACORED keys add val --keyring-backend=test
+	ADDR=$(ssh -i ~/.ssh/meta.pem $NODE $ZETACORED keys show val -a --keyring-backend=test)
+	$ZETACORED add-genesis-account $ADDR 1000000000stake --keyring-backend=test
 done
 
 
-$METACORED gentx val 1000000000stake --keyring-backend=test --chain-id=testing
 for NODE in $NODES; do
-    ssh -i ~/.ssh/meta.pem $NODE $METACORED gentx val 1000000000stake --keyring-backend=test --chain-id=testing
-    scp -i ~/.ssh/meta.pem $NODE:~/.metacore/config/gentx/*.json ~/.metacore/config/gentx/
-done
-
-$METACORED collect-gentxs
-
-
-for NODE in $NODES; do
-	scp -i ~/.ssh/meta.pem ~/.metacore/config/genesis.json $NODE:~/.metacore/config/
+	scp -i ~/.ssh/meta.pem ~/.zetacore/config/genesis.json $NODE:~/.zetacore/config/
 done
 
 
-jq '.chain_id = "testing"' ~/.metacore/config/genesis.json > temp.json && mv temp.json ~/.metacore/config/genesis.json
-sed -i '/\[api\]/,+3 s/enable = false/enable = true/' ~/.metacore/config/app.toml
-sed -i '/\[api\]/,+24 s/enabled-unsafe-cors = false/enabled-unsafe-cors = true/' ~/.metacore/config/app.toml
+$ZETACORED gentx val 1000000000stake --keyring-backend=test --chain-id=testing
 for NODE in $NODES; do
-    ssh -i ~/.ssh/meta.pem $NODE jq \'.chain_id = \"testing\"\' ~/.metacore/config/genesis.json > temp.json && mv temp.json ~/.metacore/config/genesis.json
-    ssh -i ~/.ssh/meta.pem $NODE sed -i \'/\[api\]/,+3 s/enable = false/enable = true/\' ~/.metacore/config/app.toml
-    ssh -i ~/.ssh/meta.pem $NODE sed -i \'/\[api\]/,+24 s/enabled-unsafe-cors = false/enabled-unsafe-cors = true/\' ~/.metacore/config/app.toml
+    ssh -i ~/.ssh/meta.pem $NODE $ZETACORED gentx val 1000000000stake --keyring-backend=test --chain-id=testing
+    scp -i ~/.ssh/meta.pem $NODE:~/.zetacore/config/gentx/*.json ~/.zetacore/config/gentx/
+done
+
+$ZETACORED collect-gentxs
+
+
+for NODE in $NODES; do
+	scp -i ~/.ssh/meta.pem ~/.zetacore/config/genesis.json $NODE:~/.zetacore/config/
+done
+
+
+jq '.chain_id = "testing"' ~/.zetacore/config/genesis.json > temp.json && mv temp.json ~/.zetacore/config/genesis.json
+sed -i '/\[api\]/,+3 s/enable = false/enable = true/' ~/.zetacore/config/app.toml
+sed -i '/\[api\]/,+24 s/enabled-unsafe-cors = false/enabled-unsafe-cors = true/' ~/.zetacore/config/app.toml
+for NODE in $NODES; do
+    ssh -i ~/.ssh/meta.pem $NODE jq \'.chain_id = \"testing\"\' ~/.zetacore/config/genesis.json > temp.json && mv temp.json ~/.zetacore/config/genesis.json
+    ssh -i ~/.ssh/meta.pem $NODE sed -i \'/\[api\]/,+3 s/enable = false/enable = true/\' ~/.zetacore/config/app.toml
+    ssh -i ~/.ssh/meta.pem $NODE sed -i \'/\[api\]/,+24 s/enabled-unsafe-cors = false/enabled-unsafe-cors = true/\' ~/.zetacore/config/app.toml
 done
 
