8343a50fc497625c018d6b50592c945d821b7042
brewmaster012 <pwu@zetachain.com>
2021-12-24 15:26:19 -0600

    broadcast: update seqNum based on error message

--- zetaclient/broadcast.go
+++ zetaclient/broadcast.go
@@ -11,6 +11,8 @@ import (
 	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
 	"github.com/zeta-chain/zetacore/app"
 	"github.com/zeta-chain/zetacore/cmd"
+	"regexp"
+	"strconv"
 	"strings"
 	"sync/atomic"
 )
@@ -66,19 +68,43 @@ func (b *MetachainBridge) Broadcast(msgs ...stypes.Msg) (string, error) {
 	commit, err := ctx.BroadcastTxSync(txBytes)
 	if err != nil {
 		b.logger.Error().Err(err).Msgf("fail to broadcast tx")
-		return "", err
+		err_msg := err.Error()
+		re := regexp.MustCompile(`account sequence mismatch, expected ([0-9]*), got ([0-9]*)`)
+		matches := re.FindStringSubmatch(err_msg)
+		if len(matches) != 3 {
+			return "", err
+		} else {
+			expected_seq, err := strconv.Atoi(matches[1])
+			if err != nil {
+				b.logger.Warn().Msgf("cannot parse expected seq %s", matches[1])
+				return "", err
+			}
+			got_seq, err := strconv.Atoi(matches[2])
+			if err != nil {
+				b.logger.Warn().Msgf("cannot parse got seq %s", matches[2])
+				return "", err
+			}
+			b.seqNumber = uint64(expected_seq)
+			b.logger.Warn().Msgf("Reset seq number to %d (from err msg) from %d", b.seqNumber, got_seq)
+			return "", fmt.Errorf("account seq number forced changed from %d to %d by error message", got_seq, b.seqNumber)
+		}
+
 	}
 	// Code will be the tendermint ABICode , it start at 1 , so if it is an error , code will not be zero
 	if commit.Code > 0 {
 		if commit.Code == 32 {
 			// bad sequence number, fetch new one
-			_, seqNum, _ := b.GetAccountNumberAndSequenceNumber()
-			if seqNum == b.seqNumber {
-				b.logger.Warn().Msgf("seq # %d is the most current that zetacore tells us, not sure why it's not accepting it; increment it and try later. ", seqNum)
-				//atomic.AddUint64(&b.seqNumber, 1)
-			} else {
-				b.seqNumber = seqNum
-			}
+			//_, seqNum, _ := b.GetAccountNumberAndSequenceNumber()
+			//if seqNum == b.seqNumber {
+			//	b.logger.Warn().Msgf("seq # %d is the most current that zetacore tells us, not sure why it's not accepting it; increment it and try later. ", seqNum)
+			//	//atomic.AddUint64(&b.seqNumber, 1)
+			//} else {
+			//	b.seqNumber = seqNum
+			//}
+
+			// The above logic does not work when the fecthed new one is stuck at out-of-date values. (why?)
+			// Here are directly parse the error message and get the expected seq num
+
 		}
 		b.logger.Info().Msgf("messages: %+v", msgs)
 		return commit.TxHash, fmt.Errorf("fail to broadcast to metachain,code:%d, log:%s", commit.Code, commit.RawLog)
---
+++ zetaclient/broadcast_test.go
@@ -0,0 +1,25 @@
+package zetaclient
+
+import (
+	"fmt"
+	. "gopkg.in/check.v1"
+	"regexp"
+)
+
+type BcastSuite struct {
+	bridge *MetachainBridge
+}
+
+var _ = Suite(&BcastSuite{})
+
+func (s *BcastSuite) SetUpTest(c *C) {
+	fmt.Println("hello")
+}
+
+func (s *BcastSuite) TestParsingSeqNumMismatch(c *C) {
+	err_msg := "fail to broadcast to metachain,code:32, log:account sequence mismatch, expected 386232, got 386230: incorrect account sequence"
+	re := regexp.MustCompile(`account sequence mismatch, expected ([0-9]*), got ([0-9]*)`)
+	fmt.Printf("%q\n", re.FindStringSubmatch(err_msg))
+	err_msg2 := "hahah"
+	fmt.Printf("%q\n", re.FindStringSubmatch(err_msg2))
+}
