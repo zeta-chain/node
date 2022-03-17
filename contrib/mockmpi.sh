ccabed9e6af263eb176c2b6ef0f0e65f4e806136
Charlie McCowan <charliemc@zetachain.com>
2022-03-17 12:32:38 -0600

    Minor changes to make mockmpi work

--- contrib/mockmpi.sh
+++ contrib/mockmpi.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
 
 if [[ -z "$PRIVKEY" ]]; then
-  echo "Must provide PRIVKEY in environment" 1>&2
-  exit 1
+  echo "PRIVKEY not provided. Using Test Key"
+  export PRIVKEY="2082bc9775d6ee5a05ef221a9d1c00b3cc3ecb274a4317acc0a182bc1e05d1bb"
 fi
 
 export BSC_ENDPOINT=wss://speedy-nodes-nyc.moralis.io/eb13a7dfda3e4b15212356f9/bsc/testnet/archive/ws
