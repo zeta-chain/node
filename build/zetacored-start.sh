ccb50eb0d108daff44653f5388f1cf655f419fac
Charlie McCowan <charliemc@zetachain.com>
2022-03-25 12:53:49 -0600

    Setting API to enabled on zetacore start

--- build/zetacored-start.sh
+++ build/zetacored-start.sh
@@ -23,6 +23,7 @@ else
     echo "This is Node $NODE_NUMBER"
     cp -rf /zetashared/node$NODE_NUMBER/* /root/.zetacore/
     cp -rf /zetashared/node$NODE_NUMBER/tssnew/* /root/.tssnew
+    sed -i '/\[api\]/,+3 s/enable = false/enable = true/' /root/.zetacore/config/app.toml
     zetacored start --rpc.laddr "tcp://0.0.0.0:26657" \
         --proxy_app "tcp://0.0.0.0:26658" \
         --rpc.pprof_laddr "0.0.0.0:6060" 2>&1 | tee /root/.zetacore/zetacored.log
