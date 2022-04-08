88a48cba0361d667a2dfb9c9be71c229900891b7
Charlie McCowan <mccowan.charlie@gmail.com>
2022-04-07 20:29:09 -0600

    Setup Node 0 TSS ID Dynamically

--- build/zetaclient-start.sh
+++ build/zetaclient-start.sh
@@ -18,21 +18,23 @@ NODE_0_ID=$(cat /zetashared/node0/config/NODE_VALIDATOR_ID)
 mkdir -p /root/.tssnew/
 
 sleep 5 # Waiting for Zetacored to boot
-    
-# FILE="/root/.tssnew/address_book.seed"
-# if [ ! -f "$FILE" ]; then
-#     echo "$FILE does not exist - Copying from /zetashared/node$NODE_NUMBER/"
-#     mkdir -p /root/.tssnew/
-#     # cp -rf /zetashared/node$NODE_NUMBER/tssnew/* /root/.tssnew/
-# fi
 
 if  (( $NODE_NUMBER == 0 )); then
     yes | zetaclientd -val val 2>&1 | tee ~/.zetaclient/zetaclient.log
 else
     export SEED=172.24.0.220
+        until [ -f NODE_0_TSS_ID ]
+        do
+            echo "Waiting for Node 0 Validator ID"
+            sleep 5
+            curl ${SEED}:8123/p2p -o NODE_0_TSS_ID
+        done
+    NODE_0_TSS_ID=$(cat NODE_0_TSS_ID)
+    echo "NODE_0_TSS_ID=${NODE_0_TSS_ID}"
     yes | zetaclientd -val val \
-        --peer /dns/${NODE_0_DNS}/tcp/6668/p2p/16Uiu2HAmACG5DtqmQsHtXg4G2sLS65ttv84e7MrL4kapkjfmhxAp \
+        --peer /dns/${NODE_0_DNS}/tcp/6668/p2p/${NODE_0_TSS_ID} \
         2>&1 | tee ~/.zetaclient/zetaclient.log
+
 fi
 
 
