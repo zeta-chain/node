878824e5d5dc351868343858826f8603b06e56ee
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-05-12 14:42:22 -0500

    misc: fix init bug

--- x/zetacore/types/errors.go
+++ x/zetacore/types/errors.go
@@ -14,5 +14,5 @@ var (
 	ErrFloatParseError    = sdkerrors.Register(ModuleName, 1101, "float parse error")
 	ErrUnsupportedChain   = sdkerrors.Register(ModuleName, 1102, "chain parse error")
 	ErrDuplicateMsg       = sdkerrors.Register(ModuleName, 1103, "duplicate msg error")
-	ErrNotBondedValidator = sdkerrors.Register(ModuleName, 1103, "not a bonded validator error")
+	ErrNotBondedValidator = sdkerrors.Register(ModuleName, 1104, "not a bonded validator error")
 )
