57e4bae3101007a8756212bbcf636f2178638b3a
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-05-13 16:10:23 -0500

    zetacore: enchance CLI receive confirmation of status & chain

--- x/zetacore/client/cli/tx_receive_confirmation.go
+++ x/zetacore/client/cli/tx_receive_confirmation.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/zeta-chain/zetacore/common"
 	"strconv"
@@ -15,9 +16,9 @@ var _ = strconv.Itoa(0)
 
 func CmdReceiveConfirmation() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "receive-confirmation [sendHash] [outTxHash] [outBlockHeight] [mMint]",
+		Use:   "receive-confirmation [sendHash] [outTxHash] [outBlockHeight] [mMint] [Status] [chain]",
 		Short: "Broadcast message receiveConfirmation",
-		Args:  cobra.ExactArgs(4),
+		Args:  cobra.ExactArgs(6),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			argsSendHash := (args[0])
 			argsOutTxHash := (args[1])
@@ -26,13 +27,22 @@ func CmdReceiveConfirmation() *cobra.Command {
 				return err
 			}
 			argsMMint := (args[3])
+			var status common.ReceiveStatus
+			if args[4] == "0" {
+				status = common.ReceiveStatus_Success
+			} else if args[5] == "1" {
+				status = common.ReceiveStatus_Failed
+			} else {
+				return fmt.Errorf("wrong status")
+			}
+			chain := args[5]
 
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
 
-			msg := types.NewMsgReceiveConfirmation(clientCtx.GetFromAddress().String(), (argsSendHash), (argsOutTxHash), uint64(argsOutBlockHeight), (argsMMint), common.ReceiveStatus_Success, "ETH")
+			msg := types.NewMsgReceiveConfirmation(clientCtx.GetFromAddress().String(), (argsSendHash), (argsOutTxHash), uint64(argsOutBlockHeight), (argsMMint), status, chain)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
