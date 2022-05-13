dcd8df184b82d9682d5f47de743b60bf0c7a499d
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-05-13 02:32:17 -0500

    zetaclient: WIP: sign & observe revert tx

--- zetaclient/metacore_observer.go
+++ zetaclient/metacore_observer.go
@@ -192,7 +192,7 @@ func (co *CoreObserver) shepherdSend(send *types.Send) {
 		to = ethcommon.HexToAddress(send.Sender)
 		toChain, err = common.ParseChain(send.SenderChain)
 		log.Info().Msgf("Abort: reverting inbound")
-	} else {
+	} else if send.Status == types.SendStatus_PendingOutbound {
 		to = ethcommon.HexToAddress(send.Receiver)
 		toChain, err = common.ParseChain(send.ReceiverChain)
 	}
@@ -275,8 +275,14 @@ SIGNLOOP:
 					break SIGNLOOP
 				}
 				srcChainID := config.Chains[send.SenderChain].ChainID
-				log.Info().Msgf("SignOutboundTx: %s => %s, nonce %d, sendHash %s", send.SenderChain, toChain, send.Nonce, send.Index)
-				tx, err = signer.SignOutboundTx(ethcommon.HexToAddress(send.Sender), srcChainID, to, amount, gasLimit, message, sendhash, send.Nonce, gasprice)
+				if send.Status == types.SendStatus_PendingRevert {
+					log.Info().Msgf("SignRevertTx: %s => %s, nonce %d, sendHash %s", send.SenderChain, toChain, send.Nonce, send.Index)
+					toChainID := config.Chains[send.ReceiverChain].ChainID
+					tx, err = signer.SignRevertTx(ethcommon.HexToAddress(send.Sender), srcChainID, to, toChainID, amount, gasLimit, message, sendhash, send.Nonce, gasprice)
+				} else if send.Status == types.SendStatus_PendingOutbound {
+					log.Info().Msgf("SignOutboundTx: %s => %s, nonce %d, sendHash %s", send.SenderChain, toChain, send.Nonce, send.Index)
+					tx, err = signer.SignOutboundTx(ethcommon.HexToAddress(send.Sender), srcChainID, to, amount, gasLimit, message, sendhash, send.Nonce, gasprice)
+				}
 				if err != nil {
 					log.Warn().Err(err).Msgf("SignOutboundTx error: nonce %d chain %s", send.Nonce, send.ReceiverChain)
 				}
