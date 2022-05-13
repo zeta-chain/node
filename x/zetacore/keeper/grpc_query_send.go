f2793be40cfcf123e12d6e1df81829d21142e11d
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-05-13 03:37:03 -0500

    zetacore: fix sendPending gRPC

--- x/zetacore/keeper/grpc_query_send.go
+++ x/zetacore/keeper/grpc_query_send.go
@@ -71,7 +71,7 @@ func (k Keeper) SendAllPending(c context.Context, req *types.QueryAllSendPending
 		var val types.Send
 		k.cdc.MustUnmarshal(iterator.Value(), &val)
 		// if the status of send is pending, which means Finalized/Revert
-		if val.Status == types.SendStatus_PendingOutbound || val.Status == types.SendStatus_PendingInbound {
+		if val.Status == types.SendStatus_PendingOutbound || val.Status == types.SendStatus_PendingRevert {
 			sends = append(sends, &val)
 		}
 	}
