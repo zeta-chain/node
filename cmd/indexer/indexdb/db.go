e142432cf44ff03caf4de4905fcbae28b79b2a59
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-06-02 02:30:25 -0500

    indexer: print out Tx struct again

--- cmd/indexer/indexdb/db.go
+++ cmd/indexer/indexdb/db.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	txtypes "github.com/cosmos/cosmos-sdk/types/tx"
 	"github.com/lib/pq"
 	"github.com/rs/zerolog/log"
 	"github.com/zeta-chain/zetacore/cmd/indexer/query"
@@ -327,7 +326,7 @@ func (idb *IndexDB) insertBlockTable(bn int64) error {
 	var txhashes []string
 	for _, v := range txResponses {
 		txhashes = append(txhashes, v.TxHash)
-		fmt.Printf("%s\n", v.Tx.GetCachedValue().(*txtypes.Tx).String())
+		fmt.Printf("%s\n%s\n", v.Tx.GetTypeUrl(), v.Tx.GoString())
 	}
 	_, err = idb.db.Exec("INSERT INTO block(blocknum, blocktimestamp, querytimestamp, numtxs, txhashes) values($1,$2,$3,$4,$5)",
 		block.Header.Height, block.Header.Time, time.Now().UTC(), len(txResponses), pq.Array(txhashes))
