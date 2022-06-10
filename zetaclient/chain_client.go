de418f38899b1c1597628c33a49825302e5d1320
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-06-10 15:21:07 -0500

    stress test: forgot to do uniswapv2  (#135)
    * hot fix: query log starting block from 3 days ago
    
    This will hopefully reduce the load of the external
    nodes.
    
    * hot fix: query uniswap exchange rate of latest block
    
    For BSC & Polygon, the node seem to only keep track of states
    in the last 1 block (?!) so that when we CallContract the
    bn we just queries is already discarded.
    
    Fix: re-order the BlockNumber() and CallContract() functions
    so that we always get the state; however the blockNum we
    attach to it might not be very accurate.
    
    * hot fix: CallContract on latest block on uniswap v2 as well

--- zetaclient/chain_client.go
+++ zetaclient/chain_client.go
@@ -660,20 +660,21 @@ func (ob *ChainObserver) GetZetaExchangeRateUniswapV2() (*big.Int, uint64, error
 	if err != nil {
 		return nil, 0, fmt.Errorf("fail to pack getReserves")
 	}
-	bn, err := ob.Client.BlockNumber(context.TODO())
-	if err != nil {
-		log.Err(err).Msgf("%s BlockNumber error", ob.chain)
-		return nil, 0, err
-	}
+
 	fromAddr := ethcommon.HexToAddress(config.TSS_TEST_ADDRESS)
 	toAddr := ethcommon.HexToAddress(config.Chains[ob.chain.String()].PoolContractAddress)
 	res, err := ob.Client.CallContract(context.TODO(), ethereum.CallMsg{
 		From: fromAddr,
 		To:   &toAddr,
 		Data: input,
-	}, big.NewInt(0).SetUint64(bn))
+	}, nil)
 	if err != nil {
 		log.Err(err).Msgf("%s CallContract error", ob.chain)
+		return nil, 0, err
+	}
+	bn, err := ob.Client.BlockNumber(context.TODO())
+	if err != nil {
+		log.Err(err).Msgf("%s BlockNumber error", ob.chain)
 		return nil, 0, err
 	}
 	output, err := ob.uniswapV2Abi.Unpack("getReserves", res)
