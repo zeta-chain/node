f1ecd70d07edbf526ced44cc892d5249f9ac261c
Charlie McCowan <mccowan.charlie@gmail.com>
2022-06-14 08:12:29 -0600

    fixed typo

--- aws-scripts/stop.sh
+++ aws-scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 systemctl stop zetaclientd
 systemctl stop zetacored
-echo "Stopped by CodeDeploy" >> /root/.zetacore/zetacored.log
-echo "Stopped by CodeDeploy" >> /root/.zetaclient/zetaclient.log.log
 sleep 2 
+echo "Stopped by CodeDeploy" >> /root/.zetacore/zetacored.log
+echo "Stopped by CodeDeploy" >> /root/.zetaclient/zetaclient.log
