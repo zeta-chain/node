35ab0afc145a2c4d1b38466681462a713ae3e9b1
Charlie McCowan <mccowan.charlie@gmail.com>
2022-06-15 08:46:18 -0600

    fixed saving version to logs after upgrade

--- aws-scripts/start.sh
+++ aws-scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo "echo $(zetacored version)" >> /root/.zetacore/zetacored.log
 systemctl start zetacored && echo "Started by CodeDeploy" >> /root/.zetacore/zetacored.log
 systemctl start zetaclientd && echo "Started by CodeDeploy" >> /root/.zetaclient/zetaclient.log
