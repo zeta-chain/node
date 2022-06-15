c0be472f9681f326a6984bc85b83ab48b42a48cd
Charlie McCowan <mccowan.charlie@gmail.com>
2022-06-15 08:44:43 -0600

    Adding version output to start script

--- aws-scripts/start.sh
+++ aws-scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+zetacored version
 systemctl start zetacored && echo "Started by CodeDeploy" >> /root/.zetacore/zetacored.log
 systemctl start zetaclientd && echo "Started by CodeDeploy" >> /root/.zetaclient/zetaclient.log
