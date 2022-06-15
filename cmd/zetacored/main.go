eb01b24ba5c5a7536b0393c33e7f855e5c9e99e3
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-06-15 09:39:26 -0500

    misc: remove logging commit hash in zetacore (#151)

--- cmd/zetacored/main.go
+++ cmd/zetacored/main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/ignite-hq/cli/ignite/pkg/cosmoscmd"
 	"github.com/zeta-chain/zetacore/app"
-
-	"github.com/zeta-chain/zetacore/common"
 )
 
 func main() {
-	fmt.Printf("zetacore commit hash %s version %s build time %s \n", common.CommitHash, common.Version, common.BuildTime)
 	rootCmd, _ := cosmoscmd.NewRootCmd(
 		app.Name,
 		app.AccountAddressPrefix,
