b0d941c95c91c648f8a2f7f52d0e9f2be988ccf2
Charlie McCowan <mccowan.charlie@gmail.com>
2022-06-16 17:36:27 -0600

    Hardcoding seed node id

--- build/zetaclient-start.sh
+++ build/zetaclient-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo $1 $2
+echo "$1" "$2"
 
 NODE_NUMBER=$1
 SEED_NODE=$2
@@ -12,7 +12,7 @@ export PATH=$PATH:/root/go/bin
 export IDX=$NODE_NUMBER
 export TSSPATH=/root/.tssnew
 
-if [ -z ${MYIP} ]; then
+if [ -z "${MYIP}" ]; then
     # If MYIP is not set, use the private IP of the host
     echo "MYIP ENV Variable Not Set -- Setting it automatically using host IP"
     export MYIP=$(hostname -i)
@@ -23,17 +23,18 @@ if (($NODE_NUMBER == 0)); then
     sleep 5 # Wait for Zetacored to start
     yes | zetaclientd -val val 2>&1 | tee ~/.zetaclient/zetaclient.log
 else
-    until [ -f SEED_NODE_ID ]; do
-        echo "Waiting for Seed Node Validator ID"
-        sleep 10
-        curl -s ${SEED_NODE}:8123/p2p -o SEED_NODE_ID
+    # until [ -f SEED_NODE_ID ]; do
+    #     echo "Waiting for Seed Node Validator ID"
+    #     sleep 10
+    #     curl -s ${SEED_NODE}:8123/p2p -o SEED_NODE_ID
 
-    done
-    SEED_NODE_ID=$(cat SEED_NODE_ID)
-    echo "SEED_NODE_ID=${SEED_NODE_ID}"
-
+    # done
+    # SEED_NODE_ID=$(cat SEED_NODE_ID)
+    # echo "SEED_NODE_ID=${SEED_NODE_ID}"
+    ## TEMPORARY FIX FOR ATHENS REBUILD
+    SEED_NODE_ID="16Uiu2HAmEWqQwcDqW5ULAxJdaoWcmekkzwnVt1hvkS9qCFqdrgED"
     yes | zetaclientd -val val \
-        --peer /dns/${SEED_NODE}/tcp/6668/p2p/${SEED_NODE_ID} \
+        --peer /dns/"${SEED_NODE}"/tcp/6668/p2p/${SEED_NODE_ID} \
         2>&1 | tee ~/.zetaclient/zetaclient.log
 
 fi
