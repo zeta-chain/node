458934dae09e8353e0973583f2e136790477b3a0
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-07-18 13:17:05 -0500

    temporarily remove stale unit test

--- cmd/indexer/query/tx_test.go
+++ cmd/indexer/query/tx_test.go
@@ -1,8 +1,6 @@
 package query
 
 import (
-	"fmt"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	. "gopkg.in/check.v1"
 	"testing"
 )
@@ -19,20 +17,21 @@ func Test(t *testing.T) { TestingT(t) }
 
 var _ = Suite(&TxSuite{})
 
-func (ts *TxSuite) SetUpSuite(c *C) {
-	zq, err := NewZetaQuerier(zetaNodeIP)
-	c.Assert(err, IsNil)
-	ts.zq = zq
-}
-
-func (ts *TxSuite) Test1(c *C) {
-	cnt := 0
-	total, err := ts.zq.VisitAllTxEvents("SendFinalized", 0, func(res *sdk.TxResponse) error {
-		cnt += 1
-		fmt.Printf("txhash: %s\n", res.TxHash)
-		return nil
-	})
-
-	c.Assert(err, IsNil)
-	fmt.Printf("total: %d, cnt: %d\n", total, cnt)
-}
+//
+//func (ts *TxSuite) SetUpSuite(c *C) {
+//	zq, err := NewZetaQuerier(zetaNodeIP)
+//	c.Assert(err, IsNil)
+//	ts.zq = zq
+//}
+//
+//func (ts *TxSuite) Test1(c *C) {
+//	cnt := 0
+//	total, err := ts.zq.VisitAllTxEvents("SendFinalized", 0, func(res *sdk.TxResponse) error {
+//		cnt += 1
+//		fmt.Printf("txhash: %s\n", res.TxHash)
+//		return nil
+//	})
+//
+//	c.Assert(err, IsNil)
+//	fmt.Printf("total: %d, cnt: %d\n", total, cnt)
+//}
