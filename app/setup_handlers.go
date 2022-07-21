bf032ddf2634fc50d1d20acc62ace208aa870ae8
Tanmay <tanmay@zetachain.com>
2022-07-21 14:41:02 -0400

    Remove handler for current Binary

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -1,30 +1,3 @@
 package app
 
-import (
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	m "github.com/cosmos/cosmos-sdk/types/module"
-	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
-)
-
-const releaseVersion = "0.1.0"
-
-func SetupHandlers(app *App) {
-	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm m.VersionMap) (m.VersionMap, error) {
-		app.Logger().Info("Running upgrade handler for " + releaseVersion)
-		return app.mm.RunMigrations(ctx, app.configurator, vm)
-	})
-
-	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
-	if err != nil {
-		panic(err)
-	}
-	if upgradeInfo.Name == releaseVersion && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
-		storeUpgrades := storetypes.StoreUpgrades{}
-		// Use upgrade store loader for the initial loading of all stores when app starts,
-		// it checks if version == upgradeHeight and applies store upgrades before loading the stores,
-		// so that new stores start with the correct version (the current height of chain),
-		// instead the default which is the latest version that store last committed i.e 0 for new stores.
-		app.SetStoreLoader(types.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
-	}
-}
+func SetupHandlers(_ *App) {}
