d8b54a60727ea54723ebbb7441ccfcf6678bc29c
Charlie <charliemc@zetachain.com>
2022-07-27 08:16:58 -0600

    Cleaned up old deploy scripts and added ability to update ENV vars from code  (#211)
    Environmental variables can be added using the deploy/env_variables.athens or deploy/env_variables.staging files and will be automatically updated during the next deployment.
    
    Removed old aws scripts include appspec & buildspec files.
    
    Moved deployment scripts into /deploy/` instead of build
    
    Other miner updates needed so zetachain can run as a restricted user instead of root.

--- aws-scripts/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "echo $(zetacored version)" >> /root/.zetacore/zetacored.log
-systemctl start zetacored && echo "Started by CodeDeploy" >> /root/.zetacore/zetacored.log
-systemctl start zetaclientd && echo "Started by CodeDeploy" >> /root/.zetaclient/zetaclient.log
