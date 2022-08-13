be209198aa2380e1d425af725946fce772259c11
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-08-13 18:46:57 -0500

    schedule sends based on heart beat from zetacore

--- zetaclient/zetacore_observer.go
+++ zetaclient/zetacore_observer.go
@@ -1,11 +1,19 @@
 package zetaclient
 
 import (
+	"encoding/base64"
+	"encoding/hex"
 	"errors"
+	"fmt"
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/rs/zerolog"
 	"gitlab.com/thorchain/tss/go-tss/keygen"
+	"math/big"
+	"math/rand"
 	"os"
 	"sort"
+	"strconv"
 	"time"
 
 	"github.com/rs/zerolog/log"
@@ -89,8 +97,8 @@ func (co *CoreObserver) GetPromCounter(name string) (prom.Counter, error) {
 func (co *CoreObserver) MonitorCore() {
 	myid := co.bridge.keys.GetSignerInfo().GetAddress().String()
 	log.Info().Msgf("MonitorCore started by signer %s", myid)
-	go co.startObserve()
-	go co.ShepherdManager()
+	go co.startSendScheduler()
+	//go co.ShepherdManager()
 
 	noKeygen := os.Getenv("NO_KEYGEN")
 	if noKeygen == "" {
@@ -155,32 +163,199 @@ func (co *CoreObserver) keygenObserve() {
 	}
 }
 
-// startObserve retrieves the pending list of Sends from ZetaCore every 10s
-// for each new send, it tries to launch a send shepherd.
-// the send shepherd makes sure the send is settled on all chains.
-func (co *CoreObserver) startObserve() {
-	observeTicker := time.NewTicker(12 * time.Second)
+// ZetaCore block is heart beat; each block we schedule some send according to
+// retry schedule.
+func (co *CoreObserver) startSendScheduler() {
+	retryMap := make(map[string]int)
+	observeTicker := time.NewTicker(3 * time.Second)
+	var lastBlockNum uint64 = 0
 	for range observeTicker.C {
-		sendList, err := co.bridge.GetAllPendingSend()
+		bn, err := co.bridge.GetZetaBlockHeight()
 		if err != nil {
-			co.logger.Error().Err(err).Msg("error requesting sends from zetacore")
+			co.logger.Error().Msg("GetZetaBlockHeight fail in startSendScheduler")
 			continue
 		}
-		if len(sendList) > 0 {
-			co.logger.Info().Msgf("#pending send: %d", len(sendList))
+		if bn > lastBlockNum { // we have a new block
+			co.logger.Info().Msgf("ZetaCore heart beat: %d", bn)
+			sendList, err := co.bridge.GetAllPendingSend()
+			if err != nil {
+				co.logger.Error().Err(err).Msg("error requesting sends from zetacore")
+				continue
+			}
+			if len(sendList) > 0 {
+				co.logger.Info().Msgf("#pending send: %d", len(sendList))
+			}
+			sendMap := splitAndSortSendListByChain(sendList)
+
+			// schedule sends
+			for chain, sendList := range sendMap {
+				co.logger.Info().Msgf("schedule %d sends on chain %s", len(sendList), chain)
+				for idx, send := range sendList {
+					sinceBlock := int64(bn) - int64(send.FinalizedMetaHeight)
+					if idx == 0 && sinceBlock%6 == 0 { // first send; always schedule on multiples of 6 blocks
+						go co.TrySend(send, sinceBlock, retryMap)
+					} else if isScheduled(sinceBlock) {
+						go co.TrySend(send, sinceBlock, retryMap)
+					}
+				}
+			}
+
+			// update last processed block number
+			lastBlockNum = bn
 		}
-		sendMap := splitAndSortSendListByChain(sendList)
-		for chain, sends := range sendMap {
-			if len(sends) > 0 {
-				co.logger.Info().Msgf("#pending sends on chain %s: %d nonce range [%d,%d]", chain, len(sends), sends[0].Nonce, sends[len(sends)-1].Nonce)
+
+	}
+}
+
+func (co *CoreObserver) TrySend(send *types.Send, sinceBlock int64, retryMap map[string]int) {
+	chain := getTargetChain(send)
+	sendID := fmt.Sprintf("%s/%d", chain, send.Nonce)
+	_, found := retryMap[sendID]
+	if !found {
+		retryMap[sendID] = 1
+	} else {
+		retryMap[sendID]++
+	}
+	logger := co.logger.With().
+		Str("sendHash", send.Index).
+		Str("sendID", sendID).
+		Int64("sinceFinalized", sinceBlock).
+		Int("retry", retryMap[sendID]).
+		Logger()
+	tNow := time.Now()
+	defer func() {
+		logger.Info().Msgf("TrySend finished in %s", time.Since(tNow))
+	}()
+
+	myid := co.bridge.keys.GetSignerInfo().GetAddress().String()
+	amount, ok := new(big.Int).SetString(send.ZetaMint, 0)
+	if !ok {
+		logger.Error().Msg("error converting MBurnt to big.Int")
+		return
+	}
+
+	var to ethcommon.Address
+	var err error
+	var toChain common.Chain
+	if send.Status == types.SendStatus_PendingRevert {
+		to = ethcommon.HexToAddress(send.Sender)
+		toChain, err = common.ParseChain(send.SenderChain)
+		logger.Info().Msgf("Abort: reverting inbound")
+	} else if send.Status == types.SendStatus_PendingOutbound {
+		to = ethcommon.HexToAddress(send.Receiver)
+		toChain, err = common.ParseChain(send.ReceiverChain)
+	}
+	if err != nil {
+		logger.Error().Err(err).Msg("ParseChain fail; skip")
+		return
+	}
+
+	// Early return if the send is already processed
+	included, confirmed, _ := co.clientMap[toChain].IsSendOutTxProcessed(send.Index, int(send.Nonce))
+	if included || confirmed {
+		logger.Info().Msgf("sendHash already processed; exit signer")
+		return
+	}
+
+	signer := co.signerMap[toChain]
+	message, err := base64.StdEncoding.DecodeString(send.Message)
+	if err != nil {
+		logger.Err(err).Msgf("decode send.Message %s error", send.Message)
+	}
+
+	gasLimit := send.GasLimit
+	if gasLimit < 50_000 {
+		gasLimit = 50_000
+		logger.Warn().Msgf("gasLimit %d is too low; set to %d", send.GasLimit, gasLimit)
+	}
+	if gasLimit > 1_000_000 {
+		gasLimit = 1_000_000
+		logger.Warn().Msgf("gasLimit %d is too high; set to %d", send.GasLimit, gasLimit)
+	}
+
+	logger.Info().Msgf("chain %s minting %d to %s, nonce %d, finalized zeta bn %d", toChain, amount, to.Hex(), send.Nonce, send.FinalizedMetaHeight)
+	sendHash, err := hex.DecodeString(send.Index[2:]) // remove the leading 0x
+	if err != nil || len(sendHash) != 32 {
+		logger.Error().Err(err).Msgf("decode sendHash %s error", send.Index)
+		return
+	}
+	var sendhash [32]byte
+	copy(sendhash[:32], sendHash[:32])
+	gasprice, ok := new(big.Int).SetString(send.GasPrice, 10)
+	if !ok {
+		logger.Error().Err(err).Msgf("cannot convert gas price  %s ", send.GasPrice)
+		return
+	}
+	// use 33% higher gas price for timely confirmation
+	gasprice = gasprice.Mul(gasprice, big.NewInt(4))
+	gasprice = gasprice.Div(gasprice, big.NewInt(3))
+	var tx *ethtypes.Transaction
+
+	srcChainID := config.Chains[send.SenderChain].ChainID
+	if send.Status == types.SendStatus_PendingRevert {
+		logger.Info().Msgf("SignRevertTx: %s => %s, nonce %d", send.SenderChain, toChain, send.Nonce)
+		toChainID := config.Chains[send.ReceiverChain].ChainID
+		tx, err = signer.SignRevertTx(ethcommon.HexToAddress(send.Sender), srcChainID, to.Bytes(), toChainID, amount, gasLimit, message, sendhash, send.Nonce, gasprice)
+	} else if send.Status == types.SendStatus_PendingOutbound {
+		logger.Info().Msgf("SignOutboundTx: %s => %s, nonce %d", send.SenderChain, toChain, send.Nonce)
+		tx, err = signer.SignOutboundTx(ethcommon.HexToAddress(send.Sender), srcChainID, to, amount, gasLimit, message, sendhash, send.Nonce, gasprice)
+	}
+
+	if err != nil {
+		logger.Warn().Err(err).Msgf("SignOutboundTx error: nonce %d chain %s", send.Nonce, send.ReceiverChain)
+		return
+	} else {
+		logger.Info().Msgf("Keysign success: %s => %s, nonce %d", send.SenderChain, toChain, send.Nonce)
+	}
+	cnt, err := co.GetPromCounter(OUTBOUND_TX_SIGN_COUNT)
+	if err != nil {
+		log.Error().Err(err).Msgf("GetPromCounter error")
+	} else {
+		cnt.Inc()
+	}
+	if tx != nil {
+		outTxHash := tx.Hash().Hex()
+		logger.Info().Msgf("on chain %s nonce %d, outTxHash %s signer %s", signer.chain, send.Nonce, outTxHash, myid)
+		if myid == send.Signers[send.Broadcaster] || myid == send.Signers[int(send.Broadcaster+1)%len(send.Signers)] {
+			backOff := 1000 * time.Millisecond
+			// retry loop: 1s, 2s, 4s, 8s, 16s in case of RPC error
+			for i := 0; i < 5; i++ {
+				logger.Info().Msgf("broadcasting tx %s to chain %s: nonce %d, retry %d", outTxHash, toChain, send.Nonce, i)
+				// #nosec G404 randomness is not a security issue here
+				time.Sleep(time.Duration(rand.Intn(1500)) * time.Millisecond) //random delay to avoid sychronized broadcast
+				err := signer.Broadcast(tx)
+				if err != nil {
+					retry := HandlerBroadcastError(err, co.fileLogger, strconv.FormatUint(send.Nonce, 10), toChain.String(), outTxHash)
+					if !retry {
+						break
+					}
+					backOff *= 2
+					continue
+				}
+				logger.Info().Msgf("Broadcast success: nonce %d to chain %s outTxHash %s", send.Nonce, toChain, outTxHash)
+				co.fileLogger.Info().Msgf("Broadcast success: nonce %d chain %s outTxHash %s", send.Nonce, toChain, outTxHash)
+				zetaHash, err := co.bridge.AddTxHashToWatchlist(toChain.String(), tx.Nonce(), outTxHash)
+				if err != nil {
+					logger.Err(err).Msgf("Unable to add to tracker on ZetaCore: nonce %d chain %s outTxHash %s", send.Nonce, toChain, outTxHash)
+					break
+				}
+				logger.Info().Msgf("Broadcast to core successful %s", zetaHash)
 			}
 		}
-		for _, send := range sendList {
-			if send.Status == types.SendStatus_PendingOutbound || send.Status == types.SendStatus_PendingRevert {
-				co.sendNew <- send
-			} //else if send.Status == types.SendStatus_Mined || send.Status == types.SendStatus_Reverted || send.Status == types.SendStatus_Aborted {
-		}
 	}
+
+}
+
+func isScheduled(d int64) bool {
+	//      0 ----6 ---------12-------12---------18-------24--------
+	if d <= 0 {
+		return false
+	} else if d == 1 || d == 7 || d == 19 || d == 31 || d == 43 || d == 67 {
+		return true
+	} else if d%100 == 0 {
+		return true
+	}
+	return false
 }
 
 func splitAndSortSendListByChain(sendList []*types.Send) map[string][]*types.Send {
