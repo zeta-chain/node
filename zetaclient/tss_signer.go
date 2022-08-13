b9ec70db461ce010d41a55afdfd8c3fd38435e37
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-08-13 18:48:08 -0500

    keysign 0.14.0 request

--- zetaclient/tss_signer.go
+++ zetaclient/tss_signer.go
@@ -58,7 +58,7 @@ func (tss *TSS) Sign(digest []byte) ([65]byte, error) {
 	log.Debug().Msgf("hash of digest is %s", H)
 
 	tssPubkey := tss.PubkeyInBech32
-	keysignReq := keysign.NewRequest(tssPubkey, []string{base64.StdEncoding.EncodeToString(H)}, 10, tss.Pubkeys, "0.13.0")
+	keysignReq := keysign.NewRequest(tssPubkey, []string{base64.StdEncoding.EncodeToString(H)}, 10, tss.Pubkeys, "0.14.0")
 	ks_res, err := tss.Server.KeySign(keysignReq)
 	if err != nil {
 		log.Warn().Msg("keysign fail")
