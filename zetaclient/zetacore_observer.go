9d69555aedb900b67b070c6f6c4f270d27cb2a30
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-08-15 10:23:41 -0500

    increase keysign concurrency further

--- zetaclient/zetacore_observer.go
+++ zetaclient/zetacore_observer.go
@@ -187,7 +187,7 @@ func (co *CoreObserver) startSendScheduler() {
 					if isScheduled(sinceBlock, idx < 3 || len(sendList) > 20) {
 						go co.TryProcessOutTx(send, sinceBlock)
 					}
-					if idx > 50 { // only schedule 50 sends per chain
+					if idx > 100 { // only look at 50 sends per chain
 						break
 					}
 				}
