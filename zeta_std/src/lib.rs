c1e7d3e83331cd796854462168a98ff4f9e8d6d9
Tanmay <tanmay@zetachain.com>
2022-08-16 15:09:21 -0400

    Commit zeta_std

---
+++ zeta_std/src/lib.rs
@@ -0,0 +1,52 @@
+use cosmwasm_std::{CosmosMsg,CustomQuery};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ZetaCoreMsg {
+    AddToWatchList {
+        chain: String,
+        nonce: u32,
+        tx_hash: String,
+    },
+}
+
+
+impl cosmwasm_std::CustomMsg for ZetaCoreMsg{}
+
+
+impl From<ZetaCoreMsg> for CosmosMsg<ZetaCoreMsg> {
+    fn from(original: ZetaCoreMsg) -> Self {
+        CosmosMsg::Custom(original)
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ZetaCoreQuery {
+    WatchList {},
+}
+
+impl CustomQuery for ZetaCoreQuery {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct WatchlistQueryResponse {
+    pub watchlist : Vec<OutTxTracker>
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct OutTxTracker {
+    pub index   : String ,
+    pub chain   : String ,
+    pub nonce   : String ,
+    pub hashlist: Vec<TxHashList>,
+}
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct TxHashList {
+    pub txhash :String ,
+    pub singer :String
+}
