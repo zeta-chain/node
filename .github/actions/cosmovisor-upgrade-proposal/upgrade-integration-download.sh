b675e194052848db00cd29775c1544cabde5f016
Charlie McCowan <mccowan.charlie@gmail.com>
2022-08-19 13:45:56 -0600

    fixing/testint actions

--- .github/actions/cosmovisor-upgrade-proposal/upgrade-integration-download.sh
+++ .github/actions/cosmovisor-upgrade-proposal/upgrade-integration-download.sh
@@ -20,27 +20,27 @@ rm -rf ZetaClient.log
 
 make install
 # Genesis
-mkdir -p $DAEMON_HOME/cosmovisor/genesis/bin
+mkdir -p "$DAEMON_HOME"/cosmovisor/genesis/bin
 #mkdir -p $DAEMON_HOME/cosmovisor/upgrades/0.2.1/bin
-cp $GOPATH/bin/zetacored $DAEMON_HOME/cosmovisor/genesis/bin
-cp $GOPATH/bin/zetaclientd $DAEMON_HOME/cosmovisor/genesis/bin
+cp "$GOPATH"/bin/zetacored "$DAEMON_HOME"/cosmovisor/genesis/bin
+cp "$GOPATH"/bin/zetaclientd "$DAEMON_HOME"/cosmovisor/genesis/bin
 
 
-chmod +x $DAEMON_HOME/cosmovisor/genesis/bin/zetacored
-chmod +x $DAEMON_HOME/cosmovisor/genesis/bin/zetaclientd
+chmod +x "$DAEMON_HOME"/cosmovisor/genesis/bin/zetacored
+chmod +x "$DAEMON_HOME"/cosmovisor/genesis/bin/zetaclientd
 
-zetacored init test --chain-id=localnet -o
-echo "race draft rival universe maid cheese steel logic crowd fork comic easy truth drift tomorrow eye buddy head time cash swing swift midnight borrow" | zetacored keys add zeta --recover --keyring-backend=test
-echo "hand inmate canvas head lunar naive increase recycle dog ecology inhale december wide bubble hockey dice worth gravity ketchup feed balance parent secret orchard" | zetacored keys add mario --recover --keyring-backend=test
-zetacored add-genesis-account $(zetacored keys show zeta -a --keyring-backend=test) 500000000000000000000000000000000stake --keyring-backend=test
-zetacored add-genesis-account $(zetacored keys show mario -a --keyring-backend=test)  500000000000000000000000000000000stake --keyring-backend=test
-zetacored gentx zeta 1000000000000000000000000stake --chain-id=localnet --keyring-backend=test
-zetacored collect-gentxs
-zetacored validate-genesis
+# zetacored init test --chain-id=localnet -o
+# echo "race draft rival universe maid cheese steel logic crowd fork comic easy truth drift tomorrow eye buddy head time cash swing swift midnight borrow" | zetacored keys add zeta --recover --keyring-backend=test
+# echo "hand inmate canvas head lunar naive increase recycle dog ecology inhale december wide bubble hockey dice worth gravity ketchup feed balance parent secret orchard" | zetacored keys add mario --recover --keyring-backend=test
+# zetacored add-genesis-account $(zetacored keys show zeta -a --keyring-backend=test) 500000000000000000000000000000000stake --keyring-backend=test
+# zetacored add-genesis-account $(zetacored keys show mario -a --keyring-backend=test)  500000000000000000000000000000000stake --keyring-backend=test
+# zetacored gentx zeta 1000000000000000000000000stake --chain-id=localnet --keyring-backend=test
+# zetacored collect-gentxs
+# zetacored validate-genesis
 
 
-contents="$(jq '.app_state.gov.voting_params.voting_period = "10s"' $DAEMON_HOME/config/genesis.json)" && \
-echo "${contents}" > $DAEMON_HOME/config/genesis.json
+contents="$(jq '.app_state.gov.voting_params.voting_period = "10s"' "$DAEMON_HOME"/config/genesis.json)" && \
+echo "${contents}" > "$DAEMON_HOME"/config/genesis.json
 
 
 cosmovisor start --home ~/.zetacore/ --p2p.laddr 0.0.0.0:27655  --grpc.address 0.0.0.0:9096 --grpc-web.address 0.0.0.0:9093 --address tcp://0.0.0.0:27659 --rpc.laddr tcp://127.0.0.1:26657 >> zetanode.log 2>&1  &
