4eda8428a22bcc60425572cb97114d378c616b39
Christian Kuroki <ckuroki@gmail.com>
2022-08-30 11:34:23 -0300

    info msg

--- zetaclient/zetacore_observer.go
+++ zetaclient/zetacore_observer.go
@@ -290,6 +290,7 @@ func (co *CoreObserver) startSendScheduler() {
 						go co.TryProcessOutTx(send, sinceBlock, outTxMan)
 					}
 					if idx > 50 { // only look at 50 sends per chain
+						logger.Info().Msgf("sends per chain > 50 : %v", idx)
 						break
 					}
 				}
