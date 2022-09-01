5b34e5414701df1b8305a9aa422030afc511af35
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-09-01 00:49:45 -0500

    make ethermint work

--- app/encoding.go
+++ app/encoding.go
@@ -1,16 +1,18 @@
 package app
 
 import (
-	"github.com/cosmos/cosmos-sdk/std"
-	"github.com/zeta-chain/zetacore/app/params"
+	evmenc "github.com/evmos/ethermint/encoding"
+	//"github.com/zeta-chain/zetacore/app/params"
+	"github.com/cosmos/cosmos-sdk/simapp/params"
 )
 
 // MakeEncodingConfig creates an EncodingConfig for testing
 func MakeEncodingConfig() params.EncodingConfig {
-	encodingConfig := params.MakeEncodingConfig()
-	std.RegisterLegacyAminoCodec(encodingConfig.Amino)
-	std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
-	ModuleBasics.RegisterLegacyAminoCodec(encodingConfig.Amino)
-	ModuleBasics.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	//encodingConfig := params.MakeEncodingConfig()
+	encodingConfig := evmenc.MakeConfig(ModuleBasics)
+	//std.RegisterLegacyAminoCodec(encodingConfig.Amino)
+	//std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	//ModuleBasics.RegisterLegacyAminoCodec(encodingConfig.Amino)
+	//ModuleBasics.RegisterInterfaces(encodingConfig.InterfaceRegistry)
 	return encodingConfig
 }
