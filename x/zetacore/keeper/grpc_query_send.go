1d0511e71cd898079fb2387f46d0038583638000
Tanmay <tanmay@zetachain.com>
2022-09-02 15:33:51 -0400

    Add send to scrub TX

--- x/zetacore/keeper/grpc_query_send.go
+++ x/zetacore/keeper/grpc_query_send.go
@@ -60,7 +60,7 @@ func (k Keeper) SendAllPending(c context.Context, req *types.QueryAllSendPending
 	}
 	ctx := sdk.UnwrapSDKContext(c)
 	sends := k.GetAllSend(ctx, []types.SendStatus{types.SendStatus_PendingOutbound, types.SendStatus_PendingRevert})
-	sort.SliceStable(sends,
+	sort.Slice(sends,
 		func(i, j int) bool {
 			if sends[i].FinalizedMetaHeight == sends[j].FinalizedMetaHeight {
 				return sends[i].Nonce < sends[j].Nonce
--- x/zetacore/keeper/msg_server_receive_confirmation.go
+++ x/zetacore/keeper/msg_server_receive_confirmation.go
@@ -23,7 +23,7 @@ func (k msgServer) ReceiveConfirmation(goCtx context.Context, msg *types.MsgRece
 	}
 
 	index := msg.SendHash
-	send, isFound := k.GetSend(ctx, index, types.SendStatus_PendingOutbound)
+	send, isFound := k.GetSendMultipleStatus(ctx, index, []types.SendStatus{types.SendStatus_PendingOutbound, types.SendStatus_PendingRevert})
 	if !isFound {
 		log.Error().Msgf("Cannot find broadcast tx hash %s on %s chain", index, msg.Chain)
 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, fmt.Sprintf("Cannot find broadcast tx hash %s on %s chain", index, msg.Chain))
