b9aa141f6a60b5a0261596af4bc3451020507e4e
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-09-02 17:20:26 -0500

    fix send voter logic

--- x/zetacore/keeper/msg_server_send_voter.go
+++ x/zetacore/keeper/msg_server_send_voter.go
@@ -23,7 +23,8 @@ func (k msgServer) SendVoter(goCtx context.Context, msg *types.MsgSendVoter) (*t
 	}
 
 	index := msg.Digest()
-	send, isFound := k.GetSend(ctx, index, types.SendStatus_PendingInbound)
+	send, isFound := k.GetSendMultipleStatus(ctx, index, []types.SendStatus{types.SendStatus_PendingInbound, types.SendStatus_PendingOutbound, types.SendStatus_PendingRevert})
+
 
 	if isDuplicateSigner(msg.Creator, send.Signers) {
 		return nil, sdkerrors.Wrap(types.ErrDuplicateMsg, fmt.Sprintf("signer %s double signing!!", msg.Creator))
