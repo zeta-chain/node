554e802ce64066c596a617cf5de265d8dbb16e90
Tanmay <tanmay@zetachain.com>
2022-09-14 01:24:36 -0400

    add ganache address verification

--- localnet-copier.sh
+++ localnet-copier.sh
@@ -1,6 +1,18 @@
+rm -rf zetacored
+rm -rf zetaclientd
+
+echo "Building client"
 GOOS=linux GOARCH=amd64 go build ./cmd/zetaclientd
 echo "Copying to containers"
 docker cp zetaclientd local-z-k8s-node0-1:/usr/local/bin/
 docker cp zetaclientd local-z-k8s-node1-1:/usr/local/bin/
 docker cp zetaclientd local-z-k8s-node2-1:/usr/local/bin/
 docker cp zetaclientd local-z-k8s-node3-1:/usr/local/bin/
+
+#echo "Building core"
+#GOOS=linux GOARCH=amd64 go build ./cmd/zetacored
+#echo "Copying core to containers"
+#docker cp zetacored local-z-k8s-node0-1:/usr/local/bin/
+#docker cp zetacored local-z-k8s-node1-1:/usr/local/bin/
+#docker cp zetacored local-z-k8s-node2-1:/usr/local/bin/
+#docker cp zetacored local-z-k8s-node3-1:/usr/local/bin/
