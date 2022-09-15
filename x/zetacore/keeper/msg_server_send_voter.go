ef550e6f1c2b2193631bafd7722d1709fe5ae39e
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-09-14 19:15:50 -0500

    fix deposit

--- x/zetacore/keeper/msg_server_send_voter.go
+++ x/zetacore/keeper/msg_server_send_voter.go
@@ -102,7 +102,7 @@ func (k msgServer) SendVoter(goCtx context.Context, msg *types.MsgSendVoter) (*t
 			to := ethcommon.HexToAddress(send.Receiver)
 			amount, ok := big.NewInt(0).SetString(send.ZetaBurnt, 10)
 			if !ok {
-				send.StatusMessage = fmt.Sprintf("cannot parse zetaMint: %s", send.ZetaBurnt)
+				send.StatusMessage = fmt.Sprintf("cannot parse zetaBurnt: %s", send.ZetaBurnt)
 				send.Status = types.SendStatus_Aborted
 				goto EPILOGUE
 			}
