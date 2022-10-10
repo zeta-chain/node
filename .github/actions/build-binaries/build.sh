bd82ab48dcd41ae67fba4386f4e7627ff8d43212
CharlieM <31941002+CharlieMc0@users.noreply.github.com>
2022-10-10 15:37:29 -0400

    removed unsued files from build and s3 cp

--- .github/actions/build-binaries/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Building zetacored and zetaclients"
-# go build -mod=readonly ./cmd/zetacored
-# go build -mod=readonly ./cmd/zetaclientd
-
-make install
-cp "$HOME"/go/bin/* ./
