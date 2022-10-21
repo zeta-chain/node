4be01927372f2e5acf5107dd389879a12cf54a4e
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-10-21 15:54:10 -0500

    fix app.go initialization

--- init.sh
+++ init.sh
@@ -101,4 +101,4 @@ if [[ $1 == "pending" ]]; then
 fi
 
 # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
-zetacored start --pruning=nothing --evm.tracer=json $TRACE --log_level $LOGLEVEL --minimum-gas-prices=0.0001azeta --json-rpc.api eth,txpool,personal,net,debug,web3,miner --api.enable --home ~/.zetacored
+zetacored start --pruning=nothing  $TRACE --log_level $LOGLEVEL --minimum-gas-prices=0.0001azeta --json-rpc.api eth,txpool,personal,net,debug,web3,miner --api.enable --home ~/.zetacored
