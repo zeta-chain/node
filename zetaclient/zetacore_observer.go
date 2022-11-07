22cc293cd728c59c2b6a87e4446e157bec2ea1a8
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-11-07 13:49:59 -0600

    reduce isScheduled interval to 10

--- zetaclient/zetacore_observer.go
+++ zetaclient/zetacore_observer.go
@@ -728,7 +728,7 @@ func isScheduled(diff int64, priority bool) bool {
 	if priority {
 		return d%20 == 0
 	}
-	if d < 1000 && d%20 == 0 {
+	if d < 1000 && d%10 == 0 {
 		return true
 	} else if d >= 1000 && d%100 == 0 { // after 100 blocks, schedule once per 100 blocks
 		return true
