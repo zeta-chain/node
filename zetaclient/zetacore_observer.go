a8cde8ec86f599a16a59cae19d230ed28fc82c9a
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-11-08 12:15:59 -0600

    reduce getting sendPending

--- zetaclient/zetacore_observer.go
+++ zetaclient/zetacore_observer.go
@@ -319,6 +319,24 @@ func (co *CoreObserver) startSendScheduler() {
 	observeTicker := time.NewTicker(3 * time.Second)
 	var lastBlockNum uint64
 	var sendList []*types.Send
+
+	for range observeTicker.C {
+		bn, err := co.bridge.GetZetaBlockHeight()
+		if err != nil {
+			logger.Error().Msg("GetZetaBlockHeight fail in startSendScheduler")
+			continue
+		}
+		timeStart := time.Now()
+		sendList, err := co.bridge.GetAllPendingSend()
+		logger.Info().Int64("block", int64(bn)).Dur("elapsed", time.Since(timeStart)).Int("items", len(sendList)).Msg("GetAllPendingSend")
+
+		if err != nil {
+			logger.Error().Err(err).Msg("error requesting sends from zetacore")
+			continue
+		}
+		break
+	}
+
 	for range observeTicker.C {
 		bn, err := co.bridge.GetZetaBlockHeight()
 		if err != nil {
@@ -327,14 +345,6 @@ func (co *CoreObserver) startSendScheduler() {
 		}
 		if bn > lastBlockNum { // we have a new block
 			timeStart := time.Now()
-			if bn%100 == 0 {
-				sendList, err = co.bridge.GetAllPendingSend()
-				logger.Info().Int64("block", int64(bn)).Dur("elapsed", time.Since(timeStart)).Int("items", len(sendList)).Msg("GetAllPendingSend")
-				if err != nil {
-					logger.Error().Err(err).Msg("error requesting sends from zetacore")
-					continue
-				}
-			}
 
 			sendMap := splitAndSortSendListByChain(sendList)
 
