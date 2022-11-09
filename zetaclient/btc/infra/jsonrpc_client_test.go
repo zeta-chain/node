879ceb37c24d150b82ad3b180e8394ffaf6ccd0b
Christian Kuroki <ckuroki@gmail.com>
2022-11-09 17:56:05 -0300

    filter events

--- zetaclient/btc/infra/jsonrpc_client_test.go
+++ zetaclient/btc/infra/jsonrpc_client_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	"github.com/zeta-chain/zetacore/zetaclient/btc/model"
 )
 
 type JSONRpcClientTestSuite struct {
@@ -21,7 +22,7 @@ func (suite *JSONRpcClientTestSuite) TearDownSuite() {
 }
 
 func (suite *JSONRpcClientTestSuite) TestBlockHeight() {
-	var min int64 = 2378200
+	var min int64 = 2405366
 	blockNumber, err := suite.cli.GetBlockHeight()
 	suite.Require().NoError(err)
 	suite.Assert().Greater(blockNumber, min)
@@ -29,20 +30,23 @@ func (suite *JSONRpcClientTestSuite) TestBlockHeight() {
 }
 
 func (suite *JSONRpcClientTestSuite) TestBlockHash() {
-	var expected string = "0000000039364a85b7326558802bacef7390aed973e9f9b0627f29e8ac3e6676"
-	var block int64 = 2378200
+	var block int64 = 2405366
+	expected := "00000000000000388a41c619e631bcd034e22ec6ca12a8a40123b64677f62cdd"
 	hash, err := suite.cli.GetBlockHash(block)
 	suite.Require().NoError(err)
 	suite.Assert().Equal(expected, hash)
 }
 
 func (suite *JSONRpcClientTestSuite) TestBlockByHash() {
-	var hash string = "0000000039364a85b7326558802bacef7390aed973e9f9b0627f29e8ac3e6676"
-	events, err := suite.cli.GetEventsByHash(hash)
-	for _, evt := range events {
-		suite.T().Logf("EVENT=>%v\n", evt)
-	}
+	hash := "00000000000000388a41c619e631bcd034e22ec6ca12a8a40123b64677f62cdd"
+	expected := "Amount: 0.0002, Address: 0x6162303132333435363738393031323334353637, Message: 89012345678901Hello World!"
+	rawEvents, err := suite.cli.GetEventsByHash(hash)
 	suite.Require().NoError(err)
+
+	event, err := model.ParseRawEvents(rawEvents)
+	suite.Require().NoError(err)
+
+	suite.Assert().Equal(expected, event.String())
 }
 
 // TestJSONRpcClient is the entry point of this test suite
