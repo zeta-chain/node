386cc2e3eeb58732adc3b609d2c63f3e284a23e1
Christian Kuroki <ckuroki@gmail.com>
2022-11-09 17:56:05 -0300

    filter events

--- zetaclient/btc/model/connector.go
+++ zetaclient/btc/model/connector.go
@@ -1,9 +1,11 @@
 package model
 
 import (
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"math/big"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
 )
@@ -21,9 +23,10 @@ const (
 	noMagicNumber   = "no magic number"
 	addrLen         = 20 // len of expected address
 	minLen          = addrLen + 1
+	opReturn        = "OP_RETURN "
 )
 
-func NewConnectorEvent(amt string, raw []byte) (*ConnectorEvent, error) {
+func NewConnectorEvent(amount *big.Float, raw []byte) (*ConnectorEvent, error) {
 	if raw == nil || len(raw) < minLen {
 		return nil, errors.New(invalidInput)
 	}
@@ -31,10 +34,6 @@ func NewConnectorEvent(amt string, raw []byte) (*ConnectorEvent, error) {
 		return nil, errors.New(noMagicNumber)
 	}
 	address := common.BytesToAddress(raw[1 : addrLen+1])
-	amount, ok := new(big.Float).SetString(amt)
-	if !ok {
-		return nil, errors.New(invalidAmount)
-	}
 	return &ConnectorEvent{
 		Amount:  amount,
 		Address: address,
@@ -43,5 +42,27 @@ func NewConnectorEvent(amt string, raw []byte) (*ConnectorEvent, error) {
 }
 
 func (evt *ConnectorEvent) String() string {
-	return fmt.Sprintf("Amount: %v, Address: %v, Message: %v\n", evt.Amount, evt.Address.Hex(), string(evt.Message))
+	return fmt.Sprintf("Amount: %v, Address: %v, Message: %v", evt.Amount, evt.Address.Hex(), string(evt.Message))
+}
+
+func ParseRawEvents(rawEvents []*RawEvent) (*ConnectorEvent, error) {
+	var amount *big.Float
+	var evtStr string
+	for _, raw := range rawEvents {
+		// if raw event have an address is tss_address and value is tx amount
+		if len(raw.Addresses) > 0 {
+			amount = big.NewFloat(raw.Values[0])
+			continue
+		}
+		// process memo field
+		// check OP_RETURN
+		if len(raw.ASM) >= (len(opReturn)+addrLen+1) && strings.HasPrefix(raw.ASM, opReturn) {
+			evtStr = raw.ASM[len(opReturn):]
+		}
+	}
+	evtBytes, err := hex.DecodeString(evtStr)
+	if err != nil {
+		return nil, err
+	}
+	return NewConnectorEvent(amount, evtBytes)
 }
