8bc921c719227d74c19135c82172cc7f094e9558
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-11-13 00:29:54 -0600

    BTCSigner test cases; fix go.mod btcd module

--- zetaclient/btc_signer_test.go
+++ zetaclient/btc_signer_test.go
@@ -1,10 +1,14 @@
 package zetaclient
 
 import (
-	"github.com/btcsuite/btcd/btcutil"
+	"encoding/hex"
+	"fmt"
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/btcutil"
 	"github.com/ethereum/go-ethereum/crypto"
 	. "gopkg.in/check.v1"
 )
@@ -36,27 +40,87 @@ func (s *BTCSignerSuite) SetUpTest(c *C) {
 }
 
 func (s *BTCSignerSuite) Test1(c *C) {
-	addr := s.btcSigner.tssSigner.BTCAddressPubkey()
-	originTx, err := createFakeOriginTx(addr.AddressPubKeyHash())
+	// Ordinarily the private key would come from whatever storage mechanism
+	// is being used, but for this example just hard code it.
+	privKeyBytes, err := hex.DecodeString("22a47fa09a223f2aa079edf85a7c2" +
+		"d4f8720ee63e502ee2869afab7de234b80c")
 	c.Assert(err, IsNil)
-	_ = originTx
-}
 
-// createFakeOriginTx creates a fake coinbase transaction that is used in the
-// example as a stand-in for what ordinarily be the real transaction that is
-// being spent.
-func createFakeOriginTx(addr btcutil.Address) (*wire.MsgTx, error) {
-	tx := wire.NewMsgTx(wire.TxVersion)
+	privKey, pubKey := btcec.PrivKeyFromBytes(btcec.S256(), privKeyBytes)
+	pubKeyHash := btcutil.Hash160(pubKey.SerializeCompressed())
+	addr, err := btcutil.NewAddressPubKeyHash(pubKeyHash,
+		&chaincfg.MainNetParams)
+	c.Assert(err, IsNil)
+
+	// For this example, create a fake transaction that represents what
+	// would ordinarily be the real transaction that is being spent.  It
+	// contains a single output that pays to address in the amount of 1 BTC.
+	originTx := wire.NewMsgTx(wire.TxVersion)
 	prevOut := wire.NewOutPoint(&chainhash.Hash{}, ^uint32(0))
-
-	txIn := wire.NewTxIn(prevOut, []byte{txscript.OP_0, txscript.OP_0}, [][]byte{})
-	tx.AddTxIn(txIn)
-
+	txIn := wire.NewTxIn(prevOut, []byte{txscript.OP_0, txscript.OP_0}, nil)
+	originTx.AddTxIn(txIn)
 	pkScript, err := txscript.PayToAddrScript(addr)
-	if err != nil {
-		return nil, err
-	}
+	c.Assert(err, IsNil)
+
 	txOut := wire.NewTxOut(100000000, pkScript)
-	tx.AddTxOut(txOut)
-	return tx, nil
+	originTx.AddTxOut(txOut)
+	originTxHash := originTx.TxHash()
+
+	// Create the transaction to redeem the fake transaction.
+	redeemTx := wire.NewMsgTx(wire.TxVersion)
+
+	// Add the input(s) the redeeming transaction will spend.  There is no
+	// signature script at this point since it hasn't been created or signed
+	// yet, hence nil is provided for it.
+	prevOut = wire.NewOutPoint(&originTxHash, 0)
+	txIn = wire.NewTxIn(prevOut, nil, nil)
+	redeemTx.AddTxIn(txIn)
+
+	// Ordinarily this would contain that actual destination of the funds,
+	// but for this example don't bother.
+	txOut = wire.NewTxOut(0, nil)
+	redeemTx.AddTxOut(txOut)
+
+	// Sign the redeeming transaction.
+	lookupKey := func(a btcutil.Address) (*btcec.PrivateKey, bool, error) {
+		// Ordinarily this function would involve looking up the private
+		// key for the provided address, but since the only thing being
+		// signed in this example uses the address associated with the
+		// private key from above, simply return it with the compressed
+		// flag set since the address is using the associated compressed
+		// public key.
+		//
+		// NOTE: If you want to prove the code is actually signing the
+		// transaction properly, uncomment the following line which
+		// intentionally returns an invalid key to sign with, which in
+		// turn will result in a failure during the script execution
+		// when verifying the signature.
+		//
+		// privKey.D.SetInt64(12345)
+		//
+		return privKey, true, nil
+	}
+	// Notice that the script database parameter is nil here since it isn't
+	// used.  It must be specified when pay-to-script-hash transactions are
+	// being signed.
+	sigScript, err := txscript.SignTxOutput(&chaincfg.MainNetParams,
+		redeemTx, 0, originTx.TxOut[0].PkScript, txscript.SigHashAll,
+		txscript.KeyClosure(lookupKey), nil, nil)
+	c.Assert(err, IsNil)
+
+	redeemTx.TxIn[0].SignatureScript = sigScript
+
+	// Prove that the transaction has been validly signed by executing the
+	// script pair.
+	flags := txscript.ScriptBip16 | txscript.ScriptVerifyDERSignatures |
+		txscript.ScriptStrictMultiSig |
+		txscript.ScriptDiscourageUpgradableNops
+	vm, err := txscript.NewEngine(originTx.TxOut[0].PkScript, redeemTx, 0,
+		flags, nil, nil, -1)
+	c.Assert(err, IsNil)
+
+	err = vm.Execute()
+	c.Assert(err, IsNil)
+
+	fmt.Println("Transaction successfully signed")
 }
