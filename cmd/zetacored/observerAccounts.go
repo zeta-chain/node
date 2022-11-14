a119fe94c1fa5b6cbd463020c6c1bb8fddc65f02
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-11-14 01:25:25 -0600

    fix reset-testnet.sh

--- cmd/zetacored/observerAccounts.go
+++ cmd/zetacored/observerAccounts.go
@@ -68,6 +68,7 @@ func AddObserverAccountCmd() *cobra.Command {
 					"Ganache"   
 					"Baobap"    
 					"BscTestnet"
+					"BTCTestnet"
 					
             Observation Types : 
 				    "InBoundTx",
