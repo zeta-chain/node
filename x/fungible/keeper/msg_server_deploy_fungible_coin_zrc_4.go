65fbd6684a90ebaa703f4a0417a2e859a200c637
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-11-24 01:18:22 -0600

    DeployFungibleCoinZRC20 handler: set coin and pool in system contract if coin is gas

--- x/fungible/keeper/msg_server_deploy_fungible_coin_zrc_4.go
+++ x/fungible/keeper/msg_server_deploy_fungible_coin_zrc_4.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	zetacommon "github.com/zeta-chain/zetacore/common"
 	"github.com/zeta-chain/zetacore/x/fungible/types"
 	"math/big"
 )
@@ -14,25 +15,32 @@ func (k msgServer) DeployFungibleCoinZRC20(goCtx context.Context, msg *types.Msg
 	if msg.Creator != types.AdminAddress {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "only admin can deploy fungible coin")
 	}
+	if msg.CoinType == zetacommon.CoinType_Gas {
+		_, err := k.setupChainGasCoinAndPool(ctx, "BTCTESTNET", "BTC", "tBTC", 8)
+		if err != nil {
+			return nil, sdkerrors.Wrapf(err, "failed to setupChainGasCoinAndPool")
+		}
+	} else {
+		addr, err := k.DeployZRC20Contract(ctx, msg.Name, msg.Symbol, uint8(msg.Decimals), msg.ForeignChain, msg.CoinType, msg.ERC20, big.NewInt(int64(msg.GasLimit)))
+		if err != nil {
+			return nil, err
+		}
 
-	addr, err := k.DeployZRC20Contract(ctx, msg.Name, msg.Symbol, uint8(msg.Decimals), msg.ForeignChain, msg.CoinType, msg.ERC20, big.NewInt(int64(msg.GasLimit)))
-	if err != nil {
-		return nil, err
+		//FIXME : declare the attributes as constants , in x/fungible/types
+		ctx.EventManager().EmitEvent(
+			sdk.NewEvent(sdk.EventTypeMessage,
+				sdk.NewAttribute("action", "DeployFungibleCoinZRC20"),
+				sdk.NewAttribute("chain", msg.ForeignChain),
+				sdk.NewAttribute("contract", addr.String()),
+				sdk.NewAttribute("name", msg.Name),
+				sdk.NewAttribute("symbol", msg.Symbol),
+				sdk.NewAttribute("decimals", fmt.Sprintf("%d", msg.Decimals)),
+				sdk.NewAttribute("coinType", msg.CoinType.String()),
+				sdk.NewAttribute("erc20", msg.ERC20),
+				sdk.NewAttribute("gasLimit", fmt.Sprintf("%d", msg.GasLimit)),
+			),
+		)
 	}
-	//FIXME : declare the attributes as constants , in x/fungible/types
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(sdk.EventTypeMessage,
-			sdk.NewAttribute("action", "DeployFungibleCoinZRC20"),
-			sdk.NewAttribute("chain", msg.ForeignChain),
-			sdk.NewAttribute("contract", addr.String()),
-			sdk.NewAttribute("name", msg.Name),
-			sdk.NewAttribute("symbol", msg.Symbol),
-			sdk.NewAttribute("decimals", fmt.Sprintf("%d", msg.Decimals)),
-			sdk.NewAttribute("coinType", msg.CoinType.String()),
-			sdk.NewAttribute("erc20", msg.ERC20),
-			sdk.NewAttribute("gasLimit", fmt.Sprintf("%d", msg.GasLimit)),
-		),
-	)
 
 	return &types.MsgDeployFungibleCoinZRC20Response{}, nil
 }
