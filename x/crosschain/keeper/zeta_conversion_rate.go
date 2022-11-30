93499c7b41ae23569fde4ae7b48bd4203d7b8486
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-11-29 23:54:44 -0600

    add ConvertGasToZeta RPC call that uses zEVM uniswap v2 pool

--- x/crosschain/keeper/zeta_conversion_rate.go
+++ x/crosschain/keeper/zeta_conversion_rate.go
@@ -1,36 +1,40 @@
 package keeper
 
-//
-//func (k Keeper) ZetaConversionRate(ctx context.Context, request *types.QueryZetaConversionRateRequest) (*types.QueryZetaConversionRateResponse, error) {
-//	goCtx := sdk.UnwrapSDKContext(ctx)
-//	medianGasPrice, isFound := k.GetMedianGasPriceInUint(ctx, receiveChain)
-//	if !isFound {
-//		return sdkerrors.Wrap(types.ErrUnableToGetGasPrice, fmt.Sprintf(" chain %s | Identifiers : %s ", cctx.OutBoundTxParams.ReceiverChain, cctx.LogIdentifierForCCTX()))
-//	}
-//	cctx.OutBoundTxParams.OutBoundTxGasPrice = medianGasPrice.String()
-//	gasLimit := sdk.NewUint(cctx.OutBoundTxParams.OutBoundTxGasLimit)
-//
-//	outTxGasFee := gasLimit.Mul(medianGasPrice)
-//	recvChain, err := common.ParseChain(receiveChain)
-//	if err != nil {
-//		return sdkerrors.Wrap(err, "UpdatePrices: unable to parse chain")
-//	}
-//	chainID := config.Chains[recvChain.String()].ChainID
-//	zrc20, err := k.fungibleKeeper.QuerySystemContractGasCoinZRC4(ctx, chainID)
-//	if err != nil {
-//		return sdkerrors.Wrap(err, "UpdatePrices: unable to get system contract gas coin")
-//	}
-//	outTxGasFeeInZeta, err := k.fungibleKeeper.QueryUniswapv2RouterGetAmountsIn(ctx, outTxGasFee.BigInt(), zrc20)
-//	if err != nil {
-//		return sdkerrors.Wrap(err, "UpdatePrices: unable to QueryUniswapv2RouterGetAmountsIn")
-//	}
-//	feeInZeta := types.GetProtocolFee().Add(sdk.NewUintFromBigInt(outTxGasFeeInZeta))
-//
-//	// swap the outTxGasFeeInZeta portion of zeta to the real gas ZRC20 and burn it
-//	coins := sdk.NewCoins(sdk.NewCoin("azeta", sdk.NewIntFromBigInt(feeInZeta.BigInt())))
-//	err = k.bankKeeper.MintCoins(ctx, types.ModuleName, coins)
-//	if err != nil {
-//		return sdkerrors.Wrap(err, "UpdatePrices: unable to mint coins")
-//	}
-//
-//}
+import (
+	"context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/zeta-chain/zetacore/common"
+	"github.com/zeta-chain/zetacore/x/crosschain/types"
+	"github.com/zeta-chain/zetacore/zetaclient/config"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (k Keeper) ConvertGasToZeta(context context.Context, request *types.QueryConvertGasToZetaRequest) (*types.QueryConvertGasToZetaResponse, error) {
+	ctx := sdk.UnwrapSDKContext(context)
+	medianGasPrice, isFound := k.GetMedianGasPriceInUint(ctx, request.Chain)
+	if !isFound {
+		return nil, status.Error(codes.InvalidArgument, "invalid request: param chain")
+	}
+
+	gasLimit := sdk.NewUintFromString(request.GasLimit)
+	outTxGasFee := medianGasPrice.Mul(gasLimit)
+	recvChain, err := common.ParseChain(request.Chain)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request: param chain")
+	}
+	chainID := config.Chains[recvChain.String()].ChainID
+	zrc20, err := k.fungibleKeeper.QuerySystemContractGasCoinZRC4(ctx, chainID)
+	if err != nil {
+		return nil, status.Error(codes.NotFound, "zrc20 not found")
+	}
+	outTxGasFeeInZeta, err := k.fungibleKeeper.QueryUniswapv2RouterGetAmountsIn(ctx, outTxGasFee.BigInt(), zrc20)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "zQueryUniswapv2RouterGetAmountsIn failed")
+	}
+	feeInZeta := types.GetProtocolFee().Add(sdk.NewUintFromBigInt(outTxGasFeeInZeta))
+	return &types.QueryConvertGasToZetaResponse{
+		Zeta:            feeInZeta.String(),
+		ZetaBlockHeight: uint64(ctx.BlockHeight()),
+	}, nil
+}
