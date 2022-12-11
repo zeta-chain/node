5b194294ff48b02cc372a8a69d5eb26e2061208f
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2022-12-10 23:57:57 -0600

    cache rpcclient.Client as module global variable for TxSearch rpc

--- x/crosschain/keeper/grpc_zevm.go
+++ x/crosschain/keeper/grpc_zevm.go
@@ -2,7 +2,7 @@ package keeper
 
 import (
 	"context"
-
+	"fmt"
 	"github.com/zeta-chain/zetacore/x/crosschain/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -14,4 +14,32 @@ func (k Keeper) ZEVMGetTransactionReceipt(c context.Context, req *types.QueryZEV
 	}
 
 	return &types.QueryZEVMGetTransactionReceiptResponse{}, nil
+}
+
+func (k Keeper) ZEVMGetTransaction(c context.Context, req *types.QueryZEVMGetTransactionRequest) (*types.QueryZEVMGetTransactionResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	//_ := sdk.UnwrapSDKContext(c)
+	rpcclient := types.RPCClient
+	if rpcclient == nil {
+		return nil, status.Error(codes.Internal, "rpc client is not initialized")
+	}
+	query := fmt.Sprintf("ethereum_tx.ethereumTxHash='%s'", req.Hash)
+	res, err := rpcclient.TxSearch(c, query, false, nil, nil, "asc")
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	result := ""
+	hash := ""
+	if len(res.Txs) > 0 {
+		tx := res.Txs[0]
+		result = fmt.Sprintf("%x", tx.TxResult.Log)
+		hash = tx.Hash.String()
+	}
+	return &types.QueryZEVMGetTransactionResponse{
+		Value: fmt.Sprintf("%s", result),
+		Hash:  hash,
+	}, nil
 }
