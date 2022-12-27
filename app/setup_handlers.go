fd23ff22755bf09e3011dc388b250338778565fd
tanmay <tanmay.bhattacharya.smit@gmail.com>
2022-12-27 17:36:03 -0500

    add mock setup handler

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -1,3 +1,33 @@
 package app
 
-func SetupHandlers(_ *App) {}
+import (
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+const releaseVersion = "1.1.6-rc1"
+
+func SetupHandlers(app *App) {
+	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		app.Logger().Info("Running upgrade handler for " + releaseVersion)
+
+		return app.mm.RunMigrations(ctx, app.configurator, vm)
+	})
+
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(err)
+	}
+	if upgradeInfo.Name == releaseVersion && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		storeUpgrades := storetypes.StoreUpgrades{
+			// Added: []string{},
+		}
+		// Use upgrade store loader for the initial loading of all stores when app starts,
+		// it checks if version == upgradeHeight and applies store upgrades before loading the stores,
+		// so that new stores start with the correct version (the current height of chain),
+		// instead the default which is the latest version that store last committed i.e 0 for new stores.
+		app.SetStoreLoader(types.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
+	}
+}
