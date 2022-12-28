929a9b8a39570713299e9356285755ee645bd7e1
CharlieM <31941002+CharlieMc0@users.noreply.github.com>
2022-12-28 12:16:33 -0700

    testing workflows

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-const releaseVersion = "1.1.6"
+const releaseVersion = "cosmovisor-testing-1"
 
 func SetupHandlers(app *App) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm module.VersionMap) (module.VersionMap, error) {
