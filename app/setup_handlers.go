a2ba0ac10e966144ede96a1328c05eecbdee674b
CharlieM <31941002+CharlieMc0@users.noreply.github.com>
2022-12-27 19:16:38 -0700

    fixing var names

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-const releaseVersion = "1.1.6-rc1"
+const releaseVersion = "1.1.6"
 
 func SetupHandlers(app *App) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm module.VersionMap) (module.VersionMap, error) {
