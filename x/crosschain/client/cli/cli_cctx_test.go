fc0fef6715d008903ef10cdb16c97eaefaa18ba9
aldapp7 <aldappp7@gmail.com>
2022-12-29 22:21:20 +0400

    fix: fmt, small fix

--- x/crosschain/client/cli/cli_cctx_test.go
+++ x/crosschain/client/cli/cli_cctx_test.go
@@ -36,8 +36,8 @@ func networkWithSendObjects(t *testing.T, n int) (*network.Network, []*types.Cro
 			},
 			ZetaMint:  sdk.OneUint(),
 			ZetaBurnt: sdk.OneUint(),
-			ZetaFees:  sdk.OneUint()},
-			CoinType: 0,
+			ZetaFees:  sdk.OneUint(),
+			CoinType:  0},
 		)
 	}
 	buf, err := cfg.Codec.MarshalJSON(&state)
