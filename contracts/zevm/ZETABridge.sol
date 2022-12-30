b69515732270af3d6800a6a8fd451e83d11a1742
aldapp7 <aldappp7@gmail.com>
2022-12-30 05:09:31 +0400

    fix: fixes

--- contracts/zevm/ZETABridge.sol
+++ contracts/zevm/ZETABridge.sol
@@ -3,9 +3,13 @@
 pragma solidity 0.8.7;
 
 contract ZETABridge {
+    address public constant FUNGIBLE_MODULE_ADDRESS = payable(0x735b14BB79463307AAcBED86DAf3322B1e6226aB);
+
     event ZetaSent(bytes to, uint256 toChainID, uint256 value);
 
-    function sendZeta(bytes memory to, uint256 toChainID) payable public {
+    function sendZeta(bytes calldata to, uint256 toChainID) external payable {
+        (bool success, ) = FUNGIBLE_MODULE_ADDRESS.call{value: msg.value}("");
+        require(success, "ZETABridge: failed to transfer");
         emit ZetaSent(to, toChainID, msg.value);
     }
 }
