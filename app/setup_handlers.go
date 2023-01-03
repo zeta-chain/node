835134e78226eae0a52ef2d3f5e1941367417340
tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-01-03 14:20:24 -0500

    add comments for setindex

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -13,6 +13,7 @@ func SetupHandlers(app *App) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		app.Logger().Info("Running upgrade handler for " + releaseVersion)
 
+		// This needs to be set one time only , would be removed in all future upgrades
 		for m, mb := range app.mm.Modules {
 			vm[m] = mb.ConsensusVersion()
 		}
