decc10ffd1046530261d6212645d68089d05fcb5
tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-01-03 14:02:11 -0500

    remove commented code

--- standalone-network/upgrade-integration.sh
+++ standalone-network/upgrade-integration.sh
@@ -109,12 +109,12 @@ chmod +x $DAEMON_HOME/cosmovisor/upgrades/$UpgradeName/bin/zetacored
 
 cosmovisor start --home ~/.zetacored/ --p2p.laddr 0.0.0.0:27655  --grpc.address 0.0.0.0:9096 --grpc-web.address 0.0.0.0:9093 --address tcp://0.0.0.0:27659 --rpc.laddr tcp://127.0.0.1:26657 >> zetanode.log 2>&1  &
 
-#sleep 7
-#zetacored tx gov submit-proposal software-upgrade $UpgradeName --from zeta --deposit 100000000azeta --upgrade-height 6 --title $UpgradeName --description $UpgradeName --keyring-backend test --chain-id localnet_101-1 --yes
-#sleep 7
-#zetacored tx gov vote 1 yes --from zeta --keyring-backend test --chain-id localnet_101-1 --yes
-#clear
-#sleep 7
-#zetacored query gov proposal 1
+sleep 7
+zetacored tx gov submit-proposal software-upgrade $UpgradeName --from zeta --deposit 100000000azeta --upgrade-height 6 --title $UpgradeName --description $UpgradeName --keyring-backend test --chain-id localnet_101-1 --yes
+sleep 7
+zetacored tx gov vote 1 yes --from zeta --keyring-backend test --chain-id localnet_101-1 --yes
+clear
+sleep 7
+zetacored query gov proposal 1
 
 tail -f zetanode.log
