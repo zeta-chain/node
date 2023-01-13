abae65a862aa3695dada64eba9fc54c69c5e24e3
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2023-01-13 10:55:40 -0500

    test: cctx-creator.sh fix

--- standalone-network/cctx-creator.sh
+++ standalone-network/cctx-creator.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
+set -x
+
 zetacored tx observer add-observer 5 InBoundTx --from zeta --fees=40azeta --chain-id=localnet_101-1 --keyring-backend=test -y --broadcast-mode=block
 zetacored tx observer add-observer 5 OutBoundTx --from zeta --fees=40azeta --chain-id=localnet_101-1 --keyring-backend=test -y --broadcast-mode=block
-zetacored tx crosschain gas-price-voter 5 1000000000000 2100000000000000 1 --from=zeta --keyring-backend=test --yes --chain-id=localnet_101-1 --broadcast-mode=block --gas=auto --gas-adjustment=2 --gas-prices=0.1azeta
+zetacored tx crosschain gas-price-voter 5 10000000000 100 100 --from=zeta --keyring-backend=test --yes --chain-id=localnet_101-1 --broadcast-mode=block --gas=auto --gas-adjustment=2 --gas-prices=0.1azeta
 #zetacored tx crosschain zeta-conversion-rate-voter Goerli 1 1 --from=zeta --keyring-backend=test --yes --chain-id=localnet_101-1 --broadcast-mode=block --gas=auto --gas-adjustment=2 --gas-prices=0.1azeta
 zetacored tx crosschain nonce-voter Goerli 1  --from=zeta --keyring-backend=test --yes --chain-id=localnet_101-1 --broadcast-mode=block --gas=auto --gas-adjustment=2 --gas-prices=0.1azeta --broadcast-mode=block
 
 
 
-zetacored tx crosschain inbound-voter 0x96B05C238b99768F349135de0653b687f9c13fEE 5 0x96B05C238b99768F349135de0653b687f9c13fEE 5 1000000000000000000 0 message hash 100 1 --from=zeta --keyring-backend=test --yes --chain-id=localnet_101-1 --broadcast-mode=block --gas=auto --gas-adjustment=2 --gas-prices=0.1azeta
-
+zetacored tx crosschain inbound-voter 0x96B05C238b99768F349135de0653b687f9c13fEE 5 0x96B05C238b99768F349135de0653b687f9c13fEE 5 10000000000000000000 0 "" "0x19398991572a825894b34b904ac1e3692720895351466b5c9e6bb7ae1e21d680" 100 Zeta --from=zeta --keyring-backend=test --yes --chain-id=localnet_101-1 --broadcast-mode=block --gas=auto --gas-adjustment=2 --gas-prices=0.1azeta
 zetacored q crosschain list-cctx
 
 zetacored tx crosschain outbound-voter 0x18b11d2eccd0601e3c0ffaef981892d579e74ed7eb36e7ab159f5c410c829a57 hashout 1 0 0 5 1 1 --from=zeta --keyring-backend=test --yes --chain-id=localnet_101-1 --broadcast-mode=block --gas=auto --gas-adjustment=2 --gas-prices=0.1azeta
