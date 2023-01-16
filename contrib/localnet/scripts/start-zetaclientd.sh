5e0f18dcfcf1e88d99b37ee242d1918399e9eb0f
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2023-01-16 02:56:48 -0600

    add ETHDEV chain

--- contrib/localnet/scripts/start-zetaclientd.sh
+++ contrib/localnet/scripts/start-zetaclientd.sh
@@ -8,12 +8,12 @@ mv  /root/zetacored/zetacored_$node /root/.zetacored
 
 mv /root/tss/$HOSTNAME /root/.tss
 
-source /root/env.sh
 
 if [ $HOSTNAME == "zetaclient0" ]
 then
     rm ~/.tss/address_book.seed
-    TSSPATH=~/.tss zetaclientd -val val -log-console -enable-chains GOERLI \
+    export TSSPATH=~/.tss
+    exec zetaclientd -val val -log-console -enable-chains GOERLI \
       -pre-params ~/preParams.json  -zetacore-url zetacore0 \
       -chain-id athens_101-1
 else
@@ -21,7 +21,8 @@ else
   node="zetacore$num"
   SEED=$(curl --retry 10 --retry-delay 5 --retry-connrefused  -s zetaclient0:8123/p2p)
 
-  TSSPATH=~/.tss zetaclientd -val val -log-console -enable-chains GOERLI  \
+  export TSSPATH=~/.tss
+  exec zetaclientd -val val -log-console -enable-chains ETHDEV  \
     -peer /dns/zetaclient0/tcp/6668/p2p/$SEED \
     -pre-params ~/preParams.json -zetacore-url $node \
     -chain-id athens_101-1
