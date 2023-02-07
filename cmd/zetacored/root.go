49d195578623e3449102a431940234fa3e79eaa9
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2023-02-07 15:01:04 -0600

    zetacored:cmd: remove duplicate commands
    use ethermint server.AddCommands and remove the stock cosmos
    version. The ethermint version parses additional flags and
    start the Eth JSON-RPC servers.

--- cmd/zetacored/root.go
+++ cmd/zetacored/root.go
@@ -125,7 +125,8 @@ func initRootCmd(rootCmd *cobra.Command, encodingConfig appparams.EncodingConfig
 	}
 	ethermintserver.AddCommands(rootCmd, app.DefaultNodeHome, ac.newApp, ac.appExport, addModuleInitFlags)
 
-	server.AddCommands(rootCmd, app.DefaultNodeHome, ac.newApp, ac.createSimappAndExport, addModuleInitFlags)
+	// the ethermintserver one supercedes the sdk one
+	//server.AddCommands(rootCmd, app.DefaultNodeHome, ac.newApp, ac.createSimappAndExport, addModuleInitFlags)
 
 	// add keybase, auxiliary RPC, query, and tx child commands
 	rootCmd.AddCommand(
