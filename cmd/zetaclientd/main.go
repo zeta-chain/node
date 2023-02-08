14aeb0a264e701fbf233cc9434fcf61fd7437848
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-02-08 15:20:05 -0500

    fix genesis values

--- cmd/zetaclientd/main.go
+++ cmd/zetaclientd/main.go
@@ -117,7 +117,10 @@ func main() {
 	}
 
 	var peers addr.AddrList
+
 	if *peer != "" {
+		fmt.Println("trying to parse : ", *peer)
+		log.Info().Msg("trying to parse " + *peer)
 		address, err := maddr.NewMultiaddr(*peer)
 		if err != nil {
 			log.Error().Err(err).Msg("NewMultiaddr error")
