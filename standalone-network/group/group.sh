f4485ec39dfe59321e4314a38776f9d0619cdb86
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-02-13 20:48:13 -0500

    centralize admin keys to observer account

--- standalone-network/group/group.sh
+++ standalone-network/group/group.sh
@@ -2,7 +2,7 @@ zetacored tx  group create-group-with-policy zeta1syavy2npfyt9tcncdtsdzf7kny9lh7
 zetacored tx group submit-proposal proposal_group.json --from zeta --fees=40azeta --chain-id=localnet_101-1 --keyring-backend=test -y --broadcast-mode=block --exec=1
 zetacored tx group vote 1 zeta1syavy2npfyt9tcncdtsdzf7kny9lh777heefxk VOTE_OPTION_YES metadata --from zeta --fees=40azeta --chain-id=localnet_101-1 --keyring-backend=test -y --broadcast-mode=block
 zetacored tx group vote 1 zeta1l7hypmqk2yc334vc6vmdwzp5sdefygj2w5yj50 VOTE_OPTION_YES metadata --from mario --fees=40azeta --chain-id=localnet_101-1 --keyring-backend=test -y --broadcast-mode=block
-exit 0
 zetacored q group proposal 1
 zetacored tx group exec 1 --from zeta --fees=40azeta --chain-id=localnet_101-1 --keyring-backend=test -y --broadcast-mode=block
 
+#zetacored q group group-policy-info zeta1afk9zr2hn2jsac63h4hm60vl9z3e5u69gndzf7c99cqge3vzwjzsxn0x73
