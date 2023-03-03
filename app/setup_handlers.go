8a8ed9000016b546511fb37f7e416f3c3e910a37
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-03-03 15:55:45 -0500

    change to legacy proposal

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -12,6 +12,11 @@ const releaseVersion = "athens2-v1.1.20"
 func SetupHandlers(app *App) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		app.Logger().Info("Running upgrade handler for " + releaseVersion)
+		// This needs to be set one time only , would be removed in all future upgrades
+		for m, mb := range app.mm.Modules {
+			vm[m] = mb.ConsensusVersion()
+		}
+
 		return app.mm.RunMigrations(ctx, app.configurator, vm)
 	})
 
