b98bc227df9010aac51374a751006e7014e0f711
aldapp7 <112641026+aldapp7@users.noreply.github.com>
2023-03-15 20:26:56 +0400

    Ethermint RPC for Cosmos EVM transactions (#445)
    * zetacored:cmd: remove duplicate commands
    
    use ethermint server.AddCommands and remove the stock cosmos
    version. The ethermint version parses additional flags and
    start the Eth JSON-RPC servers.
    
    * copy ethermint 0.19.3 /server and /rpc folders here
    
    * rpc: redirect zetacored to use server & rpc packages in this repo
    
    * rpc/server: replace all evmos/ethermint/rpc or server package with in-repo ones
    
    * update go.mod
    
    * rpc: add log
    
    * feat: init ethermint rpc
    
    * fix: lint
    
    * add basic test facility for eth RPC
    
    * feat: special cosmos evm tx formatting for getTransactionByHash
    
    * fix: lint
    
    * fix: revert smoke
    
    * fix: fix smoke test
    
    * fix: ignore gosec
    
    * add rpctest with raw JSON-RPC instead of ethclient
    
    * more rpctest
    
    * fix linter complaint
    
    * rpctest: more tests; contract creation tx returns wrong address
    
    * make gosec happy
    
    * fix: fix contract address handling
    
    * fix: remove main
    
    * fix: fix lint
    
    * rpctest: add one more test of logs
    
    * evm: use cosmos txhash as the synthetic ethereum txhash if available
    
    also fill in the txhash field in the log event.
    
    ---------
    
    Co-authored-by: brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
    Co-authored-by: brewmaster012 <>

---
+++ contrib/rpctest/setup.sh
@@ -0,0 +1,2 @@
+zetacored tx crosschain gas-price-voter 1337 10000000000 100 100 --from=zeta --keyring-backend=test --yes --chain-id=localnet_101-1 --broadcast-mode=block --gas=auto --gas-adjustment=2 --gas-prices=0.1azeta --log_format=json
+#zetacored tx crosschain nonce-voter Goerli 1  --from=zeta --keyring-backend=test --yes --chain-id=localnet_101-1 --broadcast-mode=block --gas=auto --gas-adjustment=2 --gas-prices=0.1azeta --broadcast-mode=block
