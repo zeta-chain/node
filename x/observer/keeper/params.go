2b4ce8aafaa1e64ccae8748a07288f11a5d96f4e
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-03-29 14:20:27 -0400

    Add client params struct

--- x/observer/keeper/params.go
+++ x/observer/keeper/params.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/zeta-chain/zetacore/common"
 	"github.com/zeta-chain/zetacore/x/observer/types"
 )
 
@@ -14,4 +15,15 @@ func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
 // SetParams set the params
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.paramstore.SetParamSet(ctx, &params)
+}
+
+func (k Keeper) UpdateClientParamsForChain(ctx sdk.Context, chain common.Chain, newClientParams *types.ClientParams) error {
+	params := k.GetParams(ctx)
+	for _, p := range params.ObserverParams {
+		if p.Chain.IsEqual(chain) {
+			p.ClientParams = newClientParams
+		}
+	}
+	k.SetParams(ctx, params)
+	return nil
 }
