7ac6d2139389b8911a0eab95ab15081475752e11
brewmaster012 <>
2023-04-06 19:41:51 -0500

    regenerate ABIs and binary with strict version solc 0.8.7

--- contracts/zevm/SystemContract.go
+++ contracts/zevm/SystemContract.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // SystemContractMetaData contains all meta data concerning the SystemContract contract.
@@ -134,11 +135,11 @@ func NewSystemContractFilterer(address common.Address, filterer bind.ContractFil
 
 // bindSystemContract binds a generic wrapper to an already deployed contract.
 func bindSystemContract(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(SystemContractABI))
+	parsed, err := SystemContractMetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
--- contracts/zevm/ZRC20.go
+++ contracts/zevm/ZRC20.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // ZRC20MetaData contains all meta data concerning the ZRC20 contract.
@@ -134,11 +135,11 @@ func NewZRC20Filterer(address common.Address, filterer bind.ContractFilterer) (*
 
 // bindZRC20 binds a generic wrapper to an already deployed contract.
 func bindZRC20(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(ZRC20ABI))
+	parsed, err := ZRC20MetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
