8b973b9c7ae3034e32c05b9db8082477d5880b58
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-04-24 13:37:34 -0400

    add new handler

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -7,16 +7,16 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-const releaseVersion = "v0.0.1"
+const releaseVersion = "v0.3.0"
 
 func SetupHandlers(app *App) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		app.Logger().Info("Running upgrade handler for " + releaseVersion)
 
 		// This needs to be set one time only , would be removed in all future upgrades
-		for m, mb := range app.mm.Modules {
-			vm[m] = mb.ConsensusVersion()
-		}
+		//for m, mb := range app.mm.Modules {
+		//	vm[m] = mb.ConsensusVersion()
+		//}
 
 		return app.mm.RunMigrations(ctx, app.configurator, vm)
 	})
