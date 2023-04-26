6569f2cf8575afb03468e6b6f7159b959b39a77c
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2023-04-26 11:10:00 -0500

    ADD: zetaclient p2p diagnostic mode (#530)
    add a p2p diag mode in zetaclient
    
    ---------
    
    Co-authored-by: brewmaster012 <>
    Co-authored-by: Charlie <31941002+CharlieMc0@users.noreply.github.com>

--- cmd/zetaclientd/init.go
+++ cmd/zetaclientd/init.go
@@ -30,6 +30,8 @@ type initArguments struct {
 	authzHotkey   string
 	devMode       bool
 	level         int8
+
+	p2pDiagnostic bool
 }
 
 func init() {
@@ -45,6 +47,7 @@ func init() {
 	InitCmd.Flags().StringVar(&initArgs.authzHotkey, "hotkey", "", "hotkey for zetaclient this key is used for TSS and ZetaClient operations")
 	InitCmd.Flags().BoolVar(&initArgs.devMode, "dev", false, "dev mode: geth private network as goerli testnet")
 	InitCmd.Flags().Int8Var(&initArgs.level, "log-level", int8(zerolog.InfoLevel), "log level (0:debug, 1:info, 2:warn, 3:error, 4:fatal, 5:panic , 6: NoLevel , 7: Disable)")
+	InitCmd.Flags().BoolVar(&initArgs.p2pDiagnostic, "p2p-diagnostic", false, "enable p2p diagnostic")
 }
 
 func Initialize(_ *cobra.Command, _ []string) error {
@@ -64,6 +67,7 @@ func Initialize(_ *cobra.Command, _ []string) error {
 	configData.AuthzHotkey = initArgs.authzHotkey
 	configData.AuthzGranter = initArgs.authzGranter
 	configData.LogLevel = zerolog.Level(initArgs.level)
+	configData.P2PDiagnostic = initArgs.p2pDiagnostic
 
 	//Save config file
 	return config.Save(&configData, rootArgs.zetaCoreHome)
