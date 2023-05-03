9fde731cf7c1b472b751693dd5acf455ee85c852
brewmaster012 <>
2023-05-02 23:54:57 -0500

    fix smoketest after merge

--- contrib/localnet/scripts/start-zetaclientd-genesis.sh
+++ contrib/localnet/scripts/start-zetaclientd-genesis.sh
@@ -20,9 +20,9 @@ if [ $HOSTNAME == "zetaclient0" ]
 then
     rm ~/.tss/*
     export TSSPATH=~/.tss
-    zetaclientd init --enable-chains "goerli_localnet,btc_regtest" \
+    zetaclientd init  \
       --pre-params ~/preParams.json  --zetacore-url zetacore0 \
-      --chain-id athens_101-1 --dev --operator "$operatorAddress" --log-level 0 --hotkey=hotkey --keygen-block 15
+      --chain-id athens_101-1  --operator "$operatorAddress" --log-level 0 --hotkey=hotkey --keygen-block 25
     zetaclientd start
 else
   num=$(echo $HOSTNAME | tr -dc '0-9')
@@ -30,9 +30,9 @@ else
   SEED=$(curl --retry 10 --retry-delay 5 --retry-connrefused  -s zetaclient0:8123/p2p)
   rm ~/.tss/*
   export TSSPATH=~/.tss
-  zetaclientd init --enable-chains "goerli_localnet,btc_regtest"  \
+  zetaclientd init  \
     --peer /ip4/172.20.0.21/tcp/6668/p2p/$SEED \
     --pre-params ~/preParams.json --zetacore-url $node \
-    --chain-id athens_101-1 --dev --operator "$operatorAddress" --log-level 0 --hotkey=hotkey --keygen-block 15
+    --chain-id athens_101-1 --operator "$operatorAddress" --log-level 0 --hotkey=hotkey --keygen-block 25
   zetaclientd start
 fi
