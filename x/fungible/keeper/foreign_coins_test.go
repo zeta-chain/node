94bf527b74d3d16ce338ecd1e7868f6860dc680f
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2023-05-09 11:48:30 -0500

    FIX: authenticate connectorZEVM address in evm hooks PostTxProcessing (#563)
    This PR fixes ZEL-01, also
    
    introduces ConnectorZEVM address into the SystemContract state
    variable, which is used against EVM hooks to authenticate ZetaSent
    event from ConnectorZEVM.  refactor foreign coin state variable and
    use ZRC20 as its store key/index.  refactor so that the two paths
    (1. withdraw events initiated by zEVM EOA; 2. withdraw events
    initiated by foreign txs that calls zEVM ZRC20 via depositAndCall)
    share almost all the events processing logic.
    
    ---------
    
    Co-authored-by: brewmaster012 <>

--- x/fungible/keeper/foreign_coins_test.go
+++ x/fungible/keeper/foreign_coins_test.go
@@ -13,33 +13,9 @@ var _ = strconv.IntSize
 func createNForeignCoins(keeper *keeper.Keeper, ctx sdk.Context, n int) []types.ForeignCoins {
 	items := make([]types.ForeignCoins, n)
 	for i := range items {
-		items[i].Index = strconv.Itoa(i)
+		items[i].Zrc20ContractAddress = strconv.Itoa(i)
 
 		keeper.SetForeignCoins(ctx, items[i])
 	}
 	return items
 }
-
-//func TestForeignCoinsGet(t *testing.T) {
-//	keeper, ctx := keepertest.FungibleKeeper(t)
-//	items := createNForeignCoins(keeper, ctx, 10)
-//	for _, item := range items {
-//		rst, found := keeper.GetForeignCoins(ctx,
-//			item.Index,
-//		)
-//		require.True(t, found)
-//		require.Equal(t,
-//			nullify.Fill(&item),
-//			nullify.Fill(&rst),
-//		)
-//	}
-//}
-//
-//func TestForeignCoinsGetAll(t *testing.T) {
-//	keeper, ctx := keepertest.FungibleKeeper(t)
-//	items := createNForeignCoins(keeper, ctx, 10)
-//	require.ElementsMatch(t,
-//		nullify.Fill(items),
-//		nullify.Fill(keeper.GetAllForeignCoins(ctx)),
-//	)
-//}
