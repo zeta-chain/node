c667be397c2b6ee4b873eb139142b349966a0a12
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-05-12 15:06:50 -0400

    remove ip

--- standalone-network/run-zetaclient.sh
+++ standalone-network/run-zetaclient.sh
@@ -1,2 +1,2 @@
-zetaclientd init --chain-id localnet_101-1 --operator zeta1syavy2npfyt9tcncdtsdzf7kny9lh777heefxk --hotkey=zeta --public-ip=76.64.207.76
+zetaclientd init --chain-id localnet_101-1 --operator zeta1syavy2npfyt9tcncdtsdzf7kny9lh777heefxk --hotkey=zeta --public-ip=0.0.0.0
 zetaclientd start
