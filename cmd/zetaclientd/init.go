93f6f049b8cfdfbcdd1f3429cf83b00e4631ad7b
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-05-12 19:29:33 -0400

    Fix/myip config (#608)
    * add log params
    set MYIP using config
    
    * move zeta-client config to config/zetaclient_config
    
    * increment to v0.3.2 and remove klaytn

--- cmd/zetaclientd/init.go
+++ cmd/zetaclientd/init.go
@@ -18,6 +18,9 @@ var initArgs = initArguments{}
 
 type initArguments struct {
 	peer               string
+	publicIP           string
+	logFormat          string
+	logSampler         bool
 	preParamsPath      string
 	chainID            string
 	zetacoreURL        string
@@ -36,12 +39,15 @@ func init() {
 	RootCmd.AddCommand(VersionCmd)
 
 	InitCmd.Flags().StringVar(&initArgs.peer, "peer", "", "peer address, e.g. /dns/tss1/tcp/6668/ipfs/16Uiu2HAmACG5DtqmQsHtXg4G2sLS65ttv84e7MrL4kapkjfmhxAp")
-	InitCmd.Flags().StringVar(&initArgs.preParamsPath, "pre-params", "", "pre-params file path")
+	InitCmd.Flags().StringVar(&initArgs.publicIP, "public-ip", "", "public ip address")
+	InitCmd.Flags().StringVar(&initArgs.preParamsPath, "pre-params", "~/preParams.json", "pre-params file path")
 	InitCmd.Flags().StringVar(&initArgs.chainID, "chain-id", "athens_7001-1", "chain id")
 	InitCmd.Flags().StringVar(&initArgs.zetacoreURL, "zetacore-url", "127.0.0.1", "zetacore node URL")
 	InitCmd.Flags().StringVar(&initArgs.authzGranter, "operator", "", "granter for the authorization , this should be operator address")
-	InitCmd.Flags().StringVar(&initArgs.authzHotkey, "hotkey", "", "hotkey for zetaclient this key is used for TSS and ZetaClient operations")
+	InitCmd.Flags().StringVar(&initArgs.authzHotkey, "hotkey", "hotkey", "hotkey for zetaclient this key is used for TSS and ZetaClient operations")
 	InitCmd.Flags().Int8Var(&initArgs.level, "log-level", int8(zerolog.InfoLevel), "log level (0:debug, 1:info, 2:warn, 3:error, 4:fatal, 5:panic , 6: NoLevel , 7: Disable)")
+	InitCmd.Flags().StringVar(&initArgs.logFormat, "log-format", "json", "log format (json, test)")
+	InitCmd.Flags().BoolVar(&initArgs.logSampler, "log-sampler", false, "set to to true to turn on log sampling")
 	InitCmd.Flags().BoolVar(&initArgs.p2pDiagnostic, "p2p-diagnostic", false, "enable p2p diagnostic")
 	InitCmd.Flags().Uint64Var(&initArgs.p2pDiagnosticTicker, "p2p-diagnostic-ticker", 30, "p2p diagnostic ticker (default: 0 means no ticker)")
 	InitCmd.Flags().Uint64Var(&initArgs.configUpdateTicker, "config-update-ticker", 6, "config update ticker (default: 0 means no ticker)")
@@ -64,12 +70,15 @@ func Initialize(_ *cobra.Command, _ []string) error {
 
 	//Populate new struct with cli arguments
 	configData.Peer = initArgs.peer
+	configData.PublicIP = initArgs.publicIP
 	configData.PreParamsPath = initArgs.preParamsPath
 	configData.ChainID = initArgs.chainID
 	configData.ZetaCoreURL = initArgs.zetacoreURL
 	configData.AuthzHotkey = initArgs.authzHotkey
 	configData.AuthzGranter = initArgs.authzGranter
 	configData.LogLevel = zerolog.Level(initArgs.level)
+	configData.LogFormat = initArgs.logFormat
+	configData.LogSampler = initArgs.logSampler
 	configData.P2PDiagnostic = initArgs.p2pDiagnostic
 	configData.TssPath = initArgs.TssPath
 	configData.P2PDiagnosticTicker = initArgs.p2pDiagnosticTicker
