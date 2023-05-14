3cba34dc717a64d781d62124dfb77c6da1d522c9
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-05-13 23:34:54 -0400

    change btc watch utxo ticker to 1

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-const releaseVersion = "v0.3.2"
+const releaseVersion = "v0.3.3"
 
 func SetupHandlers(app *App) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm module.VersionMap) (module.VersionMap, error) {
