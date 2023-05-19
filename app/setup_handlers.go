9f7c446fc979e9f4854227242754f55a4db4edd0
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-05-19 10:54:16 -0400

    chore: increment handler to v1.0.5

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-const releaseVersion = "v1.0.4"
+const releaseVersion = "v1.0.5"
 
 func SetupHandlers(app *App) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm module.VersionMap) (module.VersionMap, error) {
