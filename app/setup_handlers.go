a5fc2507922fa046c1c8c7deb5a7f995bd0f0c20
Grant Zukel <grantzukel@Grants-MacBook-Pro.local>
2023-05-22 11:27:17 -0600

    updated for tagging

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-const releaseVersion = "v1.0.6"
+const releaseVersion = "v1.1.1"
 
 func SetupHandlers(app *App) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm module.VersionMap) (module.VersionMap, error) {
