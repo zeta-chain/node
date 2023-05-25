46841150d2f1c9ab766fcc849059d54e83fbda7b
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-05-25 12:20:50 -0400

    remove unnecessary check for req

--- x/crosschain/keeper/keeper_keygen.go
+++ x/crosschain/keeper/keeper_keygen.go
@@ -37,16 +37,11 @@ func (k Keeper) RemoveKeygen(ctx sdk.Context) {
 
 // Query
 
-func (k Keeper) Keygen(c context.Context, req *types.QueryGetKeygenRequest) (*types.QueryGetKeygenResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid request")
-	}
+func (k Keeper) Keygen(c context.Context, _ *types.QueryGetKeygenRequest) (*types.QueryGetKeygenResponse, error) {
 	ctx := sdk.UnwrapSDKContext(c)
-
 	val, found := k.GetKeygen(ctx)
 	if !found {
 		return nil, status.Error(codes.InvalidArgument, "not found")
 	}
-
 	return &types.QueryGetKeygenResponse{Keygen: &val}, nil
 }
