28c40a65e11ee459d5da1038d769f13f411a3c8b
Denis Fadeev <denis@fadeev.org>
2023-06-01 09:20:59 +0400

    fix: generate definitions in openapi spec

--- scripts/protoc-gen-openapi.sh
+++ scripts/protoc-gen-openapi.sh
@@ -13,8 +13,7 @@ ZETACHAIN_PROTO_PATH="proto/"
 OPENAPI_HEADER="swagger: '2.0'
 info:
   title: ZetaChain - gRPC Gateway docs
-  description: Documentation for the API of ZetaChain.
-paths:"
+  description: Documentation for the API of ZetaChain."
 
 # Get the directory path for the cosmos-sdk
 DIR_PATH=$(go list -f '{{ .Dir }}' -m $COSMOS_SDK)
@@ -25,14 +24,17 @@ COSMOS_OPENAPI_PATH=$(find "$DIR_PATH" -type f -name "*.yaml" -exec grep -q "swa
 # Generate OpenAPI YAML file using buf
 buf generate --template $PROTO_TEMPLATE --output=$OUTPUT_DIR $ZETACHAIN_PROTO_PATH
 
-# Initialize the merged swagger file
+# Initialize the merged swagger file with header info
 echo "$OPENAPI_HEADER" > $OUTPUT_DIR/$MERGED_SWAGGER_FILE
 
-# Extract paths from the single swagger file
-yq e '.paths' $COSMOS_OPENAPI_PATH | sed -e 's/^/  /' >> $OUTPUT_DIR/$MERGED_SWAGGER_FILE
-
-# Append the generated OpenAPI YAML file to the merged file
-yq e '.paths' $OUTPUT_DIR/$ZETACHAIN_OPENAPI | sed -e 's/^/  /' >> $OUTPUT_DIR/$MERGED_SWAGGER_FILE
+# Extract paths and definitions from the cosmos swagger file and ZetaChain swagger file
+# Merge them using yq and write to the merged swagger file
+{
+  echo "paths:"
+  yq ea 'select(fileIndex == 0).paths * select(fileIndex == 1).paths' $COSMOS_OPENAPI_PATH $OUTPUT_DIR/$ZETACHAIN_OPENAPI | sed -e 's/^/  /'
+  echo "definitions:"
+  yq ea 'select(fileIndex == 0).definitions * select(fileIndex == 1).definitions' $COSMOS_OPENAPI_PATH $OUTPUT_DIR/$ZETACHAIN_OPENAPI | sed -e 's/^/  /'
+} >> $OUTPUT_DIR/$MERGED_SWAGGER_FILE
 
 # Check if the merged swagger file was created successfully
 if [ -f $OUTPUT_DIR/$MERGED_SWAGGER_FILE ]; then
