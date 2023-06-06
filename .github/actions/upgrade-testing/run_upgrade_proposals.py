9470d663ad85397cb8ee3b2ef393837bd8ddbeab
Grant Zukel <grantzukel@Grants-MacBook-Pro.local>
2023-06-06 13:10:04 -0600

    updates

--- .github/actions/upgrade-testing/run_upgrade_proposals.py
+++ .github/actions/upgrade-testing/run_upgrade_proposals.py
@@ -52,8 +52,9 @@ non_concensus_upgrades = []
 #     binary_download_list.append([f"{tag}", f"zetacored-{os.environ['BINARY_NAME_SUFFIX']}"])
 
 binary_download_list = [["v1.2.0", "zetacored-ubuntu-22-amd64"],
-                        ["v1.2.4", "zetacored-ubuntu-22-amd64"]]
-tag_list = ["v1.2.0","v1.2.4"]
+                        ["v1.2.4", "zetacored-ubuntu-22-amd64"],
+                        ["v1.2.5", "zetacored-ubuntu-22-amd64"]]
+tag_list = ["v1.2.4","v1.2.5"]
 
 
 #os.environ["STARTING_VERSION"] = tag_list[0]
