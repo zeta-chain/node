6e407dd26cc28a1d02ab81b58963bace656c261a
Grant Zukel <grantzukel@Grants-MacBook-Pro.local>
2023-06-14 10:01:11 -0600

    updated logic to catch all conditions for non concensus

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-const releaseVersion = "v1.2.2"
+const releaseVersion = "v2.0.0"
 
 func SetupHandlers(app *App) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm module.VersionMap) (module.VersionMap, error) {
