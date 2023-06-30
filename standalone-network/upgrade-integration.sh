b72a336af155d0824f778e422975a1d1b1178be4
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-06-30 13:46:07 -0400

    test : add tss pubkey to upgrde testing script

--- standalone-network/upgrade-integration.sh
+++ standalone-network/upgrade-integration.sh
@@ -67,7 +67,7 @@ echo "hand inmate canvas head lunar naive increase recycle dog ecology inhale de
 
 
 
-zetacored add-observer-list standalone-network/observers.json --keygen-block=0
+zetacored add-observer-list standalone-network/observers.json --keygen-block=0 --tss-pubkey="tsspubkey"
 zetacored gentx zeta 1000000000000000000000azeta --chain-id=$CHAINID --keyring-backend=$KEYRING
 
 echo "Collecting genesis txs..."
