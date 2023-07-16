faaa417e6624f4fa01d96f1453cae3aea6e8c9ad
brewmaster012 <>
2023-07-16 14:08:27 -0500

    add one multiple bitcoin withdraw to testout the bitcoin outbound

--- contrib/localnet/orchestrator/smoketest/main.go
+++ contrib/localnet/orchestrator/smoketest/main.go
@@ -279,6 +279,7 @@ func LocalSmokeTest(_ *cobra.Command, _ []string) {
 	smokeTest.CheckZRC20ReserveAndSupply()
 
 	smokeTest.TestERC20Withdraw()
+	//smokeTest.WithdrawBitcoinMultipleTimes(5)
 	smokeTest.CheckZRC20ReserveAndSupply()
 
 	smokeTest.TestSendZetaOut()
