262f65e84cd393b6dd4be70103e123ac96d97239
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-07-20 11:43:46 -0400

    add modifying keygen to Begin Block

--- x/observer/abci.go
+++ x/observer/abci.go
@@ -14,19 +14,20 @@ func BeginBlocker(ctx sdk.Context, k keeper.Keeper) {
 	}
 
 	allObservers := k.GetAllObserverMappers(ctx)
-	totalObserverCount := 0
+	totalObserverCountCurrentBlock := 0
 	for _, observer := range allObservers {
-		totalObserverCount += len(observer.ObserverList)
+		totalObserverCountCurrentBlock += len(observer.ObserverList)
 	}
-	if len(allObservers) != int(lastBlockObserverCount.Count) {
+	if totalObserverCountCurrentBlock < 0 {
+		ctx.Logger().Error("TotalObserverCount is negative at height", ctx.BlockHeight())
+		return
+	}
+	if totalObserverCountCurrentBlock != int(lastBlockObserverCount.Count) {
 		ctx.Logger().Error("LastBlockObserverCount does not match the number of observers found at current height", ctx.BlockHeight())
 		k.SetPermissionFlags(ctx, types.PermissionFlags{IsInboundEnabled: false})
 		k.SetKeygen(ctx, types.Keygen{BlockNumber: math.MaxInt64})
 	}
-	if totalObserverCount < 0 {
-		ctx.Logger().Error("TotalObserverCount is negative at height", ctx.BlockHeight())
-		return
-	}
-	k.SetLastBlockObserverCount(ctx, &types.LastBlockObserverCount{Count: uint64(totalObserverCount)})
+
+	k.SetLastBlockObserverCount(ctx, &types.LastBlockObserverCount{Count: uint64(totalObserverCountCurrentBlock)})
 
 }
