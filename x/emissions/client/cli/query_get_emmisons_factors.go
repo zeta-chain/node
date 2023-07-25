db38fc5b4e0c5be6bcb4989fd25a0d95be796fcf
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-07-25 19:24:08 -0400

    add new query for emissions

---
+++ x/emissions/client/cli/query_get_emmisons_factors.go
@@ -0,0 +1,42 @@
+package cli
+
+import (
+	"strconv"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+	"github.com/zeta-chain/zetacore/x/emissions/types"
+)
+
+var _ = strconv.Itoa(0)
+
+func CmdGetEmmisonsFactors() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "get-emmisons-factors",
+		Short: "Query GetEmmisonsFactors",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryGetEmmisonsFactorsRequest{}
+
+			res, err := queryClient.GetEmmisonsFactors(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
