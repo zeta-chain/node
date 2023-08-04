9b45e7028ab270c08a2660e02bc97adb75b62f06
lumtis <lucas.bertrand.22@gmail.com>
2023-08-04 18:39:10 +0200

    refactor test keeper with mock generator

--- testutil/keeper/fungible.go
+++ testutil/keeper/fungible.go
@@ -1,23 +1,22 @@
 package keeper
 
 import (
+	"testing"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/store"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	authkeeper2 "github.com/cosmos/cosmos-sdk/x/auth/keeper"
-	bankkeeper2 "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 	typesparams "github.com/cosmos/cosmos-sdk/x/params/types"
 	evmkeeper "github.com/evmos/ethermint/x/evm/keeper"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmdb "github.com/tendermint/tm-db"
+	fungiblemocks "github.com/zeta-chain/zetacore/testutil/keeper/mocks/fungible"
 	"github.com/zeta-chain/zetacore/x/fungible/keeper"
 	"github.com/zeta-chain/zetacore/x/fungible/types"
-	zetaObserverModuleKeeper "github.com/zeta-chain/zetacore/x/observer/keeper"
-	"testing"
 )
 
 func FungibleKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
@@ -40,21 +39,25 @@ func FungibleKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 		"FungibleParams",
 	)
 
-	bankkeeper := bankkeeper2.BaseKeeper{}
-	authkeeper := authkeeper2.AccountKeeper{}
+	// Initialize mocks from expected keeper interfaces
+	bankKeeper := fungiblemocks.NewFungibleBankKeeper(t)
+	authKeeper := fungiblemocks.NewFungibleAccountKeeper(t)
+	observerKeeper := fungiblemocks.NewFungibleObserverKeeper(t)
+
+	// TODO: Use mock once interface is implemented
 	evmKeeper := evmkeeper.Keeper{}
-	zetaObserverKeeper := zetaObserverModuleKeeper.Keeper{}
-	keeper := keeper.NewKeeper(
+
+	k := keeper.NewKeeper(
 		codec.NewProtoCodec(registry),
 		storeKey,
 		memStoreKey,
 		paramsSubspace,
-		authkeeper,
+		authKeeper,
 		evmKeeper,
-		bankkeeper,
-		zetaObserverKeeper,
+		bankKeeper,
+		observerKeeper,
 	)
 
 	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
-	return keeper, ctx
+	return k, ctx
 }
