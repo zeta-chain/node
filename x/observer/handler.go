2be2a2ca37d10b37f9b7b2d4fbcf59d8200a71c8
lumtis <lucas.bertrand.22@gmail.com>
2023-08-04 10:48:03 +0200

    remove sp placeholders

--- x/observer/handler.go
+++ x/observer/handler.go
@@ -11,7 +11,6 @@ import (
 
 // NewHandler ...
 func NewHandler(k keeper.Keeper) sdk.Handler {
-	// this line is used by starport scaffolding # handler/msgServer
 	msgServer := keeper.NewMsgServerImpl(k)
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
@@ -26,7 +25,6 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 		case *types.MsgUpdatePermissionFlags:
 			res, err := msgServer.UpdatePermissionFlags(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
-			// this line is used by starport scaffolding # 1
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
