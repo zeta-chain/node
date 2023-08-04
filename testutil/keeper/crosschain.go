18afff403e64aeb82b4f52c40386457510f1d8f5
lumtis <lucas.bertrand.22@gmail.com>
2023-08-04 10:01:06 +0200

    fix crosschain test keeper

--- testutil/keeper/crosschain.go
+++ testutil/keeper/crosschain.go
@@ -1,9 +1,6 @@
 package keeper
 
 import (
-	authkeeper2 "github.com/cosmos/cosmos-sdk/x/auth/keeper"
-	bankkeeper2 "github.com/cosmos/cosmos-sdk/x/bank/keeper"
-	"github.com/zeta-chain/zetacore/x/crosschain/keeper"
 	"testing"
 
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -11,19 +8,22 @@ import (
 	"github.com/cosmos/cosmos-sdk/store"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmdb "github.com/tendermint/tm-db"
-	"github.com/zeta-chain/zetacore/x/crosschain/types"
-	fungibleKeeper "github.com/zeta-chain/zetacore/x/fungible/keeper"
-	zetaobserverKeeper "github.com/zeta-chain/zetacore/x/observer/keeper"
 
-	typesparams "github.com/cosmos/cosmos-sdk/x/params/types"
+	"github.com/zeta-chain/zetacore/x/crosschain/keeper"
+	"github.com/zeta-chain/zetacore/x/crosschain/types"
+	fungiblekeeper "github.com/zeta-chain/zetacore/x/fungible/keeper"
+	observerkeeper "github.com/zeta-chain/zetacore/x/observer/keeper"
 )
 
-func ZetacoreKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
+func CrosschainKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 	storeKey := sdk.NewKVStoreKey(types.StoreKey)
 	memStoreKey := storetypes.NewMemoryStoreKey(types.MemStoreKey)
 
@@ -36,17 +36,17 @@ func ZetacoreKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 	registry := codectypes.NewInterfaceRegistry()
 	cdc := codec.NewProtoCodec(registry)
 
-	paramsSubspace := typesparams.NewSubspace(cdc,
+	paramsSubspace := paramstypes.NewSubspace(cdc,
 		types.Amino,
 		storeKey,
 		memStoreKey,
 		"ZetacoreParams",
 	)
 
-	bankkeeper := bankkeeper2.BaseKeeper{}
-	authkeeper := authkeeper2.AccountKeeper{}
-	zetaobserverKeeper := zetaobserverKeeper.Keeper{}
-	fungibleKeeper := fungibleKeeper.Keeper{}
+	bankKeeper := bankkeeper.BaseKeeper{}
+	authKeeper := authkeeper.AccountKeeper{}
+	observerKeeper := observerkeeper.Keeper{}
+	fungibleKeeper := fungiblekeeper.Keeper{}
 
 	k := keeper.NewKeeper(
 		codec.NewProtoCodec(registry),
@@ -54,10 +54,10 @@ func ZetacoreKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 		memStoreKey,
 		stakingkeeper.Keeper{}, // custom
 		paramsSubspace,
-		authkeeper,
-		bankkeeper,
-		zetaobserverKeeper,
-		fungibleKeeper,
+		authKeeper,
+		bankKeeper,
+		observerKeeper,
+		&fungibleKeeper,
 	)
 
 	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
