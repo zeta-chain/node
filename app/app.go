c1e93e8336e64e76d41b9d593612f323be2e7073
lumtis <lucas.bertrand.22@gmail.com>
2023-08-07 11:21:32 +0200

    remove comment

--- app/app.go
+++ app/app.go
@@ -580,16 +580,6 @@ func New(
 		if err := app.LoadLatestVersion(); err != nil {
 			tmos.Exit(err.Error())
 		}
-
-		// Initialize and seal the capability keeper so all persistent capabilities
-		// are loaded in-memory and prevent any further modules from creating scoped
-		// sub-keepers.
-		// This must be done during creation of baseapp rather than in InitChain so
-		// that in-memory capabilities get regenerated on app restart.
-		// Note that since this reads from the store, we can only perform it when
-		// `loadLatest` is set to true.
-		//ctx := app.BaseApp.NewUncachedContext(true, tmproto.Header{})
-		//app.CapabilityKeeper.InitializeAndSeal(ctx)
 	}
 
 	return app
@@ -758,7 +748,7 @@ func initParamsKeeper(appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino
 	return paramsKeeper
 }
 
-// VerifyAddressFormat verifis the address is compatible with ethereum
+// VerifyAddressFormat verifies the address is compatible with ethereum
 func VerifyAddressFormat(bz []byte) error {
 	if len(bz) == 0 {
 		return sdkerrors.Wrap(sdkerrors.ErrUnknownAddress, "invalid address; cannot be empty")
