f901421b1c618582c21b098181230ad7753c8890
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2023-08-08 17:51:36 -0500

    add smoketest for this

--- x/fungible/module.go
+++ x/fungible/module.go
@@ -179,11 +179,19 @@ func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
 		}
 	}
 	if ctx.BlockHeight() == 20 {
-		err := am.keeper.UpdateSystemContractAddress(sdk.WrapSDKContext(ctx))
+		err := am.keeper.TestUpdateSystemContractAddress(sdk.WrapSDKContext(ctx))
 		if err != nil {
 			ctx.Logger().Error("Unable To update system contracts", "err", err.Error())
 		} else {
 			ctx.Logger().Info("System contract updated")
+		}
+	}
+	if ctx.BlockHeight() == 25 {
+		err := am.keeper.TestUpdateZRC20WithdrawFee(sdk.WrapSDKContext(ctx))
+		if err != nil {
+			ctx.Logger().Error("Unable To update zrc20 withdraw fee", "err", err.Error())
+		} else {
+			ctx.Logger().Info("zrc20 withdraw fee updated")
 		}
 	}
 }
