460b64fa4398ed8395304b788beb03d70d40a470
lumtis <lucas.bertrand.22@gmail.com>
2023-08-11 10:43:46 +0200

    types coverage

---
+++ x/fungible/types/message_update_system_contract_test.go
@@ -0,0 +1,50 @@
+package types
+
+import (
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/stretchr/testify/require"
+	"github.com/zeta-chain/zetacore/testutil/sample"
+	"testing"
+)
+
+func TestMsgUpdateSystemContract_ValidateBasic(t *testing.T) {
+	tests := []struct {
+		name string
+		msg  MsgUpdateSystemContract
+		err  error
+	}{
+		{
+			name: "invalid address",
+			msg: MsgUpdateSystemContract{
+				Creator:                  "invalid_address",
+				NewSystemContractAddress: sample.EthAddress().String(),
+			},
+			err: sdkerrors.ErrInvalidAddress,
+		},
+		{
+			name: "invalid new system contract address",
+			msg: MsgUpdateSystemContract{
+				Creator:                  sample.AccAddress(),
+				NewSystemContractAddress: "invalid_address",
+			},
+			err: sdkerrors.ErrInvalidAddress,
+		},
+		{
+			name: "valid message",
+			msg: MsgUpdateSystemContract{
+				Creator:                  sample.AccAddress(),
+				NewSystemContractAddress: sample.EthAddress().String(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.msg.ValidateBasic()
+			if tt.err != nil {
+				require.ErrorIs(t, err, tt.err)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+}
---
+++ x/fungible/types/message_update_zrc20_withdraw_fee_test.go
@@ -0,0 +1,62 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/stretchr/testify/require"
+	"github.com/zeta-chain/zetacore/testutil/sample"
+	"testing"
+)
+
+func TestMsgUpdateZRC20WithdrawFee_ValidateBasic(t *testing.T) {
+	tests := []struct {
+		name string
+		msg  MsgUpdateZRC20WithdrawFee
+		err  error
+	}{
+		{
+			name: "invalid address",
+			msg: MsgUpdateZRC20WithdrawFee{
+				Creator:        "invalid_address",
+				Zrc20Address:   sample.EthAddress().String(),
+				NewWithdrawFee: sdk.NewUint(1),
+			},
+			err: sdkerrors.ErrInvalidAddress,
+		},
+		{
+			name: "invalid new system contract address",
+			msg: MsgUpdateZRC20WithdrawFee{
+				Creator:        sample.AccAddress(),
+				Zrc20Address:   "invalid_address",
+				NewWithdrawFee: sdk.NewUint(1),
+			},
+			err: sdkerrors.ErrInvalidAddress,
+		},
+		{
+			name: "invalid new withdraw fee",
+			msg: MsgUpdateZRC20WithdrawFee{
+				Creator:      sample.AccAddress(),
+				Zrc20Address: sample.EthAddress().String(),
+			},
+			err: sdkerrors.ErrInvalidRequest,
+		},
+		{
+			name: "valid message",
+			msg: MsgUpdateZRC20WithdrawFee{
+				Creator:        sample.AccAddress(),
+				Zrc20Address:   sample.EthAddress().String(),
+				NewWithdrawFee: sdk.NewUint(1),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.msg.ValidateBasic()
+			if tt.err != nil {
+				require.ErrorIs(t, err, tt.err)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+}
