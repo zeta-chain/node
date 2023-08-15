3bebb503c07b735a9fb2ab167d0f101a9f673fd8
brewmaster012 <>
2023-08-15 13:31:50 -0500

    fix command name

--- common/commands.go
+++ common/commands.go
@@ -3,5 +3,5 @@ package common
 // commands for the zetaclient; mostly administrative commands/txs
 
 const (
-	CmdWhitelistERC20 = "cmd_whitelist_zrc20"
+	CmdWhitelistERC20 = "cmd_whitelist_erc20"
 )
