8cc612b1c36fc281ba544131aa0da39b6ae39c80
brewmaster012 <>
2023-08-18 10:54:21 -0500

    fix

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -8,7 +8,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
-const releaseVersion = "v8.0.0"
+const releaseVersion = "v8.1.0"
 
 func SetupHandlers(app *App) {
 	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm module.VersionMap) (module.VersionMap, error) {
