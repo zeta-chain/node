52ab64119d7635c34057ab486455b56ac6105c1c
lumtis <lucas.bertrand.22@gmail.com>
2023-08-22 17:51:35 +0200

    remove zeta check

--- x/crosschain/keeper/keeper_cross_chain_tx_vote_outbound_tx.go
+++ x/crosschain/keeper/keeper_cross_chain_tx_vote_outbound_tx.go
@@ -107,9 +107,9 @@ func (k msgServer) VoteOnObservedOutboundTx(goCtx context.Context, msg *types.Ms
 	}
 	if ballot.BallotStatus != observerTypes.BallotStatus_BallotFinalized_FailureObservation {
 		if !msg.ZetaMinted.Equal(cctx.GetCurrentOutTxParam().Amount) {
-			log.Error().Msgf("ReceiveConfirmation: Mint mismatch: %s zeta minted vs %s cctx amount, cctx: %s",
+			log.Error().Msgf("ReceiveConfirmation: Mint mismatch: %s zeta minted vs %s cctx amount",
 				msg.ZetaMinted,
-				cctx.GetCurrentOutTxParam().Amount, cctx.String())
+				cctx.GetCurrentOutTxParam().Amount)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, fmt.Sprintf("ZetaMinted %s does not match send ZetaMint %s", msg.ZetaMinted, cctx.GetCurrentOutTxParam().Amount))
 		}
 	}
--- x/crosschain/keeper/keeper_utils.go
+++ x/crosschain/keeper/keeper_utils.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"math/big"
 
-	"github.com/zeta-chain/zetacore/common"
-
 	"github.com/zeta-chain/zetacore/cmd/zetacored/config"
 
 	"cosmossdk.io/math"
@@ -29,9 +27,9 @@ func (k Keeper) IsAuthorizedNodeAccount(ctx sdk.Context, address string) bool {
 // The gas ZRC20 balance is subsequently burned to account for the expense of TSS address gas fee payment in the outbound tx.
 // **Caller should feed temporary ctx into this function**
 func (k Keeper) PayGasInZetaAndUpdateCctx(ctx sdk.Context, chainID int64, cctx *types.CrossChainTx, noEthereumTxEvent bool) error {
-	if cctx.InboundTxParams.CoinType != common.CoinType_Zeta {
-		return sdkerrors.Wrapf(zetaObserverTypes.ErrInvalidCoinType, "can't pay gas in zeta with %s", cctx.InboundTxParams.CoinType.String())
-	}
+	//if cctx.InboundTxParams.CoinType != common.CoinType_Zeta {
+	//	return sdkerrors.Wrapf(zetaObserverTypes.ErrInvalidCoinType, "can't pay gas in zeta with %s", cctx.InboundTxParams.CoinType.String())
+	//}
 
 	chain := k.zetaObserverKeeper.GetParams(ctx).GetChainFromChainID(chainID)
 	if chain == nil {
