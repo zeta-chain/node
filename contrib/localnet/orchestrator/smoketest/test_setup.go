040adbabe6c8a3e947048cccd88702e18a2700e2
kevinssgh <kevinssgh.github@gmail.com>
2023-08-29 13:21:27 -0400

    fixed TestSetupZetaTokenAndConnectorAndZEVMContracts

--- contrib/localnet/orchestrator/smoketest/test_setup.go
+++ contrib/localnet/orchestrator/smoketest/test_setup.go
@@ -45,7 +45,22 @@ func (sm *SmokeTest) TestSetupZetaTokenAndConnectorAndZEVMContracts() {
 	}
 
 	LoudPrintf("Deploy ZetaETH ConnectorETH ERC20Custody USDT\n")
-	if err := CheckNonce(goerliClient, DeployerAddress, 0); err != nil {
+
+	initialNonce, err := goerliClient.PendingNonceAt(context.Background(), DeployerAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	contractsDeployed := false
+	res, err := sm.fungibleClient.ForeignCoinsAll(context.Background(), &fungibletypes.QueryAllForeignCoinsRequest{})
+	if err != nil {
+		panic(err)
+	}
+	if len(res.ForeignCoins) > 0 {
+		contractsDeployed = true
+	}
+
+	if err := CheckNonce(goerliClient, DeployerAddress, initialNonce); err != nil {
 		panic(err)
 	}
 	zetaEthAddr, tx, ZetaEth, err := zetaeth.DeployZetaEth(auth, goerliClient, DeployerAddress, big.NewInt(21_000_000_000))
@@ -58,7 +73,7 @@ func (sm *SmokeTest) TestSetupZetaTokenAndConnectorAndZEVMContracts() {
 	sm.ZetaEth = ZetaEth
 	sm.ZetaEthAddr = zetaEthAddr
 
-	if err := CheckNonce(goerliClient, DeployerAddress, 1); err != nil {
+	if err := CheckNonce(goerliClient, DeployerAddress, initialNonce+1); err != nil {
 		panic(err)
 	}
 	connectorEthAddr, tx, ConnectorEth, err := zetaconnectoreth.DeployZetaConnectorEth(auth, goerliClient, zetaEthAddr,
@@ -74,7 +89,7 @@ func (sm *SmokeTest) TestSetupZetaTokenAndConnectorAndZEVMContracts() {
 
 	fungibleClient := sm.fungibleClient
 	fmt.Printf("Deploying ERC20Custody contract\n")
-	if err := CheckNonce(goerliClient, DeployerAddress, 2); err != nil {
+	if err := CheckNonce(goerliClient, DeployerAddress, initialNonce+2); err != nil {
 		panic(err)
 	}
 	erc20CustodyAddr, tx, ERC20Custody, err := erc20custody.DeployERC20Custody(auth, goerliClient, DeployerAddress, DeployerAddress, big.NewInt(0), big.NewInt(1e18), ethcommon.HexToAddress("0x"))
@@ -84,14 +99,17 @@ func (sm *SmokeTest) TestSetupZetaTokenAndConnectorAndZEVMContracts() {
 	fmt.Printf("ERC20Custody contract address: %s, tx hash: %s\n", erc20CustodyAddr.Hex(), tx.Hash().Hex())
 	receipt = MustWaitForTxReceipt(goerliClient, tx)
 	fmt.Printf("ERC20Custody contract receipt: contract address %s, status %d\n", receipt.ContractAddress, receipt.Status)
-	if erc20CustodyAddr != ethcommon.HexToAddress(ERC20CustodyAddr) {
-		panic("ERC20Custody contract address mismatch! check order of tx")
+	if !contractsDeployed {
+		if erc20CustodyAddr != ethcommon.HexToAddress(ERC20CustodyAddr) {
+			panic("ERC20Custody contract address mismatch! check order of tx")
+		}
 	}
+
 	sm.ERC20CustodyAddr = erc20CustodyAddr
 	sm.ERC20Custody = ERC20Custody
 
 	fmt.Printf("Deploying USDT contract\n")
-	if err := CheckNonce(goerliClient, DeployerAddress, 3); err != nil {
+	if err := CheckNonce(goerliClient, DeployerAddress, initialNonce+3); err != nil {
 		panic(err)
 	}
 	usdtAddr, tx, _, err := erc20.DeployUSDT(auth, goerliClient, "USDT", "USDT", 6)
@@ -101,9 +119,13 @@ func (sm *SmokeTest) TestSetupZetaTokenAndConnectorAndZEVMContracts() {
 	fmt.Printf("USDT contract address: %s, tx hash: %s\n", usdtAddr.Hex(), tx.Hash().Hex())
 	receipt = MustWaitForTxReceipt(goerliClient, tx)
 	fmt.Printf("USDT contract receipt: contract address %s, status %d\n", receipt.ContractAddress, receipt.Status)
-	if receipt.ContractAddress != ethcommon.HexToAddress(USDTERC20Addr) {
-		panic("USDT contract address mismatch! check order of tx")
+
+	if !contractsDeployed {
+		if receipt.ContractAddress != ethcommon.HexToAddress(USDTERC20Addr) {
+			panic("USDT contract address mismatch! check order of tx")
+		}
 	}
+
 	fmt.Printf("Step 6: Whitelist USDT\n")
 	tx, err = ERC20Custody.Whitelist(auth, usdtAddr)
 	if err != nil {
@@ -121,14 +143,14 @@ func (sm *SmokeTest) TestSetupZetaTokenAndConnectorAndZEVMContracts() {
 	fmt.Printf("TSS set receipt tx hash: %s\n", tx.Hash().Hex())
 
 	fmt.Printf("Checking foreign coins...\n")
-	res, err := fungibleClient.ForeignCoinsAll(context.Background(), &fungibletypes.QueryAllForeignCoinsRequest{})
+	res, err = fungibleClient.ForeignCoinsAll(context.Background(), &fungibletypes.QueryAllForeignCoinsRequest{})
 	if err != nil {
 		panic(err)
 	}
 	found := false
 	zrc20addr := ""
 	for _, fcoin := range res.ForeignCoins {
-		if ethcommon.HexToAddress(fcoin.Asset) == usdtAddr {
+		if fcoin.Asset == USDTERC20Addr {
 			found = true
 			zrc20addr = fcoin.Zrc20ContractAddress
 		}
@@ -138,9 +160,12 @@ func (sm *SmokeTest) TestSetupZetaTokenAndConnectorAndZEVMContracts() {
 		panic(fmt.Sprintf("fungible module does not have foreign coin that represent USDT ERC20 %s", usdtAddr))
 	}
 	fmt.Printf("USDT ZRC20 Address: %s\n", zrc20addr)
-	if HexToAddress(zrc20addr) != HexToAddress(USDTZRC20Addr) {
-		panic("mismatch of foreign coin USDT ZRC20 and the USDTZRC20Addr constant in smoketest")
+	if !contractsDeployed {
+		if HexToAddress(zrc20addr) != HexToAddress(USDTZRC20Addr) {
+			panic("mismatch of foreign coin USDT ZRC20 and the USDTZRC20Addr constant in smoketest")
+		}
 	}
+
 	sm.USDTZRC20Addr = ethcommon.HexToAddress(zrc20addr)
 	sm.USDTZRC20, err = zrc20.NewZRC20(sm.USDTZRC20Addr, sm.zevmClient)
 	if err != nil {
