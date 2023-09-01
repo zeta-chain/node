780c0459c8887150f1779f4915fc26db5b82cce6
lumtis <lucas.bertrand.22@gmail.com>
2023-09-01 19:18:40 +0200

    make generate

--- x/crosschain/keeper/keeper_cross_chain_tx_vote_outbound_tx.go
+++ x/crosschain/keeper/keeper_cross_chain_tx_vote_outbound_tx.go
@@ -2,8 +2,9 @@ package keeper
 
 import (
 	"context"
-	"cosmossdk.io/math"
 	"fmt"
+
+	"cosmossdk.io/math"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
