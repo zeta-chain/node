ea2e499680467ac1f58e1a214eebdfa7c07c688c
lumtis <lucas.bertrand.22@gmail.com>
2023-09-05 17:19:26 +0200

    typo

--- x/fungible/keeper/zeta.go
+++ x/fungible/keeper/zeta.go
@@ -9,7 +9,7 @@ import (
 )
 
 // MintZetaToEVMAccount mints ZETA (gas token) to the given address
-// NOTE: this method should be used with a temporary context, and it should not be committed if the method returns an erorr
+// NOTE: this method should be used with a temporary context, and it should not be committed if the method returns an error
 func (k *Keeper) MintZetaToEVMAccount(ctx sdk.Context, to sdk.AccAddress, amount *big.Int) error {
 	coins := sdk.NewCoins(sdk.NewCoin(config.BaseDenom, sdk.NewIntFromBigInt(amount)))
 	// Mint coins
