a55268d37696713e9e89c18e74672c7393596fe0
lumtis <lucas.bertrand.22@gmail.com>
2023-09-05 13:51:36 +0200

    fix lint part 2

--- x/emissions/module.go
+++ x/emissions/module.go
@@ -126,7 +126,7 @@ func (am AppModule) Route() sdk.Route {
 func (AppModule) QuerierRoute() string { return types.QuerierRoute }
 
 // LegacyQuerierHandler returns the emissions module's Querier.
-func (am AppModule) LegacyQuerierHandler(legacyQuerierCdc *codec.LegacyAmino) sdk.Querier {
+func (am AppModule) LegacyQuerierHandler(_ *codec.LegacyAmino) sdk.Querier {
 	return nil
 }
 
