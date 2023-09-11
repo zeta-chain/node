2b9d05a400513002cdc8641a65a47d398a8d30b1
lumtis <lucas.bertrand.22@gmail.com>
2023-09-11 11:05:40 +0200

    goimports

--- rpc/namespaces/ethereum/eth/api.go
+++ rpc/namespaces/ethereum/eth/api.go
@@ -17,18 +17,15 @@ package eth
 
 import (
 	"context"
-	"github.com/ethereum/go-ethereum/rpc"
-
-	"github.com/tendermint/tendermint/libs/log"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
-
-	"github.com/zeta-chain/zetacore/rpc/backend"
-
+	"github.com/ethereum/go-ethereum/rpc"
 	ethermint "github.com/evmos/ethermint/types"
 	evmtypes "github.com/evmos/ethermint/x/evm/types"
+	"github.com/tendermint/tendermint/libs/log"
+	"github.com/zeta-chain/zetacore/rpc/backend"
 	rpctypes "github.com/zeta-chain/zetacore/rpc/types"
 )
 
