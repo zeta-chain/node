75a027336254082a3e57a20cd68c9b6163dd4c70
Grant Zukel <grant.zukel@gmail.com>
2023-09-14 16:14:33 -0600

    updates

--- .github/actions/upgrade-testing/scripts/raise_gov_proposal.py
+++ .github/actions/upgrade-testing/scripts/raise_gov_proposal.py
@@ -3,6 +3,10 @@ import requests
 os.environ['NODE'] = "http://127.0.0.1:26657"
 CURRENT_HEIGHT = requests.get(f"{os.environ['NODE']}/status").json()["result"]["sync_info"]["latest_block_height"]
 UPGRADE_HEIGHT = int(CURRENT_HEIGHT) + (int(os.environ['PROPOSAL_TIME_SECONDS']) / int(os.environ['BLOCK_TIME_SECONDS']))
+github_file = open(os.environ["GITHUB_ENV"], "a+")
+github_file.write(f"UPGRADE_HEIGHT={UPGRADE_HEIGHT}")
+github_file.close()
+
 GOV_PROPOSAL = f"""zetacored tx gov submit-legacy-proposal software-upgrade "{os.environ['VERSION']}" \
     --from "{os.environ['MONIKER']}" \
     --deposit {os.environ["DEPOSIT"]} \
