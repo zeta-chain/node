5a40c3f3ca2ce0a02bee45113eb1e3883c10368d
Grant Zukel <grant.zukel@gmail.com>
2023-09-18 08:47:32 -0600

    updates

--- .github/actions/upgrade-testing/scripts/raise_gov_proposal.py
+++ .github/actions/upgrade-testing/scripts/raise_gov_proposal.py
@@ -2,7 +2,7 @@ import os
 import requests
 os.environ['NODE'] = "http://127.0.0.1:26657"
 CURRENT_HEIGHT = requests.get(f"{os.environ['NODE']}/status").json()["result"]["sync_info"]["latest_block_height"]
-UPGRADE_HEIGHT = int(CURRENT_HEIGHT) + (int(os.environ['PROPOSAL_TIME_SECONDS']) / int(os.environ['BLOCK_TIME_SECONDS']))
+UPGRADE_HEIGHT = int(CURRENT_HEIGHT) + (int(os.environ['PROPOSAL_TIME_SECONDS']) / int(os.environ['BLOCK_TIME_SECONDS'])) + 50
 github_file = open(os.environ["GITHUB_ENV"], "a+")
 github_file.write(f"UPGRADE_HEIGHT={UPGRADE_HEIGHT}")
 github_file.close()
