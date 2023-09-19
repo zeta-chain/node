a56427fdccf5cae3c39caa463f7ff622266d8493
lumtis <lucas.bertrand.22@gmail.com>
2023-09-19 09:17:01 +0200

    complete smoke test

--- contrib/localnet/orchestrator/smoketest/test_erc20_refund.go
+++ contrib/localnet/orchestrator/smoketest/test_erc20_refund.go
@@ -4,8 +4,11 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	ethcommon "github.com/ethereum/go-ethereum/common"
 	"math/big"
 	"time"
 
@@ -22,14 +25,14 @@ func (sm *SmokeTest) TestERC20DepositAndCallRefund() {
 	// TODO: uncomment once erc20 deposit contains the sender address
 	//https://github.com/zeta-chain/node/issues/1140
 	//// Get the initial balance of the deployer
-	//initialBal, err := sm.USDTZRC20.BalanceOf(&bind.CallOpts{}, DeployerAddress)
-	//if err != nil {
-	//	panic(err)
-	//}
+	initialBal, err := sm.USDTZRC20.BalanceOf(&bind.CallOpts{}, DeployerAddress)
+	if err != nil {
+		panic(err)
+	}
 
 	fmt.Println("Sending a deposit that should revert without a liquidity pool makes the cctx aborted")
 
-	amount := big.NewInt(1e7)
+	amount := big.NewInt(1e4)
 
 	// send the deposit
 	inTxHash, err := sm.sendInvalidUSDTDeposit(amount)
@@ -46,14 +49,21 @@ func (sm *SmokeTest) TestERC20DepositAndCallRefund() {
 	// TODO: uncomment once erc20 deposit contains the sender address
 	//https://github.com/zeta-chain/node/issues/1140
 	//// Check that the erc20 in the aborted cctx was refunded on ZetaChain
-	//newBalance, err := sm.USDTZRC20.BalanceOf(&bind.CallOpts{}, DeployerAddress)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//expectedBalance := initialBal.Add(initialBal, amount)
-	//if newBalance.Cmp(expectedBalance) != 0 {
-	//	panic(fmt.Sprintf("expected balance to be %s after refund; got %s", expectedBalance.String(), newBalance.String()))
-	//}
+	newBalance, err := sm.USDTZRC20.BalanceOf(&bind.CallOpts{}, DeployerAddress)
+	if err != nil {
+		panic(err)
+	}
+	expectedBalance := initialBal.Add(initialBal, amount)
+	if newBalance.Cmp(expectedBalance) != 0 {
+		panic(fmt.Sprintf("expected balance to be %s after refund; got %s", expectedBalance.String(), newBalance.String()))
+	}
+	fmt.Println("CCTX has been aborted and the erc20 has been refunded on ZetaChain")
+
+	amount = big.NewInt(1e7)
+	goerliBalance, err := sm.USDTERC20.BalanceOf(&bind.CallOpts{}, DeployerAddress)
+	if err != nil {
+		panic(err)
+	}
 
 	fmt.Println("Sending a deposit that should revert with a liquidity pool")
 
@@ -70,33 +80,49 @@ func (sm *SmokeTest) TestERC20DepositAndCallRefund() {
 		panic(err)
 	}
 
-	// There is a liquidity pool, therefore the cctx should revert
+	// there is a liquidity pool, therefore the cctx should revert
 	cctx = WaitCctxMinedByInTxHash(inTxHash, sm.cctxClient)
 
-	// The revert tx creation will fail because the sender, used as the recipient, is not defined in the cctx
+	// the revert tx creation will fail because the sender, used as the recipient, is not defined in the cctx
 	// TODO: uncomment once erc20 deposit contains the sender address
 	//https://github.com/zeta-chain/node/issues/1140
-	//if cctx.CctxStatus.Status != types.CctxStatus_Reverted {
-	//	panic(fmt.Sprintf("expected cctx status to be PendingRevert; got %s", cctx.CctxStatus.Status))
-	//}
-	//
-	//// get revert tx
-	//revertTxHash := cctx.GetCurrentOutTxParam().OutboundTxHash
-	//tx, _, err := sm.goerliClient.TransactionByHash(context.Background(), ethcommon.HexToHash(revertTxHash))
-	//if err != nil {
-	//	panic(err)
-	//}
-	//receipt, err := sm.goerliClient.TransactionReceipt(context.Background(), ethcommon.HexToHash(revertTxHash))
-	//if err != nil {
-	//	panic(err)
-	//}
-	//if receipt.Status == 0 {
-	//	panic("expected the revert tx receipt to have status 1; got 0")
-	//}
-	//
-	//if *tx.To() != DeployerAddress {
-	//	panic(fmt.Sprintf("expected tx to %s; got %s", DeployerAddress.Hex(), tx.To().Hex()))
-	//}
+	if cctx.CctxStatus.Status != types.CctxStatus_Reverted {
+		panic(fmt.Sprintf("expected cctx status to be PendingRevert; got %s", cctx.CctxStatus.Status))
+	}
+
+	// get revert tx
+	revertTxHash := cctx.GetCurrentOutTxParam().OutboundTxHash
+	_, _, err = sm.goerliClient.TransactionByHash(context.Background(), ethcommon.HexToHash(revertTxHash))
+	if err != nil {
+		panic(err)
+	}
+	receipt, err := sm.goerliClient.TransactionReceipt(context.Background(), ethcommon.HexToHash(revertTxHash))
+	if err != nil {
+		panic(err)
+	}
+	if receipt.Status == 0 {
+		panic("expected the revert tx receipt to have status 1; got 0")
+	}
+
+	// check that the erc20 in the reverted cctx was refunded on Goerli
+	newGoerliBalance, err := sm.USDTERC20.BalanceOf(&bind.CallOpts{}, DeployerAddress)
+	if err != nil {
+		panic(err)
+	}
+	// the new balance must be higher than the previous one because of the revert refund
+	if goerliBalance.Cmp(newGoerliBalance) != -1 {
+		panic(fmt.Sprintf("expected balance to be higher than %s after refund; got %s", goerliBalance.String(), newGoerliBalance.String()))
+	}
+	// it must also be lower than the previous balance + the amount because of the gas fee for the revert tx
+	balancePlusAmount := goerliBalance.Add(goerliBalance, amount)
+	if newGoerliBalance.Cmp(balancePlusAmount) != -1 {
+		panic(fmt.Sprintf("expected balance to be lower than %s after refund; got %s", balancePlusAmount.String(), newGoerliBalance.String()))
+	}
+
+	fmt.Println("ERC20 CCTX successfully reverted")
+	fmt.Println("\tbalance before refund: ", goerliBalance.String())
+	fmt.Println("\tamount: ", amount.String())
+	fmt.Println("\tbalance after refund: ", newGoerliBalance.String())
 }
 
 func (sm *SmokeTest) createZetaERC20LiquidityPool() error {
