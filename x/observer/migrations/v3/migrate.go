71f8fda6cd6033352e54457b8bf1d168fd3b625e
lumtis <lucas.bertrand.22@gmail.com>
2023-09-25 22:55:43 +0200

    fix import cycle

--- x/observer/migrations/v3/migrate.go
+++ x/observer/migrations/v3/migrate.go
@@ -2,13 +2,17 @@ package v3
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/zeta-chain/zetacore/x/observer/keeper"
 	"github.com/zeta-chain/zetacore/x/observer/types"
 )
 
+type ObserverKeeper interface {
+	GetParams(ctx sdk.Context) types.Params
+	SetParams(ctx sdk.Context, params types.Params)
+}
+
 // MigrateStore migrates the x/observer module state from the consensus version 2 to 3
 // This migration update the policy group
-func MigrateStore(ctx sdk.Context, k keeper.Keeper) error {
+func MigrateStore(ctx sdk.Context, k ObserverKeeper) error {
 	// Get first admin policy group
 	p := k.GetParams(ctx)
 	if len(p.AdminPolicy) == 0 || p.AdminPolicy[0] == nil {
