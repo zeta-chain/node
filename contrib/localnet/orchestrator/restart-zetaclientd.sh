b66d2ec0cbfd4cf17896cccef863f8fe4fe57795
kevinssgh <kevinssgh.github@gmail.com>
2023-09-28 14:26:55 -0400

    re-enable tests and fixed zetaclient restart.

--- contrib/localnet/orchestrator/restart-zetaclientd.sh
+++ contrib/localnet/orchestrator/restart-zetaclientd.sh
@@ -40,5 +40,5 @@ done
 echo current height is "$CURRENT_HEIGHT", restarting zetaclients
 for NODE in "${CLIENT_LIST[@]}"; do
     ssh -o "StrictHostKeyChecking no" "$NODE" -i ~/.ssh/localtest.pem killall zetaclientd
-    ssh -o "StrictHostKeyChecking no" "$NODE" -i ~/.ssh/localtest.pem "$GOPATH"/bin/new/zetaclientd start > "$HOME"/zetaclient.log 2>&1 &
+    ssh -o "StrictHostKeyChecking no" "$NODE" -i ~/.ssh/localtest.pem "$GOPATH/bin/new/zetaclientd start < /dev/null > $HOME/zetaclient.log 2>&1 &"
 done
