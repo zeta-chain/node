8874348b9b9b123863f90ec564d31ab47189402e
kevinssgh <kevinssgh.github@gmail.com>
2023-10-10 11:19:33 -0400

    added cli

--- x/observer/client/cli/query_blame.go
+++ x/observer/client/cli/query_blame.go
@@ -5,6 +5,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/spf13/cobra"
 	"github.com/zeta-chain/zetacore/x/observer/types"
+	"strconv"
 )
 
 func CmdBlameByIdentifier() *cobra.Command {
@@ -57,6 +58,49 @@ func CmdGetAllBlameRecords() *cobra.Command {
 			params := &types.QueryAllBlameRecordsRequest{}
 
 			res, err := queryClient.GetAllBlameRecords(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdGetBlameByChainAndNonce() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list-blame-by-msg [chainId] [nonce]",
+		Short: "Query AllBlameRecords",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			chainId := args[0]
+			nonce := args[1]
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			chain, err := strconv.Atoi(chainId)
+			if err != nil {
+				return err
+			}
+			nonceInt, err := strconv.Atoi(nonce)
+			if err != nil {
+				return err
+			}
+			params := &types.QueryBlameByChainAndNonceRequest{
+				ChainId: uint32(chain),
+				Nonce:   uint64(nonceInt),
+			}
+
+			res, err := queryClient.BlameByChainAndNonce(cmd.Context(), params)
 			if err != nil {
 				return err
 			}
