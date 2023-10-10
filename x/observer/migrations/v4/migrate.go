f9574012a394d61e04806d565d94bc3ac6d214a4
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-10-10 11:25:52 -0400

    add upgrade script for legacy crosschain flags

--- x/observer/migrations/v4/migrate.go
+++ x/observer/migrations/v4/migrate.go
@@ -1,6 +1,9 @@
 package v4
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/zeta-chain/zetacore/x/observer/types"
 )
@@ -10,7 +13,20 @@ type ObserverKeeper interface {
 	SetCrosschainFlags(ctx sdk.Context, crosschainFlags types.CrosschainFlags)
 }
 
-func MigrateStore(ctx sdk.Context, k ObserverKeeper) error {
-	k.SetCrosschainFlags(ctx, *types.DefaultCrosschainFlags())
+func MigrateStore(ctx sdk.Context, observerStoreKey storetypes.StoreKey, cdc codec.BinaryCodec) error {
+	newCrossChainFlags := types.DefaultCrosschainFlags()
+	var val types.LegacyCrosschainFlags
+	store := prefix.NewStore(ctx.KVStore(observerStoreKey), types.KeyPrefix(types.CrosschainFlagsKey))
+	b := store.Get([]byte{0})
+	if b != nil {
+		cdc.MustUnmarshal(b, &val)
+		if val.GasPriceIncreaseFlags != nil {
+			newCrossChainFlags.GasPriceIncreaseFlags = val.GasPriceIncreaseFlags
+		}
+		newCrossChainFlags.IsOutboundEnabled = val.IsOutboundEnabled
+		newCrossChainFlags.IsInboundEnabled = val.IsInboundEnabled
+	}
+	b = cdc.MustMarshal(newCrossChainFlags)
+	store.Set([]byte{0}, b)
 	return nil
 }
