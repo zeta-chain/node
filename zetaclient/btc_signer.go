8daac18f4f7ade5eaad6813eca8ecaa3143113ad
charliec <charliec@zetachain.com>
2023-10-20 10:31:40 -0500

    use real world max outTx size

--- zetaclient/btc_signer.go
+++ zetaclient/btc_signer.go
@@ -24,8 +24,8 @@ import (
 const (
 	maxNoOfInputsPerTx = 20
 	consolidationRank  = 20     // the rank below (or equal to) which we consolidate UTXOs
-	outTxBytesMin      = 400    // 500B is a conservative estimate for a 2-input, 3-output SegWit tx
-	outTxBytesMax      = 4_000  // 4KB is a conservative estimate for a 21-input, 3-output SegWit tx
+	outTxBytesMin      = 400    // 500B is an estimated size for a 2-input, 3-output SegWit tx
+	outTxBytesMax      = 3250   // 3250B is an estimated size for a 21-input, 3-output SegWit tx
 	outTxBytesCap      = 10_000 // in case of accident
 
 	// for ZRC20 configuration
