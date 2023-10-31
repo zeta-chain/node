e08d3a3b201bd16e5ea424b869af85f48b2156ea
lumtis <lucas.bertrand.22@gmail.com>
2023-10-30 18:07:59 -0700

    refactor smoke test

--- contrib/localnet/scripts/os-info.sh
+++ contrib/localnet/scripts/os-info.sh
@@ -2,21 +2,26 @@
 
 KEYRING_TEST="test"
 KEYRING_FILE="file"
-PASSWORD="password"
 HOSTNAME=$(hostname)
 
 # Operator key
 zetacored keys add operator --algo=secp256k1 --keyring-backend=$KEYRING_TEST
 operator_address=$(zetacored keys show operator -a --keyring-backend=$KEYRING_TEST)
 
-# Hotkey key
-printf "%s\n%s\n" "$PASSWORD" "$PASSWORD" | zetacored keys add hotkey --algo=secp256k1 --keyring-backend=$KEYRING_FILE
-hotkey_address=$(printf "%s\n%s\n" "$PASSWORD" "$PASSWORD" | zetacored keys show hotkey -a --keyring-backend=$KEYRING_FILE)
+# Hotkey key depending on the keyring-backend
+if [ "$HOTKEY_BACKEND" = "$KEYRING_FILE" ]; then
+    printf "%s\n%s\n" "$HOTKEY_PASSWORD" "$HOTKEY_PASSWORD" | zetacored keys add hotkey --algo=secp256k1 --keyring-backend=$KEYRING_FILE
+    hotkey_address=$(printf "%s\n%s\n" "$HOTKEY_PASSWORD" "$HOTKEY_PASSWORD" | zetacored keys show hotkey -a --keyring-backend=$KEYRING_FILE)
 
-# Get hotkey pubkey, the command use the configured keyring-backend
-zetacored config keyring-backend "$KEYRING_FILE"
-pubkey=$(printf "%s\n%s\n" "$PASSWORD" "$PASSWORD" | zetacored get-pubkey hotkey | sed -e 's/secp256k1:"\(.*\)"/\1/' |sed 's/ //g' )
-zetacored config keyring-backend "$KEYRING_TEST"
+    # Get hotkey pubkey, the command use keyring-backend in the cosmos config
+    zetacored config keyring-backend "$KEYRING_FILE"
+    pubkey=$(printf "%s\n%s\n" "$HOTKEY_PASSWORD" "$HOTKEY_PASSWORD" | zetacored get-pubkey hotkey | sed -e 's/secp256k1:"\(.*\)"/\1/' |sed 's/ //g' )
+    zetacored config keyring-backend "$KEYRING_TEST"
+else
+    zetacored keys add hotkey --algo=secp256k1 --keyring-backend=$KEYRING
+    hotkey_address=$(zetacored keys show hotkey -a --keyring-backend=$KEYRING)
+    pubkey=$(zetacored get-pubkey hotkey|sed -e 's/secp256k1:"\(.*\)"/\1/' | sed 's/ //g' )
+fi
 
 is_observer="y"
 
--- contrib/localnet/scripts/start-zetaclientd-genesis.sh
+++ contrib/localnet/scripts/start-zetaclientd-genesis.sh
@@ -4,6 +4,9 @@
 
 HOSTNAME=$(hostname)
 
+# read HOTKEY_BACKEND env var for hotkey keyring backend and set default to test
+BACKEND="${HOTKEY_BACKEND:-test}"
+
 cp  /root/preparams/PreParams_$HOSTNAME.json /root/preParams.json
 num=$(echo $HOSTNAME | tr -dc '0-9')
 node="zetacore$num"
@@ -28,6 +31,6 @@ else
   MYIP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
   SEED=$(curl --retry 10 --retry-delay 5 --retry-connrefused  -s zetaclient0:8123/p2p)
   rm ~/.tss/*
-  zetaclientd init --peer /ip4/172.20.0.21/tcp/6668/p2p/"$SEED" --zetacore-url "$node" --chain-id athens_101-1 --operator "$operatorAddress" --log-format=text --public-ip "$MYIP" --log-level 0
+  zetaclientd init --peer /ip4/172.20.0.21/tcp/6668/p2p/"$SEED" --zetacore-url "$node" --chain-id athens_101-1 --operator "$operatorAddress" --log-format=text --public-ip "$MYIP" --log-level 0 --keyring-backend "$BACKEND"
   zetaclientd start
 fi
