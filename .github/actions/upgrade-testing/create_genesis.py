b6f633138aa98ec583cefad9c0e84b9aac0cfaf3
Grant Zukel <grant.zukel@gmail.com>
2023-11-20 17:15:53 -0700

    updated links and version for upgrade path testing from google

--- .github/actions/upgrade-testing/create_genesis.py
+++ .github/actions/upgrade-testing/create_genesis.py
@@ -14,13 +14,19 @@ def genesis_modification():
     exported_genesis = None
     genesis = None
 
+    print("APP_STATE:AUTH:ACCOUNTS")
     genesis_json_object["app_state"]["auth"]["accounts"] = genesis_json_object["app_state"]["auth"]["accounts"] + \
                                                            exported_genesis_json_object["app_state"]["auth"]["accounts"]
+    print("APP_STATE:CROSSCHAIN")
     genesis_json_object["app_state"]["crosschain"] = exported_genesis_json_object["app_state"]["crosschain"]
+    print("APP_STATE:OBSERVER")
     genesis_json_object["app_state"]["observer"] = exported_genesis_json_object["app_state"]["observer"]
+    print("APP_STATE:EMISSIONS")
     genesis_json_object["app_state"]["emissions"] = exported_genesis_json_object["app_state"]["emissions"]
+    print("APP_STATE:FUNGIBLE")
     genesis_json_object["app_state"]["fungible"] = exported_genesis_json_object["app_state"]["fungible"]
 
+    print("EVM:ACCOUNTS")
     evm_accounts = []
     for index, account in enumerate(exported_genesis_json_object["app_state"]["evm"]["accounts"]):
         if account["address"] == "0x0000000000000000000000000000000000000001":
@@ -36,12 +42,15 @@ def genesis_modification():
         else:
             evm_accounts.append(account)
 
+    print("SET:EVM:ACCOUNTS")
     exported_genesis_json_object["app_state"]["evm"]["accounts"] = evm_accounts
+    evm_accounts = None
+    print("UPDATE:EVM")
     genesis_json_object["app_state"]["evm"] = exported_genesis_json_object["app_state"]["evm"]
-    genesis = open("genesis-edited.json", "w")
-    genesis_string = json.dumps(genesis_json_object, indent=2)
-    genesis.write(genesis_string)
-    genesis.close()
+    exported_genesis_json_object = None
+    print("SAVE:EDITED:GENESIS")
+    with open("genesis-edited.json", "w") as genesis:
+        genesis.write(json.dumps(genesis_json_object))
 
 
 genesis_modification()
