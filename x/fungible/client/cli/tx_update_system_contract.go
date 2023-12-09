50434aa330ee724dd51f3c5f5493125a07acfd0a
lukema95 <867273263@qq.com>
2023-12-09 11:29:22 +0800

    add CmdUpdateSystemContract cli

---
+++ x/fungible/client/cli/tx_update_system_contract.go
@@ -0,0 +1,39 @@
+package cli
+
+import (
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/spf13/cobra"
+	"github.com/zeta-chain/zetacore/x/fungible/types"
+)
+
+func CmdUpdateSystemContract() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "update-system-contract [contractAddress]",
+		Short: "Broadcast message UpdateSystemContract",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			contractAddress := args[0]
+
+			msg := types.NewMsgUpdateSystemContract(
+				clientCtx.GetFromAddress().String(),
+				contractAddress,
+			)
+
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
