9e441d44dbe26bcd22fcf24ce23547d69256d726
lumtis <lucas.bertrand.22@gmail.com>
2023-12-15 15:12:24 +0100

    prepare migrate function

--- x/observer/migrations/v4/migrate.go
+++ x/observer/migrations/v4/migrate.go
@@ -9,6 +9,13 @@ import (
 )
 
 func MigrateStore(ctx sdk.Context, observerStoreKey storetypes.StoreKey, cdc codec.BinaryCodec) error {
+	if err := MigrateCrosschainFlags(ctx, observerStoreKey, cdc); err != nil {
+		return err
+	}
+	return nil
+}
+
+func MigrateCrosschainFlags(ctx sdk.Context, observerStoreKey storetypes.StoreKey, cdc codec.BinaryCodec) error {
 	newCrossChainFlags := types.DefaultCrosschainFlags()
 	var val types.LegacyCrosschainFlags
 	store := prefix.NewStore(ctx.KVStore(observerStoreKey), types.KeyPrefix(types.CrosschainFlagsKey))
