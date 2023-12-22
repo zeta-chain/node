38e8a4694a2d56cedf9232b9c3d5e75cf67c1bb2
CharlieM <31941002+CharlieMc0@users.noreply.github.com>
2023-12-22 10:10:05 -0700

    added keyring test

--- contrib/localnet/scripts/create-gov-proposals.sh
+++ contrib/localnet/scripts/create-gov-proposals.sh
@@ -14,29 +14,29 @@ if ! zetacored keys show localnet_gov_admin -a --keyring-backend test > /dev/nul
 fi
 
 # Create a few short lived proposals for variety of testing
-zetacored tx gov submit-proposal proposals/proposal_for_failure.json --from $WALLET_NAME --chain-id athens_101-1 --fees 2000000000000000azeta --yes && sleep 10
+zetacored tx gov submit-proposal proposals/proposal_for_failure.json --from $WALLET_NAME --keyring-backend test --chain-id athens_101-1 --fees 2000000000000000azeta --yes && sleep 10
 zetacored tx gov vote 1 no --from $WALLET_NAME --keyring-backend test --chain-id athens_101-1 --yes --fees 2000000000000000azeta --yes
 
-zetacored tx gov submit-proposal proposals/proposal_for_success.json --from $WALLET_NAME --chain-id athens_101-1 --fees 2000000000000000azeta --yes && sleep 10
+zetacored tx gov submit-proposal proposals/proposal_for_success.json --from $WALLET_NAME --keyring-backend test --chain-id athens_101-1 --fees 2000000000000000azeta --yes && sleep 10
 zetacored tx gov vote 2 yes --from $WALLET_NAME --keyring-backend test --chain-id athens_101-1 --yes --fees 2000000000000000azeta --yes
 
-zetacored tx gov submit-proposal proposals/v100.0.0_proposal.json --from $WALLET_NAME --chain-id athens_101-1 --fees 2000000000000000azeta --yes && sleep 10
+zetacored tx gov submit-proposal proposals/v100.0.0_proposal.json --from $WALLET_NAME --keyring-backend test --chain-id athens_101-1 --fees 2000000000000000azeta --yes && sleep 10
 zetacored tx gov vote 3 yes --from $WALLET_NAME --keyring-backend test --chain-id athens_101-1 --yes --fees 2000000000000000azeta --yes
 
 # Increase the length of the voting period to 1 week
-zetacored tx gov submit-proposal proposals/proposal_voting_period.json --from $WALLET_NAME --chain-id athens_101-1 --fees 2000000000000000azeta --yes && sleep 10
+zetacored tx gov submit-proposal proposals/proposal_voting_period.json --from $WALLET_NAME --keyring-backend test --chain-id athens_101-1 --fees 2000000000000000azeta --yes && sleep 10
 zetacored tx gov vote 4 yes --from $WALLET_NAME --keyring-backend test --chain-id athens_101-1 --yes --fees 2000000000000000azeta --yes
 
 # Create a few long lived proposals for variety of testing
 
-zetacored tx gov submit-proposal proposals/proposal_voting_period.json --from $WALLET_NAME --chain-id athens_101-1 --fees 2000000000000000azeta --yes && sleep 10
+zetacored tx gov submit-proposal proposals/proposal_voting_period.json --from $WALLET_NAME --keyring-backend test --chain-id athens_101-1 --fees 2000000000000000azeta --yes && sleep 10
 zetacored tx gov vote 5 yes --from $WALLET_NAME --keyring-backend test --chain-id athens_101-1 --yes --fees 2000000000000000azeta --yes
 
 
-zetacored tx gov submit-proposal proposals/v100.0.0_proposal.json --from $WALLET_NAME --chain-id athens_101-1 --fees 2000000000000000azeta --yes && sleep 10
+zetacored tx gov submit-proposal proposals/v100.0.0_proposal.json --from $WALLET_NAME --keyring-backend test --chain-id athens_101-1 --fees 2000000000000000azeta --yes && sleep 10
 zetacored tx gov vote 6 yes --from $WALLET_NAME --keyring-backend test --chain-id athens_101-1 --yes --fees 2000000000000000azeta --yes
 
-zetacored tx gov submit-proposal proposals/proposal_for_deposit.json --from $WALLET_NAME --chain-id athens_101-1 --fees 2000000000000000azeta --yes && sleep 10
+zetacored tx gov submit-proposal proposals/proposal_for_deposit.json --from $WALLET_NAME --keyring-backend test --chain-id athens_101-1 --fees 2000000000000000azeta --yes && sleep 10
 zetacored tx gov vote 7 yes --from $WALLET_NAME --keyring-backend test --chain-id athens_101-1 --yes --fees 2000000000000000azeta --yes
 
 
