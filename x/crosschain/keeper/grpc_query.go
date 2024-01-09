59f6a01042a012bc06ed3750482d1c906e8ebb82
lumtis <lucas.bertrand.22@gmail.com>
2024-01-09 14:11:01 -0800

    add comment for code to be removed

--- x/crosschain/keeper/grpc_query.go
+++ x/crosschain/keeper/grpc_query.go
@@ -13,6 +13,7 @@ var _ types.QueryServer = Keeper{}
 // GetTssAddress returns the tss address
 // Deprecated: GetTssAddress returns the tss address
 // TODO: remove after v12 once upgrade testing is no longer needed with v11
+// https://github.com/zeta-chain/node/issues/1547
 func (k Keeper) GetTssAddress(_ context.Context, _ *types.QueryGetTssAddressRequest) (*types.QueryGetTssAddressResponse, error) {
 	return nil, status.Error(codes.Unimplemented, "Deprecated")
 }
