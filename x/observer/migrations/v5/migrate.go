bfd3c01fb6cac78da8e631f4a8774371c178f618
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2024-01-12 16:54:56 -0800

    fix: resolve migration script for v12 (#1549)
    * move migration script
    
    * update tests
    
    * add monitoring for vote tx results
    
    * changelog entry
    
    * fix invalid contract setup
    
    * fix smoke tests
    
    * optimize zeta deposit
    
    * latest experimentation
    
    * revert back e2e tests
    
    * add migration script for finalized inbound
    
    * add tests
    
    * goimport
    
    * lint issues
    
    * reformat breaking changes
    
    * fix typo
    
    ---------
    
    Co-authored-by: Charlie Chen <34498985+ws4charlie@users.noreply.github.com>

--- x/observer/migrations/v5/migrate.go
+++ x/observer/migrations/v5/migrate.go
@@ -8,7 +8,24 @@ import (
 	"github.com/zeta-chain/zetacore/x/observer/types"
 )
 
-func MigrateStore(ctx sdk.Context, observerStoreKey storetypes.StoreKey, cdc codec.BinaryCodec) error {
+// observerKeeper prevents circular dependency
+type observerKeeper interface {
+	GetParams(ctx sdk.Context) types.Params
+	SetParams(ctx sdk.Context, params types.Params)
+	GetChainParamsList(ctx sdk.Context) (params types.ChainParamsList, found bool)
+	SetChainParamsList(ctx sdk.Context, params types.ChainParamsList)
+	StoreKey() storetypes.StoreKey
+	Codec() codec.BinaryCodec
+}
+
+func MigrateStore(ctx sdk.Context, observerKeeper observerKeeper) error {
+	if err := MigrateObserverMapper(ctx, observerKeeper.StoreKey(), observerKeeper.Codec()); err != nil {
+		return err
+	}
+	return MigrateObserverParams(ctx, observerKeeper)
+}
+
+func MigrateObserverMapper(ctx sdk.Context, observerStoreKey storetypes.StoreKey, cdc codec.BinaryCodec) error {
 	var legacyObserverMappers []*types.ObserverMapper
 	legacyObserverMapperStore := prefix.NewStore(ctx.KVStore(observerStoreKey), types.KeyPrefix(types.ObserverMapperKey))
 	iterator := sdk.KVStorePrefixIterator(legacyObserverMapperStore, []byte{})
@@ -33,5 +50,33 @@ func MigrateStore(ctx sdk.Context, observerStoreKey storetypes.StoreKey, cdc cod
 	for _, legacyObserverMapper := range legacyObserverMappers {
 		legacyObserverMapperStore.Delete(types.KeyPrefix(legacyObserverMapper.Index))
 	}
+	return nil
+}
+
+// MigrateObserverParams migrates the observer params to the chain params
+// the function assumes that each observer params entry has a corresponding chain params entry
+// if the chain is not found, the observer params entry is ignored because it is considered as not supported
+func MigrateObserverParams(ctx sdk.Context, observerKeeper observerKeeper) error {
+	chainParamsList, found := observerKeeper.GetChainParamsList(ctx)
+	if !found {
+		// no chain params found, nothing to migrate
+		return nil
+	}
+
+	// search for the observer params with chain params entry
+	observerParams := observerKeeper.GetParams(ctx).ObserverParams
+	for _, observerParam := range observerParams {
+		for i := range chainParamsList.ChainParams {
+			// if the chain is found, update the chain params with the observer params
+			if chainParamsList.ChainParams[i].ChainId == observerParam.Chain.ChainId {
+				chainParamsList.ChainParams[i].MinObserverDelegation = observerParam.MinObserverDelegation
+				chainParamsList.ChainParams[i].BallotThreshold = observerParam.BallotThreshold
+				chainParamsList.ChainParams[i].IsSupported = observerParam.IsSupported
+				break
+			}
+		}
+	}
+
+	observerKeeper.SetChainParamsList(ctx, chainParamsList)
 	return nil
 }
