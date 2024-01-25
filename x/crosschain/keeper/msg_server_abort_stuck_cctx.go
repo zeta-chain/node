4ccff792d45054ca675f0a0a4c02ef4491fe21ea
lumtis <lucas.bertrand.22@gmail.com>
2024-01-24 23:11:22 -0800

    tests

--- x/crosschain/keeper/msg_server_abort_stuck_cctx.go
+++ x/crosschain/keeper/msg_server_abort_stuck_cctx.go
@@ -7,6 +7,11 @@ import (
 	observertypes "github.com/zeta-chain/zetacore/x/observer/types"
 )
 
+const (
+	// AbortMessage is the message to abort a stuck CCTX
+	AbortMessage = "CCTX aborted with admin cmd"
+)
+
 // AbortStuckCCTX aborts a stuck CCTX
 // Authorized: admin policy group 2
 func (k msgServer) AbortStuckCCTX(
@@ -26,9 +31,17 @@ func (k msgServer) AbortStuckCCTX(
 		return nil, types.ErrCannotFindCctx
 	}
 
+	// check if the cctx is pending
+	isPending := cctx.CctxStatus.Status == types.CctxStatus_PendingOutbound ||
+		cctx.CctxStatus.Status == types.CctxStatus_PendingInbound ||
+		cctx.CctxStatus.Status == types.CctxStatus_PendingRevert
+	if !isPending {
+		return nil, types.ErrStatusNotPending
+	}
+
 	cctx.CctxStatus = &types.Status{
 		Status:        types.CctxStatus_Aborted,
-		StatusMessage: "CCTX aborted with admin cmd",
+		StatusMessage: AbortMessage,
 	}
 
 	k.SetCrossChainTx(ctx, cctx)
---
+++ x/crosschain/keeper/msg_server_abort_stuck_cctx_test.go
@@ -0,0 +1,151 @@
+package keeper_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	keepertest "github.com/zeta-chain/zetacore/testutil/keeper"
+	"github.com/zeta-chain/zetacore/testutil/sample"
+	crosschainkeeper "github.com/zeta-chain/zetacore/x/crosschain/keeper"
+	crosschaintypes "github.com/zeta-chain/zetacore/x/crosschain/types"
+	observertypes "github.com/zeta-chain/zetacore/x/observer/types"
+)
+
+func TestMsgServer_AbortStuckCCTX(t *testing.T) {
+	t.Run("can abort a cctx in pending inbound", func(t *testing.T) {
+		k, ctx, _, zk := keepertest.CrosschainKeeper(t)
+		msgServer := crosschainkeeper.NewMsgServerImpl(*k)
+		admin := sample.AccAddress()
+		setAdminPolicies(ctx, zk, admin)
+
+		// create a cctx
+		cctx := sample.CrossChainTx(t, "cctx_index")
+		cctx.CctxStatus = &crosschaintypes.Status{
+			Status:        crosschaintypes.CctxStatus_PendingInbound,
+			StatusMessage: "pending inbound",
+		}
+		k.SetCrossChainTx(ctx, *cctx)
+
+		// abort the cctx
+		_, err := msgServer.AbortStuckCCTX(ctx, &crosschaintypes.MsgAbortStuckCCTX{
+			Creator:   admin,
+			CctxIndex: "cctx_index",
+		})
+
+		require.NoError(t, err)
+		cctxFound, found := k.GetCrossChainTx(ctx, "cctx_index")
+		require.True(t, found)
+		require.Equal(t, crosschaintypes.CctxStatus_Aborted, cctxFound.CctxStatus.Status)
+		require.Equal(t, crosschainkeeper.AbortMessage, cctxFound.CctxStatus.StatusMessage)
+	})
+
+	t.Run("can abort a cctx in pending outbound", func(t *testing.T) {
+		k, ctx, _, zk := keepertest.CrosschainKeeper(t)
+		msgServer := crosschainkeeper.NewMsgServerImpl(*k)
+		admin := sample.AccAddress()
+		setAdminPolicies(ctx, zk, admin)
+
+		// create a cctx
+		cctx := sample.CrossChainTx(t, "cctx_index")
+		cctx.CctxStatus = &crosschaintypes.Status{
+			Status:        crosschaintypes.CctxStatus_PendingOutbound,
+			StatusMessage: "pending outbound",
+		}
+		k.SetCrossChainTx(ctx, *cctx)
+
+		// abort the cctx
+		_, err := msgServer.AbortStuckCCTX(ctx, &crosschaintypes.MsgAbortStuckCCTX{
+			Creator:   admin,
+			CctxIndex: "cctx_index",
+		})
+
+		require.NoError(t, err)
+		cctxFound, found := k.GetCrossChainTx(ctx, "cctx_index")
+		require.True(t, found)
+		require.Equal(t, crosschaintypes.CctxStatus_Aborted, cctxFound.CctxStatus.Status)
+		require.Equal(t, crosschainkeeper.AbortMessage, cctxFound.CctxStatus.StatusMessage)
+	})
+
+	t.Run("can abort a cctx in pending revert", func(t *testing.T) {
+		k, ctx, _, zk := keepertest.CrosschainKeeper(t)
+		msgServer := crosschainkeeper.NewMsgServerImpl(*k)
+		admin := sample.AccAddress()
+		setAdminPolicies(ctx, zk, admin)
+
+		// create a cctx
+		cctx := sample.CrossChainTx(t, "cctx_index")
+		cctx.CctxStatus = &crosschaintypes.Status{
+			Status:        crosschaintypes.CctxStatus_PendingRevert,
+			StatusMessage: "pending revert",
+		}
+		k.SetCrossChainTx(ctx, *cctx)
+
+		// abort the cctx
+		_, err := msgServer.AbortStuckCCTX(ctx, &crosschaintypes.MsgAbortStuckCCTX{
+			Creator:   admin,
+			CctxIndex: "cctx_index",
+		})
+
+		require.NoError(t, err)
+		cctxFound, found := k.GetCrossChainTx(ctx, "cctx_index")
+		require.True(t, found)
+		require.Equal(t, crosschaintypes.CctxStatus_Aborted, cctxFound.CctxStatus.Status)
+		require.Equal(t, crosschainkeeper.AbortMessage, cctxFound.CctxStatus.StatusMessage)
+	})
+
+	t.Run("cannot abort a cctx in pending outbound if not admin", func(t *testing.T) {
+		k, ctx, _, _ := keepertest.CrosschainKeeper(t)
+		msgServer := crosschainkeeper.NewMsgServerImpl(*k)
+
+		// create a cctx
+		cctx := sample.CrossChainTx(t, "cctx_index")
+		cctx.CctxStatus = &crosschaintypes.Status{
+			Status:        crosschaintypes.CctxStatus_PendingOutbound,
+			StatusMessage: "pending outbound",
+		}
+		k.SetCrossChainTx(ctx, *cctx)
+
+		// abort the cctx
+		_, err := msgServer.AbortStuckCCTX(ctx, &crosschaintypes.MsgAbortStuckCCTX{
+			Creator:   sample.AccAddress(),
+			CctxIndex: "cctx_index",
+		})
+		require.ErrorIs(t, err, observertypes.ErrNotAuthorized)
+	})
+
+	t.Run("cannot abort a cctx if doesn't exist", func(t *testing.T) {
+		k, ctx, _, zk := keepertest.CrosschainKeeper(t)
+		msgServer := crosschainkeeper.NewMsgServerImpl(*k)
+		admin := sample.AccAddress()
+		setAdminPolicies(ctx, zk, admin)
+
+		// abort the cctx
+		_, err := msgServer.AbortStuckCCTX(ctx, &crosschaintypes.MsgAbortStuckCCTX{
+			Creator:   admin,
+			CctxIndex: "cctx_index",
+		})
+		require.ErrorIs(t, err, crosschaintypes.ErrCannotFindCctx)
+	})
+
+	t.Run("cannot abort a cctx if not pending", func(t *testing.T) {
+		k, ctx, _, zk := keepertest.CrosschainKeeper(t)
+		msgServer := crosschainkeeper.NewMsgServerImpl(*k)
+		admin := sample.AccAddress()
+		setAdminPolicies(ctx, zk, admin)
+
+		// create a cctx
+		cctx := sample.CrossChainTx(t, "cctx_index")
+		cctx.CctxStatus = &crosschaintypes.Status{
+			Status:        crosschaintypes.CctxStatus_OutboundMined,
+			StatusMessage: "outbound minted",
+		}
+		k.SetCrossChainTx(ctx, *cctx)
+
+		// abort the cctx
+		_, err := msgServer.AbortStuckCCTX(ctx, &crosschaintypes.MsgAbortStuckCCTX{
+			Creator:   admin,
+			CctxIndex: "cctx_index",
+		})
+		require.ErrorIs(t, err, crosschaintypes.ErrStatusNotPending)
+	})
+}
