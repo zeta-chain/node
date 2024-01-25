4c92c62dec3cc26730e95204fc330a0cc4d30a1a
Tanmay <tanmay@zetachain.com>
2024-01-25 16:35:18 -0500

    Update common/gas_limits.go
    Co-authored-by: Lucas Bertrand <lucas.bertrand.22@gmail.com>

--- common/gas_limits.go
+++ common/gas_limits.go
@@ -2,6 +2,6 @@ package common
 
 const (
 	EVMSend = 21000
-	// Todo Move gas limits from zeta-client to this file
+	// TODO: Move gas limits from zeta-client to this file
 	// https://github.com/zeta-chain/node/issues/1606
 )
