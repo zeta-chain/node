d48d12943280cc5c05f21db6c0bf8c153caee436
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2024-01-29 16:43:05 -0500

    add emission mocks

--- testutil/keeper/emissions.go
+++ testutil/keeper/emissions.go
@@ -16,10 +16,17 @@ import (
 	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmdb "github.com/tendermint/tm-db"
+	emissionsmocks "github.com/zeta-chain/zetacore/testutil/keeper/mocks/emissions"
 	"github.com/zeta-chain/zetacore/x/emissions/keeper"
 	"github.com/zeta-chain/zetacore/x/emissions/types"
 	observerkeeper "github.com/zeta-chain/zetacore/x/observer/keeper"
 )
+
+type EmissionMockOptions struct {
+	UseBankMock     bool
+	UseStakingMock  bool
+	UseObserverMock bool
+}
 
 func EmissionsKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 	storeKey := sdk.NewKVStoreKey(types.StoreKey)
@@ -54,4 +61,83 @@ func EmissionsKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
 	k.SetParams(ctx, types.DefaultParams())
 	return k, ctx
+}
+
+func EmisionKeeper(t testing.TB) (*keeper.Keeper, sdk.Context, SDKKeepers, ZetaKeepers) {
+	return EmissionKeeperWithMocks(t, EmissionMockOptions{
+		UseBankMock:     false,
+		UseStakingMock:  false,
+		UseObserverMock: false,
+	})
+}
+func EmissionKeeperWithMocks(
+	t testing.TB,
+	mockOptions EmissionMockOptions,
+) (*keeper.Keeper, sdk.Context, SDKKeepers, ZetaKeepers) {
+	SetConfig(false)
+	storeKey := sdk.NewKVStoreKey(types.StoreKey)
+	memStoreKey := storetypes.NewMemoryStoreKey(types.MemStoreKey)
+
+	// Initialize local store
+	db := tmdb.NewMemDB()
+	stateStore := store.NewCommitMultiStore(db)
+	cdc := NewCodec()
+
+	// Create regular keepers
+	sdkKeepers := NewSDKKeepers(cdc, db, stateStore)
+
+	// Create zeta keepers
+	observerKeeperTmp := initObserverKeeper(
+		cdc,
+		db,
+		stateStore,
+		sdkKeepers.StakingKeeper,
+		sdkKeepers.SlashingKeeper,
+		sdkKeepers.ParamsKeeper,
+	)
+
+	zetaKeepers := ZetaKeepers{
+		ObserverKeeper: observerKeeperTmp,
+	}
+	var observerKeeper types.ObserverKeeper = observerKeeperTmp
+
+	// Create the fungible keeper
+	stateStore.MountStoreWithDB(storeKey, storetypes.StoreTypeIAVL, db)
+	stateStore.MountStoreWithDB(memStoreKey, storetypes.StoreTypeMemory, nil)
+	require.NoError(t, stateStore.LoadLatestVersion())
+
+	ctx := NewContext(stateStore)
+
+	// Initialize modules genesis
+	sdkKeepers.InitGenesis(ctx)
+	zetaKeepers.InitGenesis(ctx)
+
+	// Add a proposer to the context
+	ctx = sdkKeepers.InitBlockProposer(t, ctx)
+
+	// Initialize mocks for mocked keepers
+	var bankKeeper types.BankKeeper = sdkKeepers.BankKeeper
+	var stakingKeeper types.StakingKeeper = sdkKeepers.StakingKeeper
+	if mockOptions.UseBankMock {
+		bankKeeper = emissionsmocks.NewEmissionBankKeeper(t)
+	}
+	if mockOptions.UseStakingMock {
+		stakingKeeper = emissionsmocks.NewEmissionStakingKeeper(t)
+	}
+	if mockOptions.UseObserverMock {
+		observerKeeper = emissionsmocks.NewEmissionObserverKeeper(t)
+	}
+
+	k := keeper.NewKeeper(
+		cdc,
+		storeKey,
+		memStoreKey,
+		sdkKeepers.ParamsKeeper.Subspace(types.ModuleName),
+		authtypes.FeeCollectorName,
+		bankKeeper,
+		stakingKeeper,
+		observerKeeper,
+	)
+
+	return k, ctx, sdkKeepers, zetaKeepers
 }
