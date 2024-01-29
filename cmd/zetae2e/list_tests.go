758191611de61801d2e922e60a8cce42ac3714d1
lumtis <lucas.bertrand.22@gmail.com>
2024-01-28 17:14:57 -0800

    implement list tests command

---
+++ cmd/zetae2e/list_tests.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+	"github.com/zeta-chain/zetacore/contrib/localnet/orchestrator/smoketest/runner"
+	"github.com/zeta-chain/zetacore/contrib/localnet/orchestrator/smoketest/smoketests"
+)
+
+// NewListTestsCmd returns the list test cmd
+// which list the available tests
+func NewListTestsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list-tests",
+		Short: "List available tests",
+		RunE:  runListTests,
+		Args:  cobra.NoArgs,
+	}
+	return cmd
+}
+
+func runListTests(_ *cobra.Command, _ []string) error {
+	logger := runner.NewLogger(false, color.FgHiGreen, "")
+
+	logger.Print("Available tests:")
+	renderTests(logger, smoketests.AllSmokeTests)
+
+	return nil
+}
+
+func renderTests(logger *runner.Logger, tests []runner.SmokeTest) {
+	// Find the maximum length of the Name field
+	maxNameLength := 0
+	for _, test := range tests {
+		if len(test.Name) > maxNameLength {
+			maxNameLength = len(test.Name)
+		}
+	}
+
+	// Formatting and printing the table
+	formatString := fmt.Sprintf("%%-%ds | %%s", maxNameLength)
+	logger.Print(formatString, "Name", "Description")
+	for _, test := range tests {
+		logger.Print(formatString, test.Name, test.Description)
+	}
+}
--- cmd/zetae2e/root.go
+++ cmd/zetae2e/root.go
@@ -22,6 +22,7 @@ func NewRootCmd() *cobra.Command {
 		NewRunCmd(),
 		NewBalancesCmd(),
 		NewBitcoinAddressCmd(),
+		NewListTestsCmd(),
 		NewShowTSSCmd(),
 		local.NewLocalCmd(),
 		NewStressTestCmd(),
