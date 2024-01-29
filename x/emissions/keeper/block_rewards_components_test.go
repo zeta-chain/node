12cb109d125bf72aca8c5e4a79b77a7a203f2193
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2024-01-28 21:59:30 -0500

    add changelog entry

--- x/emissions/keeper/block_rewards_components_test.go
+++ x/emissions/keeper/block_rewards_components_test.go
@@ -6,46 +6,45 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/assert"
 	emissionskeeper "github.com/zeta-chain/zetacore/x/emissions/keeper"
-	"github.com/zeta-chain/zetacore/x/emissions/types"
 )
 
 func TestKeeper_CalculateFixedValidatorRewards(t *testing.T) {
 	tt := []struct {
-		name            string
-		blockTimeInSecs string
+		name                 string
+		blockTimeInSecs      string
+		expectedBlockRewards sdk.Dec
 	}{
 		{
-			name:            "Block Time 5.7",
-			blockTimeInSecs: "5.7",
+			name:                 "Block Time 5.7",
+			blockTimeInSecs:      "5.7",
+			expectedBlockRewards: sdk.MustNewDecFromStr("9620949074074074074.074070733466756687"),
 		},
 		{
-			name:            "Block Time 6",
-			blockTimeInSecs: "6",
+			name:                 "Block Time 6",
+			blockTimeInSecs:      "6",
+			expectedBlockRewards: sdk.MustNewDecFromStr("10127314814814814814.814814814814814815"),
 		},
 		{
-			name:            "Block Time 3",
-			blockTimeInSecs: "3",
+			name:                 "Block Time 3",
+			blockTimeInSecs:      "3",
+			expectedBlockRewards: sdk.MustNewDecFromStr("5063657407407407407.407407407407407407"),
 		},
 		{
-			name:            "Block Time 2",
-			blockTimeInSecs: "2",
+			name:                 "Block Time 2",
+			blockTimeInSecs:      "2",
+			expectedBlockRewards: sdk.MustNewDecFromStr("3375771604938271604.938271604938271605"),
 		},
 		{
-			name:            "Block Time 8",
-			blockTimeInSecs: "8",
+			name:                 "Block Time 8",
+			blockTimeInSecs:      "8",
+			expectedBlockRewards: sdk.MustNewDecFromStr("13503086419753086419.753086419753086420"),
 		},
 	}
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 			blockRewards, err := emissionskeeper.CalculateFixedValidatorRewards(tc.blockTimeInSecs)
 			assert.NoError(t, err)
-			avgBlockTime, err := sdk.NewDecFromStr(tc.blockTimeInSecs)
-			assert.NoError(t, err)
-			azetaAmountTotalRewards, err := sdk.NewDecFromStr("210000000000000000000000000")
-			numberOfBlocksInAMonth := sdk.NewDec(types.SecsInMonth).Quo(avgBlockTime)
-			numberOfBlocksTotal := numberOfBlocksInAMonth.Mul(sdk.NewDec(12)).Mul(sdk.NewDec(types.EmissionScheduledYears))
-			constantRewardPerBlock := azetaAmountTotalRewards.Quo(numberOfBlocksTotal)
-			assert.Equal(t, constantRewardPerBlock, blockRewards)
+			assert.Equal(t, tc.expectedBlockRewards, blockRewards)
 		})
 	}
 }
