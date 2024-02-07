c48e31be6601343252f43951bd0160b600cdbfdc
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2024-02-06 19:23:12 -0500

    add check for is refunded flag

--- x/crosschain/keeper/msg_server_refund_aborted_tx.go
+++ x/crosschain/keeper/msg_server_refund_aborted_tx.go
@@ -25,12 +25,17 @@ func (k msgServer) RefundAbortedCCTX(goCtx context.Context, msg *types.MsgRefund
 	if cctx.CctxStatus.Status != types.CctxStatus_Aborted {
 		return nil, errorsmod.Wrap(types.ErrInvalidStatus, "CCTX is not aborted")
 	}
+	// check if the cctx is not refunded
+	if cctx.IsRefunded {
+		return nil, errorsmod.Wrap(types.ErrUnableProcessRefund, "CCTX is already refunded")
+	}
 
 	// refund the amount
 	err := k.RefundAbortedAmountOnZetaChain(ctx, cctx)
 	if err != nil {
 		return nil, errorsmod.Wrap(types.ErrUnableProcessRefund, err.Error())
 	}
-
+	cctx.IsRefunded = true
+	k.SetCrossChainTx(ctx, cctx)
 	return &types.MsgRefundAbortedCCTXResponse{}, nil
 }
