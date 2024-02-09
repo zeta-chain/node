0d79661190793bebd4764b4db9774a9073ebe52f
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2024-02-09 12:26:25 -0500

    add comments to test

--- x/emissions/abci_test.go
+++ x/emissions/abci_test.go
@@ -1,7 +1,6 @@
 package emissions_test
 
 import (
-	"fmt"
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -17,10 +16,13 @@ import (
 )
 
 func TestDistributeObserverRewards(t *testing.T) {
+	// setup the test
 	k, ctx, sk, zk := keepertest.EmisionKeeper(t)
 	observerSet := sample.ObserverSet(10)
 	zk.ObserverKeeper.SetObserverSet(ctx, observerSet)
 	ballotList := sample.BallotList(10, observerSet.ObserverList)
+
+	// set the ballot list
 	ballotIdentifiers := []string{}
 	for _, ballot := range ballotList {
 		zk.ObserverKeeper.SetBallot(ctx, &ballot)
@@ -30,39 +32,46 @@ func TestDistributeObserverRewards(t *testing.T) {
 		Height:           0,
 		BallotsIndexList: ballotIdentifiers,
 	})
+
+	// Total block rewards is the fixed amount of rewards that are distributed
 	totalBlockRewards, err := common.GetAzetaDecFromAmountInZeta(emissionstypes.BlockRewardsInZeta)
 	rewardCoins := sdk.NewCoins(sdk.NewCoin(config.BaseDenom, totalBlockRewards.TruncateInt()))
 	assert.NoError(t, err)
+	// Fund the emission pool to start the emission process
 	err = sk.BankKeeper.MintCoins(ctx, emissionstypes.ModuleName, rewardCoins)
 	assert.NoError(t, err)
+
+	// Setup module accounts for emission pools
 	undistributedObserverPoolAddress := sk.AuthKeeper.GetModuleAccount(ctx, emissionstypes.UndistributedObserverRewardsPool).GetAddress()
 	undistributedTssPoolAddress := sk.AuthKeeper.GetModuleAccount(ctx, emissionstypes.UndistributedTssRewardsPool).GetAddress()
 	feeCollecterAddress := sk.AuthKeeper.GetModuleAccount(ctx, types.FeeCollectorName).GetAddress()
 	emissionPool := sk.AuthKeeper.GetModuleAccount(ctx, emissionstypes.ModuleName).GetAddress()
-
-	for i := 0; i < 20736000; i++ {
-		balanceEmissonPoolBeforeBlockDistribution := sk.BankKeeper.GetBalance(ctx, emissionPool, config.BaseDenom).Amount
-
+	totalDistributedTillLastBlock := sdk.ZeroInt()
+	blockRewards := emissionstypes.BlockReward
+	for i := 0; i < 100; i++ {
+		balanceEmissionPoolBeforeBlockDistribution := sk.BankKeeper.GetBalance(ctx, emissionPool, config.BaseDenom).Amount
+		// produce a block
 		emissionsModule.BeginBlocker(ctx, *k)
 
 		feeCollecterBalance := sk.BankKeeper.GetBalance(ctx, feeCollecterAddress, config.BaseDenom).Amount
 		observerPoolBalance := sk.BankKeeper.GetBalance(ctx, undistributedObserverPoolAddress, config.BaseDenom).Amount
 		tssPoolBalance := sk.BankKeeper.GetBalance(ctx, undistributedTssPoolAddress, config.BaseDenom).Amount
 		emissionPoolBalanceAfterBlockDistribution := sk.BankKeeper.GetBalance(ctx, emissionPool, config.BaseDenom).Amount
+
+		// Assert the rewards pool has enough balance to distribute rewards
 		rewardsDistributedAndLeftInPool := emissionPoolBalanceAfterBlockDistribution.Sub(rewardCoins.AmountOf(config.BaseDenom))
-		assert.True(t, balanceEmissonPoolBeforeBlockDistribution.Sub(rewardsDistributedAndLeftInPool).GTE(sdk.ZeroInt()))
-		totalPoolBalance := feeCollecterBalance.Add(observerPoolBalance).Add(tssPoolBalance)
-		assert.True(t, rewardCoins.AmountOf(config.BaseDenom).Sub(totalPoolBalance).GTE(sdk.ZeroInt()))
+		assert.True(t, balanceEmissionPoolBeforeBlockDistribution.Sub(rewardsDistributedAndLeftInPool).GTE(sdk.ZeroInt()))
+
+		// totalDistributedTillCurrentBlock is the net amount of rewards distributed till the current block , this works in a unit test as the fees are not being collected by validators
+		totalDistributedTillCurrentBlock := feeCollecterBalance.Add(observerPoolBalance).Add(tssPoolBalance)
+
+		// Assert that a maximum of value of block rewards is distributed in each block
+		assert.True(t, totalDistributedTillCurrentBlock.Sub(totalDistributedTillLastBlock).LTE(blockRewards.TruncateInt()))
+
+		// Assert we are always under the max limit of block rewards
+		assert.True(t, rewardCoins.AmountOf(config.BaseDenom).Sub(totalDistributedTillCurrentBlock).GTE(sdk.ZeroInt()))
+
+		totalDistributedTillLastBlock = totalDistributedTillCurrentBlock
 		ctx = ctx.WithBlockHeight(ctx.BlockHeight() + 1)
 	}
-
-}
-
-func TestKeeper_CalculateFixedObserverRewards(t *testing.T) {
-	SecsInAHour := float64(60 * 60)
-	BlockTime := 6.0
-	BlocksInAHour := SecsInAHour / BlockTime
-	NoOfHours := 2
-	StartingBlock := 1940897
-	fmt.Println("Proposal Block :", StartingBlock+int(BlocksInAHour*float64(NoOfHours)))
 }
