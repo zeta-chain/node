63ae7d7b01f5dbbeaef5f01527eb384cd4f7ca37
Charlie Chen <charliec@zetachain.com>
2024-02-12 00:01:43 -0600

    fix gosec

--- zetaclient/testutils/utils.go
+++ zetaclient/testutils/utils.go
@@ -3,6 +3,7 @@ package testutils
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 )
 
 const (
@@ -11,7 +12,7 @@ const (
 
 // SaveObjectToJSONFile saves an object to a file in JSON format
 func SaveObjectToJSONFile(obj interface{}, filename string) error {
-	file, err := os.Create(filename)
+	file, err := os.Create(filepath.Clean(filename))
 	if err != nil {
 		return err
 	}
@@ -24,7 +25,7 @@ func SaveObjectToJSONFile(obj interface{}, filename string) error {
 
 // LoadObjectFromJSONFile loads an object from a file in JSON format
 func LoadObjectFromJSONFile(obj interface{}, filename string) error {
-	file, err := os.Open(filename)
+	file, err := os.Open(filepath.Clean(filename))
 	if err != nil {
 		return err
 	}
