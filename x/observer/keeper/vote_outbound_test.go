f0c7843b16d5b7984154b8ad2a29303101aef070
lumtis <lucas.bertrand.22@gmail.com>
2024-02-20 17:42:39 +0100

    initialize outbound tests

---
+++ x/observer/keeper/vote_outbound_test.go
@@ -0,0 +1,17 @@
+package keeper_test
+
+import "testing"
+
+func TestKeeper_VoteOnOutboundBallot(t *testing.T) {
+	t.Run("fail if chain is not supported", func(t *testing.T) {
+
+	})
+
+	t.Run("fail if sender is not authorized", func(t *testing.T) {
+
+	})
+
+	t.Run("fail if sender is not authorized", func(t *testing.T) {
+
+	})
+}
