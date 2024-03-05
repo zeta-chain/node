4ad7f95c736ffc674a5fa6e7a005c5fe94cb0908
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2024-03-05 09:59:21 +0100

    Update x/authority/client/cli/query.go
    Co-authored-by: Tanmay <tanmay@zetachain.com>

--- x/authority/client/cli/query.go
+++ x/authority/client/cli/query.go
@@ -10,7 +10,7 @@ import (
 
 // GetQueryCmd returns the cli query commands for this module
 func GetQueryCmd(_ string) *cobra.Command {
-	// Group crosschain queries under a subcommand
+	// Group authority queries under a subcommand
 	cmd := &cobra.Command{
 		Use:                        types.ModuleName,
 		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
