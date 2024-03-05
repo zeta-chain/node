c3173de1fc03359344cec0bc814e0cdad8b12377
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2024-03-05 14:05:16 +0100

    Update x/emissions/types/keys.go

--- x/emissions/types/keys.go
+++ x/emissions/types/keys.go
@@ -23,7 +23,6 @@ const (
 	// MemStoreKey defines the in-memory store key
 	MemStoreKey              = "mem_emissions"
 	WithdrawableEmissionsKey = "WithdrawableEmissions-value-"
-	WithdrawEmissionsKey     = "WithdrawEmissions-value-"
 
 	SecsInMonth        = 30 * 24 * 60 * 60
 	BlockRewardsInZeta = "210000000"
