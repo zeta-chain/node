6c07f0f59dc25d9cb31aadbb1f1cd4d7996d5418
lumtis <lucas.bertrand.22@gmail.com>
2024-03-09 16:33:55 +0100

    add param store keeper

--- x/emissions/types/distributions.go
+++ x/emissions/types/distributions.go
@@ -10,23 +10,25 @@ import (
 // for validators, observers and TSS signers
 // If the percentage is not set, it returns 0
 func (k Keeper) GetDistributions(ctx sdk.Context) (sdkmath.Int, sdkmath.Int, sdkmath.Int) {
+	params := k.GetParamSetIfExists(ctx)
+
 	// Fetch the validator rewards, use 0 if the percentage is not set
 	validatorRewards := sdk.NewInt(0)
-	validatorRewardsDec, err := sdk.NewDecFromStr(k.GetParamSetIfExists(ctx).ValidatorEmissionPercentage)
+	validatorRewardsDec, err := sdk.NewDecFromStr(params.ValidatorEmissionPercentage)
 	if err == nil {
 		validatorRewards = validatorRewardsDec.Mul(types.BlockReward).TruncateInt()
 	}
 
 	// Fetch the observer rewards, use 0 if the percentage is not set
 	observerRewards := sdk.NewInt(0)
-	observerRewardsDec, err := sdk.NewDecFromStr(k.GetParamSetIfExists(ctx).ObserverEmissionPercentage)
+	observerRewardsDec, err := sdk.NewDecFromStr(params.ObserverEmissionPercentage)
 	if err == nil {
 		observerRewards = observerRewardsDec.Mul(types.BlockReward).TruncateInt()
 	}
 
 	// Fetch the TSS signer rewards, use 0 if the percentage is not set
 	tssSignerRewards := sdk.NewInt(0)
-	tssSignerRewardsDec, err := sdk.NewDecFromStr(k.GetParamSetIfExists(ctx).TssSignerEmissionPercentage)
+	tssSignerRewardsDec, err := sdk.NewDecFromStr(params.TssSignerEmissionPercentage)
 	if err == nil {
 		tssSignerRewards = tssSignerRewardsDec.Mul(types.BlockReward).TruncateInt()
 	}
