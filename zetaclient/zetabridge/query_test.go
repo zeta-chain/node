7698377dd1979dbb36967aa20d86564beb7b8219
kevinssgh <kevinssgh.github@gmail.com>
2024-03-21 12:11:22 -0400

    setup query testing

--- zetaclient/zetabridge/query_test.go
+++ zetaclient/zetabridge/query_test.go
@@ -1,50 +1,68 @@
 package zetabridge
 
 import (
-	"context"
-	"fmt"
-	"github.com/stretchr/testify/require"
-	observertypes "github.com/zeta-chain/zetacore/x/observer/types"
-	"go.nhat.io/grpcmock"
-	"go.nhat.io/grpcmock/planner"
-	"google.golang.org/grpc"
 	"net"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+	observertypes "github.com/zeta-chain/zetacore/x/observer/types"
+	"github.com/zeta-chain/zetacore/zetaclient/keys"
+	"github.com/zeta-chain/zetacore/zetaclient/metrics"
+	"go.nhat.io/grpcmock"
+	"go.nhat.io/grpcmock/planner"
 )
 
-func TestZetaCoreBridge_GetBallot(t *testing.T) {
+func setupMockServer(t *testing.T, serviceFunc any, method string, input any, expectedOutput any) *grpcmock.Server {
 	l, err := net.Listen("tcp", "127.0.0.1:9090")
 	require.NoError(t, err)
 
 	server := grpcmock.MockUnstartedServer(
-		grpcmock.RegisterService(observertypes.RegisterQueryServer),
+		grpcmock.RegisterService(serviceFunc),
 		grpcmock.WithPlanner(planner.FirstMatch()),
 		grpcmock.WithListener(l),
 		func(s *grpcmock.Server) {
-			s.ExpectUnary("/zetachain.zetacore.observer.Query/BallotByIdentifier").
+			s.ExpectUnary(method).
 				UnlimitedTimes().
-				Return(observertypes.QueryBallotByIdentifierResponse{
-					BallotIdentifier: "456",
-					Voters:           nil,
-					ObservationType:  0,
-					BallotStatus:     0,
-				})
+				WithPayload(input).
+				Return(expectedOutput)
 		},
 	)(t)
+
+	return server
+}
+
+func closeMockServer(t *testing.T, server *grpcmock.Server) {
+	err := server.Close()
+	require.NoError(t, err)
+}
+
+func setupCorBridge() (*ZetaCoreBridge, error) {
+	return NewZetaCoreBridge(
+		&keys.Keys{},
+		"127.0.0.1",
+		"",
+		"zetachain_7000-1",
+		false,
+		&metrics.TelemetryServer{})
+}
+
+func TestZetaCoreBridge_GetBallot(t *testing.T) {
+	expectedOutput := observertypes.QueryBallotByIdentifierResponse{
+		BallotIdentifier: "456",
+		Voters:           nil,
+		ObservationType:  0,
+		BallotStatus:     0,
+	}
+	input := observertypes.QueryBallotByIdentifierRequest{BallotIdentifier: "123"}
+	method := "/zetachain.zetacore.observer.Query/BallotByIdentifier"
+	server := setupMockServer(t, observertypes.RegisterQueryServer, method, input, expectedOutput)
 	server.Serve()
-	defer func(server *grpcmock.Server) {
-		err := server.Close()
-		require.NoError(t, err)
-	}(server)
+	defer closeMockServer(t, server)
 
-	grpcConn, err := grpc.Dial(
-		fmt.Sprintf("%s:9090", "127.0.0.1"),
-		grpc.WithInsecure(),
-	)
-	client := observertypes.NewQueryClient(grpcConn)
-	resp, err := client.BallotByIdentifier(context.Background(), &observertypes.QueryBallotByIdentifierRequest{
-		BallotIdentifier: "123",
-	})
+	zetabridge, err := setupCorBridge()
+	require.NoError(t, err)
 
-	fmt.Println(resp)
+	resp, err := zetabridge.GetBallotByID("123")
+	require.NoError(t, err)
+	require.Equal(t, expectedOutput, *resp)
 }
