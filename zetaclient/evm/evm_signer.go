25ec0bc54c8e453597a7bfbd527723df4abdb4b3
kevinssgh <kevinssgh.github@gmail.com>
2024-03-25 11:59:25 -0400

    fix some unit tests

--- zetaclient/evm/evm_signer.go
+++ zetaclient/evm/evm_signer.go
@@ -676,7 +676,7 @@ func (signer *Signer) EvmSigner() ethtypes.Signer {
 func getEVMRPC(endpoint string) (interfaces.EVMRPCClient, ethtypes.Signer, error) {
 	if endpoint == stub.EVMRPCEnabled {
 		chainID := big.NewInt(common.BscMainnetChain().ChainId)
-		ethSigner := ethtypes.NewEIP155Signer(chainID)
+		ethSigner := ethtypes.NewLondonSigner(chainID)
 		client := &stub.MockEvmClient{}
 		return client, ethSigner, nil
 	}
