a409b7485688d40aa4e6d38e863d4c30aa698685
skosito <skostic9242@gmail.com>
2024-03-26 16:38:47 +0100

    PR comments

--- pkg/commands.go
+++
@@ -1,8 +0,0 @@
-package pkg
-
-// commands for the zetaclient; mostly administrative commands/txs
-
-const (
-	CmdWhitelistERC20  = "cmd_whitelist_erc20"
-	CmdMigrateTssFunds = "cmd_migrate_tss_funds"
-)
