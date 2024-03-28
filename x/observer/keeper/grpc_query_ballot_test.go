19b4d9009a8795ed549153fcab026986b06b0972
skosito <skostic9242@gmail.com>
2024-03-29 00:35:03 +0100

    Add some grpc query keeper tests

---
+++ x/observer/keeper/grpc_query_ballot_test.go
@@ -0,0 +1,136 @@
+package keeper_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+	keepertest "github.com/zeta-chain/zetacore/testutil/keeper"
+	"github.com/zeta-chain/zetacore/testutil/sample"
+	"github.com/zeta-chain/zetacore/x/observer/types"
+)
+
+func TestKeeper_HasVoted(t *testing.T) {
+	t.Run("should error if req is nil", func(t *testing.T) {
+		k, ctx, _, _ := keepertest.ObserverKeeper(t)
+		wctx := sdk.WrapSDKContext(ctx)
+
+		res, err := k.HasVoted(wctx, nil)
+		require.Nil(t, res)
+		require.Error(t, err)
+	})
+
+	t.Run("should return false if ballot not found", func(t *testing.T) {
+		k, ctx, _, _ := keepertest.ObserverKeeper(t)
+		wctx := sdk.WrapSDKContext(ctx)
+
+		res, err := k.HasVoted(wctx, &types.QueryHasVotedRequest{
+			BallotIdentifier: "test",
+		})
+		require.NoError(t, err)
+		require.Equal(t, &types.QueryHasVotedResponse{
+			HasVoted: false,
+		}, res)
+	})
+
+	t.Run("should return true if ballot found and voted", func(t *testing.T) {
+		k, ctx, _, _ := keepertest.ObserverKeeper(t)
+		wctx := sdk.WrapSDKContext(ctx)
+
+		voter := sample.AccAddress()
+		ballot := types.Ballot{
+			Index:            "index",
+			BallotIdentifier: "index",
+			VoterList:        []string{voter},
+			Votes:            []types.VoteType{types.VoteType_SuccessObservation},
+			BallotStatus:     types.BallotStatus_BallotInProgress,
+		}
+		k.SetBallot(ctx, &ballot)
+
+		res, err := k.HasVoted(wctx, &types.QueryHasVotedRequest{
+			BallotIdentifier: "index",
+			VoterAddress:     voter,
+		})
+		require.NoError(t, err)
+		require.Equal(t, &types.QueryHasVotedResponse{
+			HasVoted: true,
+		}, res)
+	})
+
+	t.Run("should return false if ballot found and not voted", func(t *testing.T) {
+		k, ctx, _, _ := keepertest.ObserverKeeper(t)
+		wctx := sdk.WrapSDKContext(ctx)
+
+		voter := sample.AccAddress()
+		ballot := types.Ballot{
+			Index:            "index",
+			BallotIdentifier: "index",
+			VoterList:        []string{voter},
+			Votes:            []types.VoteType{types.VoteType_SuccessObservation},
+			BallotStatus:     types.BallotStatus_BallotInProgress,
+		}
+		k.SetBallot(ctx, &ballot)
+
+		res, err := k.HasVoted(wctx, &types.QueryHasVotedRequest{
+			BallotIdentifier: "index",
+			VoterAddress:     sample.AccAddress(),
+		})
+		require.NoError(t, err)
+		require.Equal(t, &types.QueryHasVotedResponse{
+			HasVoted: false,
+		}, res)
+	})
+}
+
+func TestKeeper_BallotByIdentifier(t *testing.T) {
+	t.Run("should error if req is nil", func(t *testing.T) {
+		k, ctx, _, _ := keepertest.ObserverKeeper(t)
+		wctx := sdk.WrapSDKContext(ctx)
+
+		res, err := k.BallotByIdentifier(wctx, nil)
+		require.Nil(t, res)
+		require.Error(t, err)
+	})
+
+	t.Run("should return nil if ballot not found", func(t *testing.T) {
+		k, ctx, _, _ := keepertest.ObserverKeeper(t)
+		wctx := sdk.WrapSDKContext(ctx)
+
+		res, err := k.BallotByIdentifier(wctx, &types.QueryBallotByIdentifierRequest{
+			BallotIdentifier: "test",
+		})
+		require.Error(t, err)
+		require.Nil(t, res)
+	})
+
+	t.Run("should return ballot if exists", func(t *testing.T) {
+		k, ctx, _, _ := keepertest.ObserverKeeper(t)
+		wctx := sdk.WrapSDKContext(ctx)
+
+		voter := sample.AccAddress()
+		ballot := types.Ballot{
+			Index:            "index",
+			BallotIdentifier: "index",
+			VoterList:        []string{voter},
+			Votes:            []types.VoteType{types.VoteType_SuccessObservation},
+			BallotStatus:     types.BallotStatus_BallotInProgress,
+		}
+		k.SetBallot(ctx, &ballot)
+
+		res, err := k.BallotByIdentifier(wctx, &types.QueryBallotByIdentifierRequest{
+			BallotIdentifier: "index",
+		})
+		require.NoError(t, err)
+		require.Equal(t, &types.QueryBallotByIdentifierResponse{
+			BallotIdentifier: ballot.BallotIdentifier,
+			Voters: []*types.VoterList{
+				{
+					VoterAddress: voter,
+					VoteType:     types.VoteType_SuccessObservation,
+				},
+			},
+			ObservationType: ballot.ObservationType,
+			BallotStatus:    ballot.BallotStatus,
+		}, res)
+	})
+}
--- x/observer/keeper/grpc_query_keygen_test.go
+++ x/observer/keeper/grpc_query_keygen_test.go
@@ -1,4 +1,4 @@
-package keeper
+package keeper_test
 
 import (
 	"github.com/stretchr/testify/require"
@@ -6,33 +6,32 @@ import (
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	keepertest "github.com/zeta-chain/zetacore/testutil/keeper"
 	"github.com/zeta-chain/zetacore/x/observer/types"
 )
 
-func TestKeygenQuery(t *testing.T) {
-	keeper, ctx := SetupKeeper(t)
-	wctx := sdk.WrapSDKContext(ctx)
-	item := createTestKeygen(keeper, ctx)
-	for _, tc := range []struct {
-		desc     string
-		request  *types.QueryGetKeygenRequest
-		response *types.QueryGetKeygenResponse
-		err      error
-	}{
-		{
-			desc:     "First",
-			request:  &types.QueryGetKeygenRequest{},
-			response: &types.QueryGetKeygenResponse{Keygen: &item},
-		},
-	} {
-		tc := tc
-		t.Run(tc.desc, func(t *testing.T) {
-			response, err := keeper.Keygen(wctx, tc.request)
-			if tc.err != nil {
-				require.ErrorIs(t, err, tc.err)
-			} else {
-				require.Equal(t, tc.response, response)
-			}
-		})
-	}
+func TestKeeper_Keygen(t *testing.T) {
+	t.Run("should error if keygen not found", func(t *testing.T) {
+		k, ctx, _, _ := keepertest.ObserverKeeper(t)
+		wctx := sdk.WrapSDKContext(ctx)
+
+		res, err := k.Keygen(wctx, &types.QueryGetKeygenRequest{})
+		require.Nil(t, res)
+		require.Error(t, err)
+	})
+
+	t.Run("should return if keygen found", func(t *testing.T) {
+		k, ctx, _, _ := keepertest.ObserverKeeper(t)
+		wctx := sdk.WrapSDKContext(ctx)
+		keygen := types.Keygen{
+			BlockNumber: 10,
+		}
+		k.SetKeygen(ctx, keygen)
+
+		res, err := k.Keygen(wctx, &types.QueryGetKeygenRequest{})
+		require.NoError(t, err)
+		require.Equal(t, &types.QueryGetKeygenResponse{
+			Keygen: &keygen,
+		}, res)
+	})
 }
