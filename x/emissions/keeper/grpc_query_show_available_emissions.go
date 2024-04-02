62999f666cfe9361ce2877c4d6a75011695f1cd9
skosito <skostic9242@gmail.com>
2024-04-02 18:42:03 +0100

    test: improve emissions module coverage (#1955)

--- x/emissions/keeper/grpc_query_show_available_emissions.go
+++ x/emissions/keeper/grpc_query_show_available_emissions.go
@@ -5,11 +5,17 @@ import (
 
 	"github.com/zeta-chain/zetacore/cmd/zetacored/config"
 	"github.com/zeta-chain/zetacore/x/emissions/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k Keeper) ShowAvailableEmissions(goCtx context.Context, req *types.QueryShowAvailableEmissionsRequest) (*types.QueryShowAvailableEmissionsResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	emissions, found := k.GetWithdrawableEmission(ctx, req.Address)
 	if !found {
---
+++ x/emissions/keeper/grpc_query_show_available_emissions_test.go
@@ -0,0 +1,56 @@
+package keeper_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+	"github.com/zeta-chain/zetacore/cmd/zetacored/config"
+	keepertest "github.com/zeta-chain/zetacore/testutil/keeper"
+	"github.com/zeta-chain/zetacore/testutil/sample"
+	"github.com/zeta-chain/zetacore/x/emissions/types"
+)
+
+func TestKeeper_ShowAvailableEmissions(t *testing.T) {
+	t.Run("should error if req is nil", func(t *testing.T) {
+		k, ctx, _, _ := keepertest.EmissionsKeeper(t)
+		wctx := sdk.WrapSDKContext(ctx)
+
+		res, err := k.ShowAvailableEmissions(wctx, nil)
+		require.Nil(t, res)
+		require.Error(t, err)
+	})
+
+	t.Run("should return 0 if emissions not found", func(t *testing.T) {
+		k, ctx, _, _ := keepertest.EmissionsKeeper(t)
+		wctx := sdk.WrapSDKContext(ctx)
+
+		req := &types.QueryShowAvailableEmissionsRequest{
+			Address: sample.AccAddress(),
+		}
+		res, err := k.ShowAvailableEmissions(wctx, req)
+		require.NoError(t, err)
+		expectedRes := &types.QueryShowAvailableEmissionsResponse{
+			Amount: sdk.NewCoin(config.BaseDenom, sdk.ZeroInt()).String(),
+		}
+		require.Equal(t, expectedRes, res)
+	})
+
+	t.Run("should return emissions if found", func(t *testing.T) {
+		k, ctx, _, _ := keepertest.EmissionsKeeper(t)
+		wctx := sdk.WrapSDKContext(ctx)
+
+		emissions := sample.WithdrawableEmissions(t)
+		k.SetWithdrawableEmission(ctx, emissions)
+
+		req := &types.QueryShowAvailableEmissionsRequest{
+			Address: emissions.Address,
+		}
+		res, err := k.ShowAvailableEmissions(wctx, req)
+		require.NoError(t, err)
+		expectedRes := &types.QueryShowAvailableEmissionsResponse{
+			Amount: sdk.NewCoin(config.BaseDenom, emissions.Amount).String(),
+		}
+		require.Equal(t, expectedRes, res)
+	})
+}
