785e788c193d1fd87c75b6cef4e65306a2ae1fb5
lumtis <lucas.bertrand.22@gmail.com>
2024-04-17 13:10:16 +0200

    update verification flags comment

--- x/lightclient/keeper/msg_server_update_verification_flags.go
+++ x/lightclient/keeper/msg_server_update_verification_flags.go
@@ -10,7 +10,7 @@ import (
 
 // UpdateVerificationFlags updates the light client verification flags.
 // This disables/enables blocks verification of the light client for the specified chain.
-// Emergency group can disable flags while operation group can enable/disable
+// Emergency group can disable flags, it requires operational group if at least one flag is being enabled
 func (k msgServer) UpdateVerificationFlags(goCtx context.Context, msg *types.MsgUpdateVerificationFlags) (
 	*types.MsgUpdateVerificationFlagsResponse,
 	error,
