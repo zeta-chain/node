c8f95dbbce36fff0c177b64d1539934bc5174f40
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2024-04-24 16:28:03 -0400

    add e2e tests

--- e2e/e2etests/test_zeta_deposit.go
+++ e2e/e2etests/test_zeta_deposit.go
@@ -6,6 +6,7 @@ import (
 	ethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/zeta-chain/zetacore/e2e/runner"
 	"github.com/zeta-chain/zetacore/e2e/utils"
+	"github.com/zeta-chain/zetacore/testutil/sample"
 	"github.com/zeta-chain/zetacore/zetaclient/testutils"
 )
 
@@ -20,6 +21,24 @@ func TestZetaDeposit(r *runner.E2ERunner, args []string) {
 	}
 
 	hash := r.DepositZetaWithAmount(r.DeployerAddress, amount)
+
+	// wait for the cctx to be mined
+	cctx := utils.WaitCctxMinedByInTxHash(r.Ctx, hash.Hex(), r.CctxClient, r.Logger, r.CctxTimeout)
+	r.Logger.CCTX(*cctx, "deposit")
+}
+
+func TestZetaDepositNewAddress(r *runner.E2ERunner, args []string) {
+	if len(args) != 1 {
+		panic("TestZetaDeposit requires exactly one argument for the amount.")
+	}
+
+	amount, ok := big.NewInt(0).SetString(args[0], 10)
+	if !ok {
+		panic("Invalid amount specified for TestZetaDeposit.")
+	}
+
+	newAddress := sample.EthAddress()
+	hash := r.DepositZetaWithAmount(newAddress, amount)
 
 	// wait for the cctx to be mined
 	cctx := utils.WaitCctxMinedByInTxHash(r.Ctx, hash.Hex(), r.CctxClient, r.Logger, r.CctxTimeout)
