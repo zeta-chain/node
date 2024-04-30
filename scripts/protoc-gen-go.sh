aa4351930ddb465729e6ce36e3971fe3df2315dc
skosito <skostic9242@gmail.com>
2024-04-30 17:40:29 +0200

    cleanup

--- scripts/protoc-gen-go.sh
+++ scripts/protoc-gen-go.sh
@@ -3,25 +3,17 @@
 set -eo pipefail
 
 echo "Generating gogo proto code"
-echo "$PWD"
-echo */
 cd proto
-echo "$PWD"
 proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  echo $dir
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep "option go_package" $file &> /dev/null ; then
       buf generate --template buf.gen.gogo.yaml $file
     fi
   done
 done
-echo */
-cd zetachain
-echo */
-cd ../..
-echo "$PWD"
-echo */
+
+cd ..
 
 # Move proto files to the right places.
 cp -r github.com/zeta-chain/zetacore/* ./
