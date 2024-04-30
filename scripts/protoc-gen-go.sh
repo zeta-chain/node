2c6386a68613b554f1379c0de06731b1bc1f7678
skosito <skostic9242@gmail.com>
2024-04-30 15:48:39 +0200

    add some logs to proto gen script

--- scripts/protoc-gen-go.sh
+++ scripts/protoc-gen-go.sh
@@ -3,9 +3,13 @@
 set -eo pipefail
 
 echo "Generating gogo proto code"
+echo "$PWD"
+echo */
 cd proto
+echo "$PWD"
 proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
+  echo $dir
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep "option go_package" $file &> /dev/null ; then
       buf generate --template buf.gen.gogo.yaml $file
@@ -14,6 +18,8 @@ for dir in $proto_dirs; do
 done
 
 cd ..
+echo "$PWD"
+echo */
 
 # Move proto files to the right places.
 cp -r github.com/zeta-chain/zetacore/* ./
