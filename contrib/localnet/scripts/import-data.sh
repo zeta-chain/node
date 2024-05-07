f9c8a884a4d3d7e4eb346791d8f9ca80b110a62a
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2024-05-07 01:44:11 -0400

    parameterize import data script

--- contrib/localnet/scripts/import-data.sh
+++ contrib/localnet/scripts/import-data.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+if [ $# -lt 1 ]
+then
+  echo "Usage: import-data.sh [network]"
+  exit 1
+fi
+
+NETWORK=$1
+echo "NETWORK: ${NETWORK}"
 mkdir ~/genesis_export/
 echo "Download Latest State Export"
-LATEST_EXPORT_URL=$(curl https://snapshots.zetachain.com/latest-state-export | jq -r .mainnet)
+LATEST_EXPORT_URL=$(curl https://snapshots.zetachain.com/latest-state-export | jq -r ."${NETWORK}")
 echo "LATEST EXPORT URL: ${LATEST_EXPORT_URL}"
 wget -q ${LATEST_EXPORT_URL} -O ~/genesis_export/exported-genesis.json
