856811094fb655400fcab55ef8b2260e6cf62c60
skosito <skostic9242@gmail.com>
2024-05-14 14:38:42 +0200

    POC for stress tests txs priority

--- e2e/e2etests/test_stress_eth_deposit.go
+++ e2e/e2etests/test_stress_eth_deposit.go
@@ -1,11 +1,14 @@
 package e2etests
 
 import (
+	"context"
 	"fmt"
 	"math/big"
 	"strconv"
+	"strings"
 	"time"
 
+	rpchttp "github.com/cometbft/cometbft/rpc/client/http"
 	ethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/zeta-chain/zetacore/e2e/runner"
 	"github.com/zeta-chain/zetacore/e2e/utils"
@@ -33,6 +36,13 @@ func TestStressEtherDeposit(r *runner.E2ERunner, args []string) {
 
 	// create a wait group to wait for all the deposits to complete
 	var eg errgroup.Group
+
+	// cancel MonitorTxPriorityInBlocks when test completes
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go func() {
+		MonitorTxPriorityInBlocks(r, ctx)
+	}()
 
 	// send the deposits
 	for i := 0; i < numDeposits; i++ {
@@ -69,4 +79,59 @@ func MonitorEtherDeposit(r *runner.E2ERunner, hash ethcommon.Hash, index int, st
 	r.Logger.Print("index %d: deposit cctx success in %s", index, timeToComplete.String())
 
 	return nil
+}
+
+func MonitorTxPriorityInBlocks(r *runner.E2ERunner, ctx context.Context) {
+	url := "http://zetacore0:26657" // TODO: init rpc client in runner?
+	rpc, err := rpchttp.New(url, "/websocket")
+	if err != nil {
+		return
+	}
+
+	ticker := time.NewTicker(2 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			block, err := rpc.Block(ctx, nil)
+			if err != nil {
+				return
+			}
+			r.Logger.Print("block %d has %d txs", block.Block.Height, len(block.Block.Txs))
+
+			// iterate txs events and check if some MsgEthereumTx is included above crosschain and observer txs
+			nonSystemTxFound := false
+			for _, tx := range block.Block.Txs {
+				txRes, err := rpc.Tx(context.Background(), tx.Hash(), false)
+				if err != nil {
+					continue
+				}
+
+				for _, ev := range txRes.TxResult.Events {
+					for _, attr := range ev.Attributes {
+						if attr.Key == "msg_type_url" {
+							if strings.Contains(attr.Value, "zetachain.zetacore.crosschain.Msg") ||
+								strings.Contains(attr.Value, "zetachain.zetacore.observer.Msg") {
+								r.Logger.Print("system tx %s", attr.Value)
+								if nonSystemTxFound {
+									// TODO: communicate failure to main routine
+									r.Logger.Print("wrong priority")
+									panic("wrong priority")
+								}
+							}
+						}
+						if attr.Key == "action" {
+							if strings.Contains(attr.Value, "ethermint.evm.v1.MsgEthereumTx") {
+								nonSystemTxFound = true
+								r.Logger.Print("non system tx %s", attr.Value)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
