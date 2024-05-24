01a590d5004f44fbb3ea51c65e5e627634f0f20d
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2024-05-24 06:55:33 -0400

    add import data script

--- contrib/localnet/scripts/import-data.sh
+++ contrib/localnet/scripts/import-data.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#if [ $# -lt 1 ]
-#then
-#  echo "Usage: import-data.sh [network]"
-#  exit 1
-#fi
-#
-#NETWORK=$1
-#echo "NETWORK: ${NETWORK}"
-#rm -rf ~/genesis_export/
-#mkdir ~/genesis_export/
-#echo "Download Latest State Export"
-#LATEST_EXPORT_URL=$(curl https://snapshots.zetachain.com/latest-state-export | jq -r ."${NETWORK}")
-#echo "LATEST EXPORT URL: ${LATEST_EXPORT_URL}"
-#wget -q ${LATEST_EXPORT_URL} -O ~/genesis_export/exported-genesis.json
+if [ $# -lt 1 ]
+then
+  echo "Usage: import-data.sh [network]"
+  exit 1
+fi
+
+NETWORK=$1
+echo "NETWORK: ${NETWORK}"
+rm -rf ~/genesis_export/
+mkdir ~/genesis_export/
+echo "Download Latest State Export"
+LATEST_EXPORT_URL=$(curl https://snapshots.zetachain.com/latest-state-export | jq -r ."${NETWORK}")
+echo "LATEST EXPORT URL: ${LATEST_EXPORT_URL}"
+wget -q ${LATEST_EXPORT_URL} -O ~/genesis_export/exported-genesis.json
