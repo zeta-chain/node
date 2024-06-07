eccd52e40864f394470cd8b1f85487ff8e6f38ba
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2024-06-07 15:53:53 -0400

    add migration for authorization list

---
+++ x/authority/keeper/migrator.go
@@ -0,0 +1,23 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	v2 "github.com/zeta-chain/zetacore/x/authority/migrations/v2"
+)
+
+// Migrator is a struct for handling in-place store migrations.
+type Migrator struct {
+	authorityKeeper Keeper
+}
+
+// NewMigrator returns a new Migrator for the authority module.
+func NewMigrator(keeper Keeper) Migrator {
+	return Migrator{
+		authorityKeeper: keeper,
+	}
+}
+
+// Migrate1to2 migrates the authority store from consensus version 1 to 2
+func (m Migrator) Migrate1to2(ctx sdk.Context) error {
+	return v2.MigrateStore(ctx, m.authorityKeeper)
+}
