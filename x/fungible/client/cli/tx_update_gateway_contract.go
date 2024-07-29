9cdae6c118092e5d111f8c920840f5b8ec77af58
lumtis <lucas.bertrand.22@gmail.com>
2024-07-29 17:30:21 +0200

    cli

---
+++ x/fungible/client/cli/tx_update_gateway_contract.go
@@ -0,0 +1,35 @@
+package cli
+
+import (
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/spf13/cobra"
+
+	"github.com/zeta-chain/zetacore/x/fungible/types"
+)
+
+func CmdUpdateGatewayContract() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "update-gateway-contract [contract-address] ",
+		Short: "Broadcast message UpdateGatewayContract to update the gateway contract address",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgUpdateGatewayContract(clientCtx.GetFromAddress().String(), args[0])
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
