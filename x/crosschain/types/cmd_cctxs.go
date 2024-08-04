2a026cd7ed8a1921de6c3dd26bb2b28f5843a0aa
lumtis <lucas.bertrand.22@gmail.com>
2024-08-04 14:35:44 +0200

    refactor cmd cctx creation

---
+++ x/crosschain/types/cmd_cctxs.go
@@ -0,0 +1,196 @@
+package types
+
+import (
+	"fmt"
+
+	errorsmod "cosmossdk.io/errors"
+	sdkmath "cosmossdk.io/math"
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+
+	"github.com/zeta-chain/zetacore/pkg/chains"
+	"github.com/zeta-chain/zetacore/pkg/coin"
+	"github.com/zeta-chain/zetacore/pkg/constant"
+	zetacrypto "github.com/zeta-chain/zetacore/pkg/crypto"
+	"github.com/zeta-chain/zetacore/pkg/gas"
+)
+
+// WhitelistERC20CmdCCTX returns a CCTX allowing to whitelist an ERC20 token on an external chain
+func WhitelistERC20CmdCCTX(
+	creator string,
+	zrc20Address ethcommon.Address,
+	erc20Address string,
+	custodyContractAddress string,
+	chainID int64,
+	gasPrice string,
+	priorityFee string,
+	tssPubKey string,
+) CrossChainTx {
+	// calculate the cctx index
+	// we use the deployed zrc20 contract address to generate a unique index
+	// since other parts of the system may use the zrc20 for the index, we add a message specific suffix
+	hash := crypto.Keccak256Hash(zrc20Address.Bytes(), []byte("WhitelistERC20"))
+
+	return newCmdCCTX(
+		creator,
+		hash.Hex(),
+		fmt.Sprintf("%s:%s", constant.CmdWhitelistERC20, erc20Address),
+		"",
+		hash.String(),
+		custodyContractAddress,
+		chainID,
+		sdkmath.NewUint(0),
+		100_000,
+		gasPrice,
+		priorityFee,
+		tssPubKey,
+	)
+}
+
+// MigrateFundCmdCCTX returns a CCTX allowing to migrate funds from the current TSS to the new TSS
+func MigrateFundCmdCCTX(
+	blockHeight int64,
+	creator string,
+	inboundHash string,
+	chainID int64,
+	amount sdkmath.Uint,
+	medianGasPrice sdkmath.Uint,
+	priorityFee sdkmath.Uint,
+	currentTSSPubKey string,
+	newTSSPubKey string,
+	additionalStaticChainInfo []chains.Chain,
+) (CrossChainTx, error) {
+	var (
+		sender      string
+		receiver    string
+		gasLimit    uint64
+		gasPrice    string
+		finalAmount sdkmath.Uint
+	)
+
+	// set sender, receiver, gas limit, gas price and final amount based on the chain
+	switch {
+	case chains.IsEVMChain(chainID, additionalStaticChainInfo):
+		ethAddressOld, err := zetacrypto.GetTssAddrEVM(currentTSSPubKey)
+		if err != nil {
+			return CrossChainTx{}, err
+		}
+		ethAddressNew, err := zetacrypto.GetTssAddrEVM(newTSSPubKey)
+		if err != nil {
+			return CrossChainTx{}, err
+		}
+		sender = ethAddressOld.String()
+		receiver = ethAddressNew.String()
+		gasLimit = gas.EVMSend
+		gasPriceUint, err := gas.MultiplyGasPrice(medianGasPrice, TssMigrationGasMultiplierEVM)
+		if err != nil {
+			return CrossChainTx{}, err
+		}
+		evmFee := sdkmath.NewUint(gasLimit).
+			Mul(gasPriceUint).
+			Add(sdkmath.NewUintFromString(TSSMigrationBufferAmountEVM))
+		if evmFee.GT(amount) {
+			return CrossChainTx{}, errorsmod.Wrap(
+				ErrInsufficientFundsTssMigration,
+				fmt.Sprintf(
+					"insufficient funds to pay for gas fee, amount: %s, gas fee: %s, chainid: %d",
+					amount.String(),
+					evmFee.String(),
+					chainID,
+				),
+			)
+		}
+		gasPrice = gasPriceUint.String()
+		finalAmount = amount.Sub(evmFee)
+	case chains.IsBitcoinChain(chainID, additionalStaticChainInfo):
+		bitcoinNetParams, err := chains.BitcoinNetParamsFromChainID(chainID)
+		if err != nil {
+			return CrossChainTx{}, err
+		}
+		btcAddressOld, err := zetacrypto.GetTssAddrBTC(currentTSSPubKey, bitcoinNetParams)
+		if err != nil {
+			return CrossChainTx{}, err
+		}
+		btcAddressNew, err := zetacrypto.GetTssAddrBTC(newTSSPubKey, bitcoinNetParams)
+		if err != nil {
+			return CrossChainTx{}, err
+		}
+		sender = btcAddressOld
+		receiver = btcAddressNew
+		gasLimit = 1_000_000
+		gasPrice = medianGasPrice.MulUint64(2).String()
+		finalAmount = amount
+	default:
+		return CrossChainTx{}, errorsmod.Wrap(ErrUnsupportedChain, fmt.Sprintf("chain %d is not supported", chainID))
+	}
+
+	indexString := GetTssMigrationCCTXIndexString(currentTSSPubKey, newTSSPubKey, chainID, amount, blockHeight)
+	hash := crypto.Keccak256Hash([]byte(indexString))
+
+	return newCmdCCTX(
+		creator,
+		hash.Hex(),
+		fmt.Sprintf("%s:%s", constant.CmdMigrateTssFunds, "Funds Migrator Admin Cmd"),
+		sender,
+		inboundHash,
+		receiver,
+		chainID,
+		finalAmount,
+		gasLimit,
+		gasPrice,
+		priorityFee.MulUint64(2).String(),
+		newTSSPubKey,
+	), nil
+}
+
+// GetTssMigrationCCTXIndexString returns the index string of the CCTX for migrating funds from the current TSS to the new TSS
+func GetTssMigrationCCTXIndexString(
+	currentTssPubkey, newTssPubkey string,
+	chainID int64,
+	amount sdkmath.Uint,
+	height int64,
+) string {
+	return fmt.Sprintf("%s-%s-%d-%s-%d", currentTssPubkey, newTssPubkey, chainID, amount.String(), height)
+}
+
+// newCmdCCTX returns a new CCTX for admin cmd with the given parameters
+func newCmdCCTX(
+	creator string,
+	index string,
+	relayedMessage,
+	sender string,
+	inboundHash string,
+	receiver string,
+	chainID int64,
+	amount sdkmath.Uint,
+	gasLimit uint64,
+	medianGasPrice string,
+	priorityFee string,
+	tssPubKey string,
+) CrossChainTx {
+	return CrossChainTx{
+		Creator:        creator,
+		Index:          index,
+		RelayedMessage: relayedMessage,
+		CctxStatus: &Status{
+			Status: CctxStatus_PendingOutbound,
+		},
+		InboundParams: &InboundParams{
+			Sender:       sender,
+			CoinType:     coin.CoinType_Cmd,
+			ObservedHash: inboundHash,
+		},
+		OutboundParams: []*OutboundParams{
+			{
+				Receiver:        receiver,
+				ReceiverChainId: chainID,
+				CoinType:        coin.CoinType_Cmd,
+				Amount:          amount,
+				GasLimit:        gasLimit,
+				GasPrice:        medianGasPrice,
+				GasPriorityFee:  priorityFee,
+				TssPubkey:       tssPubKey,
+			},
+		},
+	}
+}
