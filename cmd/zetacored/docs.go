6055c47c2a572fb3e970f94bbb92bf8c942ebd31
Denis Fadeev <denis@fadeev.org>
2024-08-07 13:53:26 +0500

    ci: generate zetacored CLI docs in a single file

--- cmd/zetacored/docs.go
+++ cmd/zetacored/docs.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"os"
-
+	"bytes"
+	"strings"
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
@@ -10,7 +11,7 @@ import (
 func docsCmd(cmd *cobra.Command, args []string) error {
 	var path string
 
-	// If path is provided as an argument, use it. Else, get from flag.
+	// Determine the output path
 	if len(args) > 0 {
 		path = args[0]
 	} else {
@@ -21,6 +22,7 @@ func docsCmd(cmd *cobra.Command, args []string) error {
 		}
 	}
 
+	// Create the output directory if it doesn't exist
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		err = os.MkdirAll(path, 0750)
 		if err != nil {
@@ -28,18 +30,62 @@ func docsCmd(cmd *cobra.Command, args []string) error {
 		}
 	}
 
-	err := doc.GenMarkdownTree(cmd.Root(), path)
+	// Set the output file
+	outputFile := path + "/cli.md"
+	file, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Generate the documentation
+	err = GenMarkdownToSingleFile(cmd.Root(), file)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
+// GenMarkdownToSingleFile generates markdown documentation for all commands into a single file
+func GenMarkdownToSingleFile(cmd *cobra.Command, w *os.File) error {
+	buf := new(bytes.Buffer)
+	linkHandler := func(s string) string {
+		anchor := strings.ReplaceAll(strings.ToLower(s), " ", "-")
+		anchor = strings.ReplaceAll(anchor, "_", "-")
+		return "#" + anchor
+	}
+
+	cmd.DisableAutoGenTag = true
+
+	err := doc.GenMarkdownCustom(cmd, buf, linkHandler)
+	if err != nil {
+		return err
+	}
+
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		return err
+	}
+
+	for _, c := range cmd.Commands() {
+		if !c.IsAvailableCommand() || c.IsAdditionalHelpTopicCommand() {
+			continue
+		}
+		err := GenMarkdownToSingleFile(c, w)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+
 func docsCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "docs [path]",
 		Short: "Generate markdown documentation for zetacored",
-		RunE:  docsCmd,
+		RunE:  docsCmd, 
 		Args:  cobra.MaximumNArgs(1),
 	}
 
