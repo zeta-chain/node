0da87abbfa882e8577b7fb41f7f4e90756c37e7c
lumtis <lucas.bertrand.22@gmail.com>
2024-08-13 14:08:58 +0200

    fix test

--- cmd/zetae2e/local/local.go
+++ cmd/zetae2e/local/local.go
@@ -344,39 +344,39 @@ func localE2ETest(cmd *cobra.Command, _ []string) {
 		// Test happy paths for gas token workflow
 		eg.Go(v2TestRoutine(conf, "eth", conf.AdditionalAccounts.UserEther, color.FgHiGreen, deployerRunner, verbose,
 			e2etests.TestV2ETHDepositName,
-			//e2etests.TestV2ETHDepositAndCallName,
-			//e2etests.TestV2ETHWithdrawName,
-			//e2etests.TestV2ETHWithdrawAndCallName,
-			//e2etests.TestV2ZEVMToEVMCallName,
-			//e2etests.TestV2EVMToZEVMCallName,
+			e2etests.TestV2ETHDepositAndCallName,
+			e2etests.TestV2ETHWithdrawName,
+			e2etests.TestV2ETHWithdrawAndCallName,
+			e2etests.TestV2ZEVMToEVMCallName,
+			e2etests.TestV2EVMToZEVMCallName,
 		))
 
 		// Test happy paths for erc20 token workflow
 		eg.Go(v2TestRoutine(conf, "erc20", conf.AdditionalAccounts.UserERC20, color.FgHiBlue, deployerRunner, verbose,
 			e2etests.TestV2ETHDepositName, // necessary to pay fees on ZEVM
-			//e2etests.TestV2ERC20DepositName,
-			//e2etests.TestV2ERC20DepositAndCallName,
-			//e2etests.TestV2ERC20WithdrawName,
-			//e2etests.TestV2ERC20WithdrawAndCallName,
+			e2etests.TestV2ERC20DepositName,
+			e2etests.TestV2ERC20DepositAndCallName,
+			e2etests.TestV2ERC20WithdrawName,
+			e2etests.TestV2ERC20WithdrawAndCallName,
 		))
 
-		// Test revert cases for gas token workflow
-		eg.Go(v2TestRoutine(conf, "eth-revert", conf.AdditionalAccounts.UserEther, color.FgHiGreen, deployerRunner, verbose,
-			e2etests.TestV2ETHDepositName,
-			//e2etests.TestV2ETHDepositAndCallRevertName,
-			//e2etests.TestV2ETHDepositAndCallRevertWithCallName,
-			//e2etests.TestV2ETHWithdrawAndCallRevertName,
-			//e2etests.TestV2ETHWithdrawAndCallRevertWithCallName,
-		))
-
-		// Test revert cases for erc20 token workflow
-		eg.Go(v2TestRoutine(conf, "erc20-revert", conf.AdditionalAccounts.UserERC20, color.FgHiBlue, deployerRunner, verbose,
-			e2etests.TestV2ETHDepositName, // necessary to pay fees on ZEVM
-			//e2etests.TestV2ERC20DepositAndCallRevertName,
-			//e2etests.TestV2ERC20DepositAndCallRevertWithCallName,
-			//e2etests.TestV2ERC20WithdrawAndCallRevertName,
-			//e2etests.TestV2ERC20WithdrawAndCallRevertWithCallName,
-		))
+		//// Test revert cases for gas token workflow
+		//eg.Go(v2TestRoutine(conf, "eth-revert", conf.AdditionalAccounts.UserEther, color.FgHiGreen, deployerRunner, verbose,
+		//	e2etests.TestV2ETHDepositName,
+		//	//e2etests.TestV2ETHDepositAndCallRevertName,
+		//	//e2etests.TestV2ETHDepositAndCallRevertWithCallName,
+		//	//e2etests.TestV2ETHWithdrawAndCallRevertName,
+		//	//e2etests.TestV2ETHWithdrawAndCallRevertWithCallName,
+		//))
+		//
+		//// Test revert cases for erc20 token workflow
+		//eg.Go(v2TestRoutine(conf, "erc20-revert", conf.AdditionalAccounts.UserERC20, color.FgHiBlue, deployerRunner, verbose,
+		//	e2etests.TestV2ETHDepositName, // necessary to pay fees on ZEVM
+		//	//e2etests.TestV2ERC20DepositAndCallRevertName,
+		//	//e2etests.TestV2ERC20DepositAndCallRevertWithCallName,
+		//	//e2etests.TestV2ERC20WithdrawAndCallRevertName,
+		//	//e2etests.TestV2ERC20WithdrawAndCallRevertWithCallName,
+		//))
 
 	}
 
