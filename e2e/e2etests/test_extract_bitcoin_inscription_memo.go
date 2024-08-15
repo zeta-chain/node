39435742be3f11f6db50a885537f91ed06d60cab
dev <dev@bitsmiley.io>
2024-08-15 16:07:12 +0800

    test

--- e2e/e2etests/test_extract_bitcoin_inscription_memo.go
+++ e2e/e2etests/test_extract_bitcoin_inscription_memo.go
@@ -1,6 +1,9 @@
 package e2etests
 
 import (
+	"encoding/hex"
+	"github.com/btcsuite/btcd/btcjson"
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/require"
 	zetabitcoin "github.com/zeta-chain/zetacore/zetaclient/chains/bitcoin"
 	btcobserver "github.com/zeta-chain/zetacore/zetaclient/chains/bitcoin/observer"
@@ -9,13 +12,29 @@ import (
 )
 
 func TestExtractBitcoinInscriptionMemo(r *runner.E2ERunner, args []string) {
-	require.Len(r, args, 2)
+	r.Logger.Info("Testing extract memo from btc inscription")
 
 	r.SetBtcAddress(r.Name, false)
 
-	r.InscribeToTSSFromDeployerWithMemo()
-	_, err := r.GenerateToAddressIfLocalBitcoin(6, r.BTCDeployerAddress)
+	// obtain some initial fund
+	stop := r.MineBlocksIfLocalBitcoin()
+	defer stop()
+
+	// list deployer utxos
+	utxos, err := r.ListDeployerUTXOs()
 	require.NoError(r, err)
+
+	amount := parseFloat(r, args[0])
+	memo, _ := hex.DecodeString(
+		"72f080c854647755d0d9e6f6821f6931f855b9acffd53d87433395672756d58822fd143360762109ab898626556b1c3b8d3096d2361f1297df4a41c1b429471a9aa2fc9be5f27c13b3863d6ac269e4b587d8389f8fd9649859935b0d48dea88cdb40f20c",
+	)
+
+	txid, err := r.InscribeToTSSFromDeployerWithMemo(amount, utxos, memo)
+	require.NoError(r, err)
+
+	_, err = r.GenerateToAddressIfLocalBitcoin(6, r.BTCDeployerAddress)
+	require.NoError(r, err)
+
 	gtx, err := r.BtcRPCClient.GetTransaction(txid)
 	require.NoError(r, err)
 	r.Logger.Info("rawtx confirmation: %d", gtx.BlockIndex)
