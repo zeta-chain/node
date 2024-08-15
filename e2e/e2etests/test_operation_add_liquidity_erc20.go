fc67bba13ab6eafe4297373369b2003b75aca725
lumtis <lucas.bertrand.22@gmail.com>
2024-08-15 13:49:59 +0200

    fix liquidity test

--- e2e/e2etests/test_operation_add_liquidity_erc20.go
+++ e2e/e2etests/test_operation_add_liquidity_erc20.go
@@ -8,15 +8,18 @@ import (
 	"github.com/zeta-chain/zetacore/e2e/runner"
 )
 
-// TestOperationAddLiquidityETH is an operational test to add liquidity in gas token
-func TestOperationAddLiquidityETH(r *runner.E2ERunner, args []string) {
+// TestOperationAddLiquidityERC20 is an operational test to add liquidity in erc20 token
+func TestOperationAddLiquidityERC20(r *runner.E2ERunner, args []string) {
 	require.Len(r, args, 2)
 
-	// #nosec G115 e2e - always in range
-	liqZETA := big.NewInt(int64(parseInt(r, args[0])))
-	// #nosec G115 e2e - always in range
-	liqETH := big.NewInt(int64(parseInt(r, args[1])))
+	liqZETA := big.NewInt(0)
+	_, ok := liqZETA.SetString(args[0], 10)
+	require.True(r, ok, "Invalid amount specified for TestOperationAddLiquidityERC20")
+
+	liqERC20 := big.NewInt(0)
+	_, ok = liqERC20.SetString(args[1], 10)
+	require.True(r, ok, "Invalid amount specified for TestOperationAddLiquidityERC20")
 
 	// perform the add liquidity
-	r.AddLiquidityETH(liqZETA, liqETH)
+	r.AddLiquidityERC20(liqZETA, liqERC20)
 }
--- e2e/e2etests/test_operation_add_liquidity_eth.go
+++ e2e/e2etests/test_operation_add_liquidity_eth.go
@@ -8,15 +8,18 @@ import (
 	"github.com/zeta-chain/zetacore/e2e/runner"
 )
 
-// TestOperationAddLiquidityERC20 is an operational test to add liquidity in erc20 token
-func TestOperationAddLiquidityERC20(r *runner.E2ERunner, args []string) {
+// TestOperationAddLiquidityETH is an operational test to add liquidity in gas token
+func TestOperationAddLiquidityETH(r *runner.E2ERunner, args []string) {
 	require.Len(r, args, 2)
 
-	// #nosec G115 e2e - always in range
-	liqZETA := big.NewInt(int64(parseInt(r, args[0])))
-	// #nosec G115 e2e - always in range
-	liqERC20 := big.NewInt(int64(parseInt(r, args[1])))
+	liqZETA := big.NewInt(0)
+	_, ok := liqZETA.SetString(args[0], 10)
+	require.True(r, ok, "Invalid amount specified for TestOperationAddLiquidityETH")
+
+	liqETH := big.NewInt(0)
+	_, ok = liqETH.SetString(args[1], 10)
+	require.True(r, ok, "Invalid amount specified for TestOperationAddLiquidityETH")
 
 	// perform the add liquidity
-	r.AddLiquidityERC20(liqZETA, liqERC20)
+	r.AddLiquidityETH(liqZETA, liqETH)
 }
