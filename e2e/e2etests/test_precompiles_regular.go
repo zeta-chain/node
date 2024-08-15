fadf37a0e4a2b119ceebea4a7288a5d46e30979e
Francisco de Borja Aranda Castillejo <me@fbac.dev>
2024-08-15 15:37:32 +0200

    add RegularCaller contract to e2e

--- e2e/e2etests/test_precompiles_regular.go
+++ e2e/e2etests/test_precompiles_regular.go
@@ -1,27 +1,29 @@
 package e2etests
 
 import (
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/stretchr/testify/require"
 
 	"github.com/zeta-chain/zetacore/e2e/runner"
-	"github.com/zeta-chain/zetacore/precompiles/regular"
+	"github.com/zeta-chain/zetacore/e2e/utils"
+	regularcaller "github.com/zeta-chain/zetacore/precompiles/regular/testutil"
 )
 
 func TestPrecompilesRegular(r *runner.E2ERunner, args []string) {
 	require.Len(r, args, 0, "No arguments expected")
 
-	dummyBech32Addr := "1h8duy2dltz9xz0qqhm5wvcnj02upy887fyn43u"
+	_, tx, contract, err := regularcaller.DeployRegularCaller(&bind.TransactOpts{}, r.ZEVMClient)
+	require.NoError(r, err, "Failed to deploy RegularCaller contract")
+
+	receipt := utils.MustWaitForTxReceipt(r.Ctx, r.ZEVMClient, tx, r.Logger, r.ReceiptTimeout)
+	require.Equal(r, receipt.Status, 1, "Failed to deploy RegularCaller contract")
 
 	// Call the Regular contract in the static precompile address.
-	contract, err := regular.NewRegular(regular.ContractAddress, r.EVMClient)
+	ok, err := contract.TestBech32ToHexAddr(&bind.CallOpts{})
 	require.NoError(r, err, "Failed to create Regular contract caller")
+	require.True(r, ok, "Failed to validate Bech32ToHexAddr function")
 
-	addr, err := contract.Bech32ToHexAddr(
-		nil,
-		common.HexToAddress("0xB9Dbc229Bf588A613C00BEE8e662727AB8121cfE").String(),
-	)
-	require.NoError(r, err, "Failed to call Bech32ToHexAddr in Regular precompiled contract")
-
-	require.Equal(r, dummyBech32Addr, addr.String(), "Expected address %s, got %s", dummyBech32Addr, addr.String())
+	ok, err = contract.TestBech32ify(&bind.CallOpts{})
+	require.NoError(r, err, "Failed to create Regular contract caller")
+	require.True(r, ok, "Failed to validate Bech32ToHexAddr function")
 }
