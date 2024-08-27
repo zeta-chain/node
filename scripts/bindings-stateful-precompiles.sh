45370d430a52d37b68ab9872955df4f403b3acb3
skosito <skostic9242@gmail.com>
2024-08-27 13:00:52 +0200

    cleanup action a bit

--- scripts/bindings-stateful-precompiles.sh
+++ scripts/bindings-stateful-precompiles.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
+# Check if abigen is installed
+if ! command -v abigen &> /dev/null
+then
+    echo "abigen could not be found, installing..."
+    go install github.com/ethereum/go-ethereum/cmd/abigen@latest
+fi
+
 # Generic function to generate bindings
 function bindings() {
     cd $1
@@ -8,5 +17,5 @@ function bindings() {
 }
 
 # List of bindings to generate
-bindings $(pwd)/precompiles/prototype
+bindings ./precompiles/prototype
 
