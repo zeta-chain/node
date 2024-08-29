3025cde9b29458a460e64520bb8fd0fbbb2c36bc
Alex Gartner <alexg@zetachain.com>
2024-08-29 12:20:46 -0700

    embed rpc.Clients in zetaclient/zetacored.Client

--- pkg/rpc/clients.go
+++ pkg/rpc/clients.go
@@ -20,7 +20,7 @@ import (
 	observertypes "github.com/zeta-chain/zetacore/x/observer/types"
 )
 
-// Clients contains RPC client interfaces to interact with zetacored
+// Clients contains RPC client interfaces to interact with ZetaCore
 type Clients struct {
 	// Cosmos SDK clients
 
