7464d148a2a697855f3928f7d2d6b0083bd6aee6
skosito <skostic9242@gmail.com>
2024-09-05 01:15:51 +0200

    split tests into 2 files

--- e2e/e2etests/test_precompiles_prototype_through_contract.go
+++ e2e/e2etests/test_precompiles_prototype_through_contract.go
@@ -4,21 +4,23 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/require"
 
+	"github.com/zeta-chain/node/e2e/contracts/testprototype"
 	"github.com/zeta-chain/node/e2e/runner"
-	"github.com/zeta-chain/node/precompiles/prototype"
+	"github.com/zeta-chain/node/e2e/utils"
 )
 
-func TestPrecompilesPrototype(r *runner.E2ERunner, args []string) {
+func TestPrecompilesPrototypeThroughContract(r *runner.E2ERunner, args []string) {
 	require.Len(r, args, 0, "No arguments expected")
 
-	iPrototype, err := prototype.NewIPrototype(prototype.ContractAddress, r.ZEVMClient)
-	require.NoError(r, err, "Failed to create prototype contract caller")
+	_, testPrototypeTx, testPrototype, err := testprototype.DeployTestPrototype(r.ZEVMAuth, r.ZEVMClient)
+	require.NoError(r, err)
+	utils.MustWaitForTxReceipt(r.Ctx, r.ZEVMClient, testPrototypeTx, r.Logger, r.ReceiptTimeout)
 
-	res, err := iPrototype.Bech32ify(nil, "zeta", common.HexToAddress("0xB9Dbc229Bf588A613C00BEE8e662727AB8121cfE"))
+	res, err := testPrototype.Bech32ify(nil, "zeta", common.HexToAddress("0xB9Dbc229Bf588A613C00BEE8e662727AB8121cfE"))
 	require.NoError(r, err, "Error calling Bech32ify")
 	require.Equal(r, "zeta1h8duy2dltz9xz0qqhm5wvcnj02upy887fyn43u", res, "Failed to validate Bech32ify result")
 
-	addr, err := iPrototype.Bech32ToHexAddr(nil, "zeta1h8duy2dltz9xz0qqhm5wvcnj02upy887fyn43u")
+	addr, err := testPrototype.Bech32ToHexAddr(nil, "zeta1h8duy2dltz9xz0qqhm5wvcnj02upy887fyn43u")
 	require.NoError(r, err, "Error calling Bech32ToHexAddr")
 	require.Equal(
 		r,
@@ -30,7 +32,7 @@ func TestPrecompilesPrototype(r *runner.E2ERunner, args []string) {
 	chainID, err := r.EVMClient.ChainID(r.Ctx)
 	require.NoError(r, err, "Error retrieving ChainID")
 
-	balance, err := iPrototype.GetGasStabilityPoolBalance(nil, chainID.Int64())
+	balance, err := testPrototype.GetGasStabilityPoolBalance(nil, chainID.Int64())
 	require.NoError(r, err, "Error calling GetGasStabilityPoolBalance")
 	require.NotNil(r, balance, "GetGasStabilityPoolBalance returned balance is nil")
 }
