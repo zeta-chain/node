ebeedfc712ae5908e89cfe973e5a4d48382f197e
Julian Rubino <julian.rubino0@gmail.com>
2024-09-05 00:27:06 +0900

    chore: change snapshots API on contrib/localnet/scripts to consume state exports (#2819)
    * Refactor contrib/rpc and contrib/docker-scripts to use snapshots API
    
    * Replace Snapshot API on localnet scripts for consuming state exports
    
    ---------
    
    Co-authored-by: Julian Rubino <julian@zetachain.com>

--- contrib/localnet/scripts/import-data.sh
+++ contrib/localnet/scripts/import-data.sh
@@ -10,6 +10,6 @@ echo "NETWORK: ${NETWORK}"
 rm -rf ~/.zetacored/genesis_data
 mkdir -p ~/.zetacored/genesis_data
 echo "Download Latest State Export"
-LATEST_EXPORT_URL=$(curl https://snapshots.zetachain.com/latest-state-export | jq -r ."${NETWORK}")
+LATEST_EXPORT_URL=$(curl https://snapshots.rpc.zetachain.com/${NETWORK}/state/latest.json | jq -r '.snapshots[0].link')
 echo "LATEST EXPORT URL: ${LATEST_EXPORT_URL}"
 wget -q ${LATEST_EXPORT_URL} -O ~/.zetacored/genesis_data/exported-genesis.json
