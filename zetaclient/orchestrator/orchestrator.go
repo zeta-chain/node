de1471b3412a987dc4f299d729f3a531045cb75b
Alex Gartner <alexg@zetachain.com>
2024-09-07 10:44:52 -0700

    fix: update go-tss with known fixes (v19) (#2849)
    * fix: update go-tss with known fixes (v19)
    
    * manually cherry pick https://github.com/zeta-chain/node/pull/2843
    
    * manually cherry pick remediation for schedule 0 value
    
    ---------
    
    Co-authored-by: skosito <skostic9242@gmail.com>

--- zetaclient/orchestrator/orchestrator.go
+++ zetaclient/orchestrator/orchestrator.go
@@ -449,6 +449,11 @@ func (oc *Orchestrator) ScheduleCctxEVM(
 	outboundScheduleLookback := uint64(float64(outboundScheduleLookahead) * evmOutboundLookbackFactor)
 	// #nosec G115 positive
 	outboundScheduleInterval := uint64(observer.GetChainParams().OutboundScheduleInterval)
+	// determining critical outbound; if it satisfies following criteria
+	// 1. it's the first pending outbound for this chain
+	// 2. the following 5 nonces have been in tracker
+	criticalInterval := uint64(10)                      // for critical pending outbound we reduce re-try interval
+	nonCriticalInterval := outboundScheduleInterval * 2 // for non-critical pending outbound we increase re-try interval
 
 	for idx, cctx := range cctxList {
 		params := cctx.GetCurrentOutboundParam()
@@ -480,11 +485,6 @@ func (oc *Orchestrator) ScheduleCctxEVM(
 			continue
 		}
 
-		// determining critical outbound; if it satisfies following criteria
-		// 1. it's the first pending outbound for this chain
-		// 2. the following 5 nonces have been in tracker
-		criticalInterval := uint64(10)                      // for critical pending outbound we reduce re-try interval
-		nonCriticalInterval := outboundScheduleInterval * 2 // for non-critical pending outbound we increase re-try interval
 		if nonce%criticalInterval == zetaHeight%criticalInterval {
 			count := 0
 			for i := nonce + 1; i <= nonce+10; i++ {
@@ -506,8 +506,8 @@ func (oc *Orchestrator) ScheduleCctxEVM(
 			// logging observer chain params to help with debugging if issue happens again
 			oc.logger.Error().
 				Interface("observer.chain_params", observer.GetChainParams()).
-				Msgf("ScheduleCctxEVM: outboundScheduleInterval set to 0 for chain %d", chainID)
-			return
+				Msgf("ScheduleCctxEVM: outboundScheduleInterval set to 0 for chain %d, hardcoding to 64", chainID)
+			outboundScheduleInterval = 64
 		}
 
 		// otherwise, the normal interval is used
