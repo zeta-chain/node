19f072c2f138ae0e24888f942975bdacafa88103
skosito <skostic9242@gmail.com>
2024-09-11 00:52:17 +0200

    PR comments

--- precompiles/staking/logs.go
+++ precompiles/staking/logs.go
@@ -4,10 +4,10 @@ import (
 	"math/big"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
+
+	"github.com/zeta-chain/node/precompiles/logs"
 )
 
 func (c *Contract) AddStakeLog(
@@ -25,18 +25,18 @@ func (c *Contract) AddStakeLog(
 	}
 
 	// staker and validator are indexed event params
-	topics, err := makeTopics(event, []interface{}{staker}, []interface{}{common.BytesToAddress(valAddr.Bytes())})
+	topics, err := logs.MakeTopics(event, []interface{}{staker}, []interface{}{common.BytesToAddress(valAddr.Bytes())})
 	if err != nil {
 		return err
 	}
 
 	// amount is part of event data
-	data, err := packAmount(amount)
+	data, err := logs.PackBigInt(amount)
 	if err != nil {
 		return err
 	}
 
-	c.addLog(ctx, stateDB, topics, data)
+	logs.AddLog(ctx, c.Address(), stateDB, topics, data)
 
 	return nil
 }
@@ -55,18 +55,18 @@ func (c *Contract) AddUnstakeLog(
 	}
 
 	// staker and validator are indexed event params
-	topics, err := makeTopics(event, []interface{}{staker}, []interface{}{common.BytesToAddress(valAddr.Bytes())})
+	topics, err := logs.MakeTopics(event, []interface{}{staker}, []interface{}{common.BytesToAddress(valAddr.Bytes())})
 	if err != nil {
 		return err
 	}
 
 	// amount is part of event data
-	data, err := packAmount(amount)
+	data, err := logs.PackBigInt(amount)
 	if err != nil {
 		return err
 	}
 
-	c.addLog(ctx, stateDB, topics, data)
+	logs.AddLog(ctx, c.Address(), stateDB, topics, data)
 
 	return nil
 }
@@ -91,7 +91,7 @@ func (c *Contract) AddMoveStakeLog(
 	}
 
 	// staker and validators are indexed event params
-	topics, err := makeTopics(
+	topics, err := logs.MakeTopics(
 		event,
 		[]interface{}{staker},
 		[]interface{}{common.BytesToAddress(validatorSrcAddr.Bytes())},
@@ -102,53 +102,12 @@ func (c *Contract) AddMoveStakeLog(
 	}
 
 	// amount is part of event data
-	data, err := packAmount(amount)
+	data, err := logs.PackBigInt(amount)
 	if err != nil {
 		return err
 	}
 
-	c.addLog(ctx, stateDB, topics, data)
+	logs.AddLog(ctx, c.Address(), stateDB, topics, data)
 
 	return nil
-}
-
-func (c *Contract) addLog(ctx sdk.Context, stateDB vm.StateDB, topics []common.Hash, data []byte) {
-	stateDB.AddLog(&types.Log{
-		Address:     c.Address(),
-		Topics:      topics,
-		Data:        data,
-		BlockNumber: uint64(ctx.BlockHeight()),
-	})
-}
-
-func makeTopics(event abi.Event, query ...[]interface{}) ([]common.Hash, error) {
-	topics := []common.Hash{event.ID}
-
-	topicsRes, err := abi.MakeTopics(
-		query...,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, topic := range topicsRes {
-		topics = append(topics, topic[0])
-	}
-
-	return topics, nil
-}
-
-// helper function to pack a uint256 amount
-func packAmount(amount *big.Int) ([]byte, error) {
-	uint256Type, err := abi.NewType("uint256", "", nil)
-	if err != nil {
-		return nil, err
-	}
-
-	arguments := abi.Arguments{
-		{
-			Type: uint256Type,
-		},
-	}
-	return arguments.Pack(amount)
 }
