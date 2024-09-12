e0fa24aab4fb211c4f0c6faab3ab1fc5ab64fcaa
Francisco de Borja Aranda Castillejo <me@fbac.dev>
2024-09-12 13:09:06 +0200

    cover al error types test

--- precompiles/types/errors_test.go
+++ precompiles/types/errors_test.go
@@ -5,9 +5,10 @@ import "testing"
 func Test_ErrInvalidAddr(t *testing.T) {
 	e := ErrInvalidAddr{
 		Got: "foo",
+		Reason: "bar",
 	}
 	got := e.Error()
-	expect := "invalid address foo"
+	expect := "invalid address foo, reason: bar"
 	if got != expect {
 		t.Errorf("Expected %v, got %v", expect, got)
 	}
@@ -42,6 +43,42 @@ func Test_ErrInvalidMethod(t *testing.T) {
 	}
 	got := e.Error()
 	expect := "invalid method: foo"
+	if got != expect {
+		t.Errorf("Expected %v, got %v", expect, got)
+	}
+}
+
+func Test_ErrInvalidCoin(t *testing.T) {
+	e := ErrInvalidCoin{
+		Got:      "foo",
+		Negative: true,
+		Nil:      false,
+	}
+	got := e.Error()
+	expect := "invalid coin: denom: foo, is negative: true, is nil: false"
+	if got != expect {
+		t.Errorf("Expected %v, got %v", expect, got)
+	}
+}
+
+func Test_ErrInvalidAmount(t *testing.T) {
+	e := ErrInvalidAmount{
+		Got: "foo",
+	}
+	got := e.Error()
+	expect := "invalid token amount: foo"
+	if got != expect {
+		t.Errorf("Expected %v, got %v", expect, got)
+	}
+}
+
+func Test_ErrUnexpected(t *testing.T) {
+	e := ErrUnexpected{
+		When: "foo",
+		Got:  "bar",
+	}
+	got := e.Error()
+	expect := "unexpected foo, got: bar"
 	if got != expect {
 		t.Errorf("Expected %v, got %v", expect, got)
 	}
