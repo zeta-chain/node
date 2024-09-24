bc9f2d66542ec1ab81cb012e25b89131702b5b8f
jkan2 <5862123+jkan2@users.noreply.github.com>
2024-09-24 16:32:43 -0700

    add test vulnerable file just to validate the install of semgrep in repo

---
+++ cmd/test-semgrep.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	// Another example of untrusted input
+	input := "ping -c 7 google.com; echo hacked"
+
+	ctx := context.Background()
+
+	// Vulnerable: input is directly concatenated into the command
+	command := fmt.Sprintf("sh -c %s", input)
+	cmd := exec.CommandContext(ctx, command)
+
+	// Execute and print the output
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println("Output:", string(output))
+}
+
