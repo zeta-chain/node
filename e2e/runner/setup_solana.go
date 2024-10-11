404bd9625da63d958daa185839fc2fe5d1d93475
Alex Gartner <alexg@zetachain.com>
2024-10-11 10:19:32 -0700

    refactor: configure solana chain during e2e setup (#2990)
    * refactor: configure solana chain during setup
    
    * add sol back to app_test.go

--- e2e/runner/setup_solana.go
+++ e2e/runner/setup_solana.go
@@ -1,14 +1,20 @@
 package runner
 
 import (
+	"time"
+
 	ethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/gagliardetto/solana-go"
 	"github.com/gagliardetto/solana-go/rpc"
 	"github.com/near/borsh-go"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 
+	"github.com/zeta-chain/node/e2e/utils"
 	"github.com/zeta-chain/node/pkg/chains"
+	"github.com/zeta-chain/node/pkg/constant"
 	solanacontracts "github.com/zeta-chain/node/pkg/contracts/solana"
+	observertypes "github.com/zeta-chain/node/x/observer/types"
 )
 
 // SetupSolanaAccount imports the deployer's private key
@@ -20,8 +26,8 @@ func (r *E2ERunner) SetupSolanaAccount() {
 	r.Logger.Info("SolanaDeployerAddress: %s", r.SolanaDeployerAddress)
 }
 
-// SetSolanaContracts set Solana contracts
-func (r *E2ERunner) SetSolanaContracts(deployerPrivateKey string) {
+// SetupSolana sets Solana contracts and params
+func (r *E2ERunner) SetupSolana(deployerPrivateKey string) {
 	r.Logger.Print("⚙️ initializing gateway program on Solana")
 
 	// set Solana contracts
@@ -78,4 +84,64 @@ func (r *E2ERunner) SetSolanaContracts(deployerPrivateKey string) {
 	balance, err := r.SolanaClient.GetBalance(r.Ctx, pdaComputed, rpc.CommitmentConfirmed)
 	require.NoError(r, err)
 	r.Logger.Info("initial PDA balance: %d lamports", balance.Value)
+
+	err = r.ensureSolanaChainParams()
+	require.NoError(r, err)
+}
+
+func (r *E2ERunner) ensureSolanaChainParams() error {
+	if r.ZetaTxServer == nil {
+		return errors.New("ZetaTxServer is not initialized")
+	}
+
+	creator := r.ZetaTxServer.MustGetAccountAddressFromName(utils.OperationalPolicyName)
+
+	chainID := chains.SolanaLocalnet.ChainId
+
+	chainParams := &observertypes.ChainParams{
+		ChainId:                     chainID,
+		ConfirmationCount:           32,
+		ZetaTokenContractAddress:    constant.EVMZeroAddress,
+		ConnectorContractAddress:    constant.EVMZeroAddress,
+		Erc20CustodyContractAddress: constant.EVMZeroAddress,
+		GasPriceTicker:              5,
+		WatchUtxoTicker:             0,
+		InboundTicker:               2,
+		OutboundTicker:              2,
+		OutboundScheduleInterval:    2,
+		OutboundScheduleLookahead:   5,
+		BallotThreshold:             observertypes.DefaultBallotThreshold,
+		MinObserverDelegation:       observertypes.DefaultMinObserverDelegation,
+		IsSupported:                 true,
+		GatewayAddress:              solanacontracts.SolanaGatewayProgramID,
+	}
+
+	updateMsg := observertypes.NewMsgUpdateChainParams(creator, chainParams)
+
+	if _, err := r.ZetaTxServer.BroadcastTx(utils.OperationalPolicyName, updateMsg); err != nil {
+		return errors.Wrap(err, "unable to broadcast solana chain params tx")
+	}
+
+	resetMsg := observertypes.NewMsgResetChainNonces(creator, chainID, 0, 0)
+	if _, err := r.ZetaTxServer.BroadcastTx(utils.OperationalPolicyName, resetMsg); err != nil {
+		return errors.Wrap(err, "unable to broadcast solana chain nonce reset tx")
+	}
+
+	r.Logger.Print("⚙️ voted for adding solana chain params (localnet). Waiting for confirmation")
+
+	query := &observertypes.QueryGetChainParamsForChainRequest{ChainId: chainID}
+
+	const duration = 2 * time.Second
+
+	for i := 0; i < 10; i++ {
+		_, err := r.ObserverClient.GetChainParamsForChain(r.Ctx, query)
+		if err == nil {
+			r.Logger.Print("⚙️ solana chain params are set")
+			return nil
+		}
+
+		time.Sleep(duration)
+	}
+
+	return errors.New("unable to set Solana chain params")
 }
