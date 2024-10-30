036fa41ce257440cf5dc3a7f16d726dd0a0049aa
lumtis <lucas.bertrand.22@gmail.com>
2024-10-30 19:31:19 +0100

    refactor: max supply check

--- x/fungible/keeper/zeta_test.go
+++ x/fungible/keeper/zeta_test.go
@@ -2,6 +2,8 @@ package keeper_test
 
 import (
 	"errors"
+	"github.com/stretchr/testify/mock"
+	"github.com/zeta-chain/zetacore/x/fungible/keeper"
 	"math/big"
 	"testing"
 
@@ -29,6 +31,46 @@ func TestKeeper_MintZetaToEVMAccount(t *testing.T) {
 		require.True(t, bal.Amount.Equal(sdk.NewInt(42)))
 	})
 
+	t.Run("mint the token to reach max supply", func(t *testing.T) {
+		k, ctx, sdkk, _ := testkeeper.FungibleKeeper(t)
+		k.GetAuthKeeper().GetModuleAccount(ctx, types.ModuleName)
+
+		acc := sample.Bech32AccAddress()
+		bal := sdkk.BankKeeper.GetBalance(ctx, acc, config.BaseDenom)
+		require.True(t, bal.IsZero())
+
+		zetaMaxSupply, ok := sdk.NewIntFromString(keeper.ZETAMaxSupplyStr)
+		require.True(t, ok)
+
+		supply := sdkk.BankKeeper.GetSupply(ctx, config.BaseDenom).Amount
+
+		newAmount := zetaMaxSupply.Sub(supply)
+
+		err := k.MintZetaToEVMAccount(ctx, acc, newAmount.BigInt())
+		require.NoError(t, err)
+		bal = sdkk.BankKeeper.GetBalance(ctx, acc, config.BaseDenom)
+		require.True(t, bal.Amount.Equal(newAmount))
+	})
+
+	t.Run("can't mint more than max supply", func(t *testing.T) {
+		k, ctx, sdkk, _ := testkeeper.FungibleKeeper(t)
+		k.GetAuthKeeper().GetModuleAccount(ctx, types.ModuleName)
+
+		acc := sample.Bech32AccAddress()
+		bal := sdkk.BankKeeper.GetBalance(ctx, acc, config.BaseDenom)
+		require.True(t, bal.IsZero())
+
+		zetaMaxSupply, ok := sdk.NewIntFromString(keeper.ZETAMaxSupplyStr)
+		require.True(t, ok)
+
+		supply := sdkk.BankKeeper.GetSupply(ctx, config.BaseDenom).Amount
+
+		newAmount := zetaMaxSupply.Sub(supply).Add(sdk.NewInt(1))
+
+		err := k.MintZetaToEVMAccount(ctx, acc, newAmount.BigInt())
+		require.ErrorIs(t, err, types.ErrMaxSupplyReached)
+	})
+
 	coins42 := sdk.NewCoins(sdk.NewCoin(config.BaseDenom, sdk.NewInt(42)))
 
 	t.Run("should fail if minting fail", func(t *testing.T) {
@@ -36,6 +78,9 @@ func TestKeeper_MintZetaToEVMAccount(t *testing.T) {
 
 		mockBankKeeper := testkeeper.GetFungibleBankMock(t, k)
 
+		mockBankKeeper.On("GetSupply", ctx, mock.Anything, mock.Anything).
+			Return(sdk.NewCoin(config.BaseDenom, sdk.NewInt(0))).
+			Once()
 		mockBankKeeper.On(
 			"MintCoins",
 			ctx,
@@ -55,6 +100,9 @@ func TestKeeper_MintZetaToEVMAccount(t *testing.T) {
 
 		mockBankKeeper := testkeeper.GetFungibleBankMock(t, k)
 
+		mockBankKeeper.On("GetSupply", ctx, mock.Anything, mock.Anything).
+			Return(sdk.NewCoin(config.BaseDenom, sdk.NewInt(0))).
+			Once()
 		mockBankKeeper.On(
 			"MintCoins",
 			ctx,
@@ -74,5 +122,35 @@ func TestKeeper_MintZetaToEVMAccount(t *testing.T) {
 		require.Error(t, err)
 
 		mockBankKeeper.AssertExpectations(t)
+	})
+}
+
+func TestKeeper_MintZetaToFungibleModule(t *testing.T) {
+	t.Run("should mint the token in the specified balance", func(t *testing.T) {
+		k, ctx, sdkk, _ := testkeeper.FungibleKeeper(t)
+		acc := k.GetAuthKeeper().GetModuleAccount(ctx, types.ModuleName).GetAddress()
+
+		bal := sdkk.BankKeeper.GetBalance(ctx, acc, config.BaseDenom)
+		require.True(t, bal.IsZero())
+
+		err := k.MintZetaToEVMAccount(ctx, acc, big.NewInt(42))
+		require.NoError(t, err)
+		bal = sdkk.BankKeeper.GetBalance(ctx, acc, config.BaseDenom)
+		require.True(t, bal.Amount.Equal(sdk.NewInt(42)))
+	})
+
+	t.Run("can't mint more than max supply", func(t *testing.T) {
+		k, ctx, sdkk, _ := testkeeper.FungibleKeeper(t)
+		k.GetAuthKeeper().GetModuleAccount(ctx, types.ModuleName)
+
+		zetaMaxSupply, ok := sdk.NewIntFromString(keeper.ZETAMaxSupplyStr)
+		require.True(t, ok)
+
+		supply := sdkk.BankKeeper.GetSupply(ctx, config.BaseDenom).Amount
+
+		newAmount := zetaMaxSupply.Sub(supply).Add(sdk.NewInt(1))
+
+		err := k.MintZetaToFungibleModule(ctx, newAmount.BigInt())
+		require.ErrorIs(t, err, types.ErrMaxSupplyReached)
 	})
 }
