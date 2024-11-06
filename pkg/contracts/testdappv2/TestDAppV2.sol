b0bb05b6907869fdf6d5ac6f572711fa3932d62e
lumtis <lucas.bertrand.22@gmail.com>
2024-11-06 16:44:57 +0100

    add withdraw in testdapp

--- pkg/contracts/testdappv2/TestDAppV2.sol
+++ pkg/contracts/testdappv2/TestDAppV2.sol
@@ -1,36 +1,76 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
+/// @title IGatewayZEVM
+/// @notice Interface for the GatewayZEVM contract.
+/// @dev Defines functions for cross-chain interactions and token handling.
+interface IGatewayZEVM {
+    /// @notice Withdraw ZRC20 tokens to an external chain.
+    /// @param receiver The receiver address on the external chain.
+    /// @param amount The amount of tokens to withdraw.
+    /// @param zrc20 The address of the ZRC20 token.
+    /// @param revertOptions Revert options.
+    function withdraw(
+        bytes memory receiver,
+        uint256 amount,
+        address zrc20,
+        RevertOptions calldata revertOptions
+    )
+    external;
+}
+
+/// @title IZRC20
+/// @notice Interface for the ZRC20 token contract.
+interface IZRC20 {
+    function approve(address spender, uint256 amount) external returns (bool);
+    function withdrawGasFee() external view returns (address, uint256);
+}
+
 interface IERC20 {
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
 
+/// @notice Struct containing revert options
+/// @param revertAddress Address to receive revert.
+/// @param callOnRevert Flag if onRevert hook should be called.
+/// @param abortAddress Address to receive funds if aborted.
+/// @param revertMessage Arbitrary data sent back in onRevert.
+/// @param onRevertGasLimit Gas limit for revert tx, unused on GatewayZEVM methods
+struct RevertOptions {
+    address revertAddress;
+    bool callOnRevert;
+    address abortAddress;
+    bytes revertMessage;
+    uint256 onRevertGasLimit;
+}
+
+struct zContext {
+    bytes origin;
+    address sender;
+    uint256 chainID;
+}
+
+/// @notice Struct containing revert context passed to onRevert.
+/// @param sender Address of account that initiated smart contract call.
+/// @param asset Address of asset, empty if it's gas token.
+/// @param amount Amount specified with the transaction.
+/// @param revertMessage Arbitrary data sent back in onRevert.
+struct RevertContext {
+    address sender;
+    address asset;
+    uint256 amount;
+    bytes revertMessage;
+}
+
+/// @notice Message context passed to execute function.
+/// @param sender Sender from omnichain contract.
+struct MessageContext {
+    address sender;
+}
+
 contract TestDAppV2 {
     string public constant NO_MESSAGE_CALL = "called with no message";
-
-    struct zContext {
-        bytes origin;
-        address sender;
-        uint256 chainID;
-    }
-
-    /// @notice Struct containing revert context passed to onRevert.
-    /// @param sender Address of account that initiated smart contract call.
-    /// @param asset Address of asset, empty if it's gas token.
-    /// @param amount Amount specified with the transaction.
-    /// @param revertMessage Arbitrary data sent back in onRevert.
-    struct RevertContext {
-        address sender;
-        address asset;
-        uint256 amount;
-        bytes revertMessage;
-    }
-
-    /// @notice Message context passed to execute function.
-    /// @param sender Sender from omnichain contract.
-    struct MessageContext {
-        address sender;
-    }
+    string public constant WITHDRAW = "withdraw";
 
     // these structures allow to assess contract calls
     mapping(bytes32 => bool) public calledWithMessage;
@@ -62,8 +102,8 @@ contract TestDAppV2 {
 
     // Universal contract interface on ZEVM
     function onCall(
-        zContext calldata _context,
-        address _zrc20,
+        zContext calldata context,
+        address zrc20,
         uint256 amount,
         bytes calldata message
     )
@@ -71,10 +111,29 @@ contract TestDAppV2 {
     {
         require(!isRevertMessage(string(message)));
 
-        string memory messageStr = message.length == 0 ? getNoMessageIndex(_context.sender) : string(message);
+        // if withdraw is specified in the call, withdraw back the funds
+        if (isWithdrawMessage(string(message))) {
+            // get the fees for the withdraw, note: zrc20 should be the gas token for now
+            (address feeToken, uint256 feeAmount) = IZRC20(zrc20).withdrawGasFee();
+            require(feeToken == zrc20, "zrc20 is not gas token");
+            require(feeAmount <= amount, "fee amount is higher than the amount");
+            uint256 withdrawAmount = amount - feeAmount;
 
-        setCalledWithMessage(messageStr);
-        setAmountWithMessage(messageStr, amount);
+            IZRC20(zrc20).approve(msg.sender, amount);
+
+            // caller is the gateway
+            IGatewayZEVM(msg.sender).withdraw(
+                abi.encode(context.sender),
+                withdrawAmount,
+                zrc20,
+                RevertOptions(address(0), false, address(0), "", 0)
+            );
+        } else {
+            string memory messageStr = message.length == 0 ? getNoMessageIndex(context.sender) : string(message);
+
+            setCalledWithMessage(messageStr);
+            setAmountWithMessage(messageStr, amount);
+        }
     }
 
     // called with gas token
@@ -106,6 +165,11 @@ contract TestDAppV2 {
     // used to make functions revert
     function isRevertMessage(string memory message) internal pure returns (bool) {
         return keccak256(abi.encodePacked(message)) == keccak256(abi.encodePacked("revert"));
+    }
+
+    // used to trigger withdraw back
+    function isWithdrawMessage(string memory message) internal pure returns (bool) {
+        return keccak256(abi.encodePacked(message)) == keccak256(abi.encodePacked(WITHDRAW));
     }
 
     // Revertable interface
