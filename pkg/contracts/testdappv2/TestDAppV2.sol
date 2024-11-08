f62df34524f50d333024d5653844fa726cc575c8
lumtis <lucas.bertrand.22@gmail.com>
2024-11-08 19:11:33 +0100

    change gas

--- pkg/contracts/testdappv2/TestDAppV2.sol
+++ pkg/contracts/testdappv2/TestDAppV2.sol
@@ -223,7 +223,7 @@ contract TestDAppV2 {
 
     function consumeGas() internal {
         // Approximate target gas consumption
-        uint256 targetGas = 2000000;
+        uint256 targetGas = 1400000;
         // Approximate gas cost for a single storage write
         uint256 storageWriteGasCost = 20000;
         uint256 iterations = targetGas / storageWriteGasCost;
