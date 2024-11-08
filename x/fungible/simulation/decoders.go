ea92069a34529612387904753abcadf0d531160e
Tanmay <tanmay@zetachain.com>
2024-11-08 13:43:57 -0500

    Update x/fungible/simulation/decoders.go
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- x/fungible/simulation/decoders.go
+++ x/fungible/simulation/decoders.go
@@ -14,19 +14,30 @@ import (
 // Value to the corresponding fungible types.
 func NewDecodeStore(cdc codec.Codec) func(kvA, kvB kv.Pair) string {
 	return func(kvA, kvB kv.Pair) string {
+		if !bytes.Equal(kvA.Key[:1], kvB.Key[:1]) {
+			return fmt.Sprintf("key prefixes do not match. A: %X, B: %X", kvA.Key[:1], kvB.Key[:1])
+		}
 		switch {
 		case bytes.Equal(kvA.Key, types.KeyPrefix(types.SystemContractKey)):
 			var systemContractA, systemContractB types.SystemContract
-			cdc.MustUnmarshal(kvA.Value, &systemContractA)
-			cdc.MustUnmarshal(kvB.Value, &systemContractB)
+			if err := cdc.Unmarshal(kvA.Value, &systemContractA); err != nil {
+				return fmt.Sprintf("failed to unmarshal A: %v", err)
+			}
+			if err := cdc.Unmarshal(kvB.Value, &systemContractB); err != nil {
+				return fmt.Sprintf("failed to unmarshal B: %v", err)
+			}
 			return fmt.Sprintf("%v\n%v", systemContractA, systemContractB)
 		case bytes.Equal(kvA.Key, types.KeyPrefix(types.ForeignCoinsKeyPrefix)):
 			var foreignCoinsA, foreignCoinsB types.ForeignCoins
-			cdc.MustUnmarshal(kvA.Value, &foreignCoinsA)
-			cdc.MustUnmarshal(kvB.Value, &foreignCoinsB)
+			if err := cdc.Unmarshal(kvA.Value, &foreignCoinsA); err != nil {
+				return fmt.Sprintf("failed to unmarshal A: %v", err)
+			}
+			if err := cdc.Unmarshal(kvB.Value, &foreignCoinsB); err != nil {
+				return fmt.Sprintf("failed to unmarshal B: %v", err)
+			}
 			return fmt.Sprintf("%v\n%v", foreignCoinsA, foreignCoinsB)
 		default:
-			panic(fmt.Sprintf("invalid fungible key prefix %X", kvA.Key[:1]))
+			return fmt.Sprintf("invalid fungible key prefix %X", kvA.Key[:1])
 		}
 	}
 }
