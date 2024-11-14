105defec94ce68b07b02994f15d08e6cc829e7e1
Alex Gartner <git@agartner.com>
2024-11-14 09:48:16 -0800

    fix lint

--- cmd/zetacored/version.go
+++ cmd/zetacored/version.go
@@ -12,7 +12,7 @@ func UpgradeHandlerVersionCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "upgrade-handler-version",
 		Short: "Print the default upgrade handler version",
-		Run: func(cmd *cobra.Command, args []string) {
+		Run: func(_ *cobra.Command, _ []string) {
 			fmt.Println(app.GetDefaultUpgradeHandlerVersion())
 		},
 	}
