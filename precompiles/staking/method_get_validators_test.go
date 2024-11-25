8a4953b0ccda985cee75b677397ea0d5d82ce39b
Francisco de Borja Aranda Castillejo <me@fbac.dev>
2024-11-25 21:42:47 +0100

    add big validator set test

--- precompiles/staking/method_get_validators_test.go
+++ precompiles/staking/method_get_validators_test.go
@@ -114,4 +114,53 @@ func Test_GetValidators(t *testing.T) {
 		require.Len(t, list, 1)
 		require.Equal(t, validator.GetOperator().String(), list[0])
 	})
+
+	t.Run(" should return staker's validator list - heavy test with 100 validators", func(t *testing.T) {
+		/* ARRANGE */
+		s := newTestSuite(t)
+
+		// Create staker.
+		stakerEVMAddr := sample.EthAddress()
+		stakerCosmosAddr, err := precompiletypes.GetCosmosAddress(s.sdkKeepers.BankKeeper, stakerEVMAddr)
+		require.NoError(t, err)
+
+		// Create 100 validators, and stake on each of them.
+		for n := range 100 {
+			validator := sample.Validator(t, rand.New(rand.NewSource(int64(n))))
+			s.sdkKeepers.StakingKeeper.SetValidator(s.ctx, validator)
+
+			stakeThroughCosmosAPI(
+				t,
+				s.ctx,
+				s.sdkKeepers.BankKeeper,
+				s.sdkKeepers.StakingKeeper,
+				validator,
+				stakerCosmosAddr,
+				math.NewInt(100),
+			)
+		}
+
+		/* ACT */
+		// Call getValidatorListForDelegator.
+		getValidatorsMethod := s.stkContractABI.Methods[GetValidatorsMethodName]
+
+		s.mockVMContract.Input = packInputArgs(
+			t,
+			getValidatorsMethod,
+			[]interface{}{stakerEVMAddr}...,
+		)
+
+		bytes, err := s.stkContract.Run(s.mockEVM, s.mockVMContract, false)
+		require.NoError(t, err)
+
+		res, err := getValidatorsMethod.Outputs.Unpack(bytes)
+		require.NoError(t, err)
+		require.NotEmpty(t, res)
+
+		list, ok := res[0].([]string)
+		require.True(t, ok)
+
+		// The returned list should contain 100 entries.
+		require.Len(t, list, 100)
+	})
 }
