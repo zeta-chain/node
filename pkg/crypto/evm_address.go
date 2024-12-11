00563a9dea579bde88dcec6deb86ec6d99838f94
lumtis <lucas.bertrand.22@gmail.com>
2024-12-11 10:47:30 +0100

    add checksum check

---
+++ pkg/crypto/evm_address.go
@@ -0,0 +1,29 @@
+package crypto
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"strings"
+
+	"github.com/zeta-chain/node/pkg/constant"
+)
+
+// IsEmptyAddress returns true if the address is empty
+func IsEmptyAddress(address common.Address) bool {
+	return address == (common.Address{}) || address.Hex() == constant.EVMZeroAddress
+}
+
+// IsEVMAddress returns true if the string is an EVM address
+// independently of the checksum format
+func IsEVMAddress(address string) bool {
+	return len(address) == 42 && strings.HasPrefix(address, "0x") && common.IsHexAddress(address)
+}
+
+// IsChecksumAddress returns true if the EVM address string is a valid checksum address
+func IsChecksumAddress(address string) bool {
+	return address == common.HexToAddress(address).Hex()
+}
+
+// ToChecksumAddress returns the checksum address of the given EVM address
+func ToChecksumAddress(address string) string {
+	return common.HexToAddress(address).Hex()
+}
