73da862155fee559d1b5150cbbbac00599b2c132
Alex Gartner <git@agartner.com>
2024-12-10 21:00:42 -0800

    refactor(e2e): use seed address book rather than bootstrap peer

---
+++ cmd/zetae2e/get_zetaclient_bootstrap.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/spf13/cobra"
+	"github.com/zeta-chain/node/pkg/rpc"
+	"github.com/zeta-chain/node/pkg/sdkconfig"
+	observertypes "github.com/zeta-chain/node/x/observer/types"
+	"gitlab.com/thorchain/tss/go-tss/conversion"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func NewGetZetaclientBootstrap() *cobra.Command {
+	var ConfigureZetaclientBootstrapCmd = &cobra.Command{
+		Use:   "get-zetaclient-bootstrap",
+		Short: "get bootstrap address book entries for zetaclient",
+		RunE:  getZetaclientBootstrap,
+	}
+
+	return ConfigureZetaclientBootstrapCmd
+}
+
+func getZetaclientBootstrap(cmd *cobra.Command, _ []string) error {
+	sdkconfig.SetDefault(true)
+	rpcClient, err := rpc.NewGRPCClients(
+		"zetacore0:9090",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		return fmt.Errorf("get zetacore rpc client: %w", err)
+	}
+	var res *observertypes.QueryAllNodeAccountResponse
+	for {
+		res, err = rpcClient.Observer.NodeAccountAll(cmd.Context(), &observertypes.QueryAllNodeAccountRequest{})
+		if err != nil {
+			return fmt.Errorf("get all node accounts: %w", err)
+		}
+		if len(res.NodeAccount) > 1 {
+			break
+		}
+		fmt.Fprintln(cmd.OutOrStderr(), "waiting for node accounts")
+	}
+
+	// note that we deliberately do not filter ourselfs/localhost
+	// to mirror the production configuration
+	for _, account := range res.NodeAccount {
+		accAddr, err := sdk.AccAddressFromBech32(account.Operator)
+		if err != nil {
+			return err
+		}
+		valAddr := sdk.ValAddress(accAddr).String()
+		validatorRes, err := rpcClient.Staking.Validator(cmd.Context(), &stakingtypes.QueryValidatorRequest{
+			ValidatorAddr: valAddr,
+		})
+		if err != nil {
+			return fmt.Errorf("getting validator info for %s: %w", account.Operator, err)
+		}
+		// in localnet, moniker is also the hostname
+		moniker := validatorRes.Validator.Description.Moniker
+
+		peerId, err := conversion.Bech32PubkeyToPeerID(account.GranteePubkey.Secp256k1.String())
+		if err != nil {
+			return fmt.Errorf("conferting pubkey to peerid: %w", err)
+		}
+		zetaclientHostname := strings.ReplaceAll(moniker, "zetacore", "zetaclient")
+
+		// resolve the hostname
+		// something in libp2p/go-tss requires /ip4/<ip> and doesn't tolerate /dns4/<hostname>
+		ipAddresses, err := net.LookupIP(zetaclientHostname)
+		if err != nil {
+			return fmt.Errorf("failed to resolve hostname %s: %w", zetaclientHostname, err)
+		}
+		if len(ipAddresses) == 0 {
+			return fmt.Errorf("no IP addresses found for hostname %s", zetaclientHostname)
+		}
+		ipv4Address := ""
+		for _, ip := range ipAddresses {
+			if ip.To4() != nil {
+				ipv4Address = ip.String()
+				break
+			}
+		}
+		if ipv4Address == "" {
+			return fmt.Errorf("no IPv4 address found for hostname %s", zetaclientHostname)
+		}
+		fmt.Printf("/ip4/%s/tcp/6668/p2p/%s\n", ipv4Address, peerId.String())
+	}
+
+	return nil
+}
