259d5f832718e5b703446cc3b3cea17ea89e08cc
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2025-01-07 10:47:27 -0500

    remove max limit for Ballots query

--- x/observer/keeper/grpc_query_ballot.go
+++ x/observer/keeper/grpc_query_ballot.go
@@ -69,11 +69,6 @@ func (k Keeper) Ballots(goCtx context.Context, req *types.QueryBallotsRequest) (
 		req.Pagination = &query.PageRequest{}
 	}
 
-	if req.Pagination.Limit > 100 {
-		req.Pagination.Limit = 100
-	}
-	// The ballots are not sorted in any particular order therefore this query only has limited usefulness
-	// if the number of ballots is too large
 	pageRes, err := query.Paginate(ballotStore, req.Pagination, func(_ []byte, value []byte) error {
 		var ballot types.Ballot
 		if err := k.cdc.Unmarshal(value, &ballot); err != nil {
