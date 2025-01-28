8720ce9bb7eec4adefa56a7eafd3b6ccf1e11586
Alex Gartner <git@agartner.com>
2025-01-28 08:42:33 -0800

    set minimum version for bitcoin_deposit_and_abort_with_low_deposit_fee

--- e2e/e2etests/e2etests.go
+++ e2e/e2etests/e2etests.go
@@ -663,6 +663,7 @@ var AllE2ETests = []runner.E2ETest{
 		"deposit Bitcoin into ZEVM that aborts due to insufficient deposit fee",
 		[]runner.ArgDefinition{},
 		TestBitcoinDepositAndAbortWithLowDepositFee,
+		runner.WithMinimumVersion("v27.0.0"),
 	),
 	runner.NewE2ETest(
 		TestBitcoinWithdrawSegWitName,
