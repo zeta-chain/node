44d83fab191594784cf5861c773f213f31a01583
lumtis <lucas.bertrand.22@gmail.com>
2025-01-28 11:31:03 +0100

    improve parsing

--- e2e/e2etests/test_operation_add_liquidity_spl.go
+++ e2e/e2etests/test_operation_add_liquidity_spl.go
@@ -1,24 +1,18 @@
 package e2etests
 
 import (
-	"math/big"
-
 	"github.com/stretchr/testify/require"
 
 	"github.com/zeta-chain/node/e2e/runner"
+	"github.com/zeta-chain/node/e2e/utils"
 )
 
 // TestOperationAddLiquiditySPL is an operational test to add liquidity in spl token
 func TestOperationAddLiquiditySPL(r *runner.E2ERunner, args []string) {
 	require.Len(r, args, 2)
 
-	liqZETA := big.NewInt(0)
-	_, ok := liqZETA.SetString(args[0], 10)
-	require.True(r, ok, "Invalid amount specified for TestOperationAddLiquiditySPL")
-
-	liqSPL := big.NewInt(0)
-	_, ok = liqSPL.SetString(args[1], 10)
-	require.True(r, ok, "Invalid amount specified for TestOperationAddLiquiditySPL")
+	liqZETA := utils.ParseBigInt(r, args[0])
+	liqSPL := utils.ParseBigInt(r, args[1])
 
 	// perform the add liquidity
 	r.AddLiquiditySPL(liqZETA, liqSPL)
