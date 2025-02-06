b7be235ffc8e6ee73f1e82f5cbb813bd064498b5
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2025-02-06 10:25:40 -0500

    update migration script

--- x/authority/migrations/v3/migrate.go
+++ x/authority/migrations/v3/migrate.go
@@ -8,6 +8,7 @@ import (
 
 type authorityKeeper interface {
 	SetAuthorizationList(ctx sdk.Context, list types.AuthorizationList)
+	GetAuthorizationList(ctx sdk.Context) (val types.AuthorizationList, found bool)
 }
 
 // MigrateStore migrates the authority module state from the consensus version 2 to 3
@@ -15,7 +16,32 @@ func MigrateStore(
 	ctx sdk.Context,
 	keeper authorityKeeper,
 ) error {
-	// It is okay to not validate here, as the authorization list is fixed and will not change
+
+	//
+	var (
+		authorizationList          = types.DefaultAuthorizationsList()
+		removeInboundAuthorization = types.Authorization{
+			MsgUrl:           "/zetachain.zetacore.crosschain.MsgRemoveInboundTracker",
+			AuthorizedPolicy: types.PolicyType_groupEmergency,
+		}
+	)
+
+	// Fetch the current authorization list, if found use that instead of default list
+	al, found := keeper.GetAuthorizationList(ctx)
+	if found {
+		authorizationList = al
+	}
+
+	// Add the new authorization
+	authorizationList.SetAuthorization(removeInboundAuthorization)
+
+	// Validate the authorization list
+	err := authorizationList.Validate()
+	if err != nil {
+		return err
+	}
+
+	// Set the new authorization list
 	keeper.SetAuthorizationList(ctx, types.DefaultAuthorizationsList())
 	return nil
 }
--- x/authority/migrations/v3/migrate_test.go
+++ x/authority/migrations/v3/migrate_test.go
@@ -6,12 +6,12 @@ import (
 	"github.com/stretchr/testify/require"
 
 	keepertest "github.com/zeta-chain/node/testutil/keeper"
-	v2 "github.com/zeta-chain/node/x/authority/migrations/v2"
+	v3 "github.com/zeta-chain/node/x/authority/migrations/v3"
 	"github.com/zeta-chain/node/x/authority/types"
 )
 
 func TestMigrateStore(t *testing.T) {
-	t.Run("set authorization list", func(t *testing.T) {
+	t.Run("update authorization list", func(t *testing.T) {
 		// Arrange
 		k, ctx := keepertest.AuthorityKeeper(t)
 
@@ -20,12 +20,48 @@ func TestMigrateStore(t *testing.T) {
 		k.SetAuthorizationList(ctx, list)
 
 		// Act
-		err := v2.MigrateStore(ctx, *k)
+		err := v3.MigrateStore(ctx, *k)
 
 		// Assert
 		require.NoError(t, err)
 		list, found := k.GetAuthorizationList(ctx)
 		require.True(t, found)
 		require.Equal(t, types.DefaultAuthorizationsList(), list)
+	})
+
+	t.Run("set default authorization list if list is not found", func(t *testing.T) {
+		// Arrange
+		k, ctx := keepertest.AuthorityKeeper(t)
+
+		// Act
+		err := v3.MigrateStore(ctx, *k)
+
+		// Assert
+		require.NoError(t, err)
+		list, found := k.GetAuthorizationList(ctx)
+		require.True(t, found)
+		require.Equal(t, types.DefaultAuthorizationsList(), list)
+	})
+
+	t.Run("return error list is invalid", func(t *testing.T) {
+		// Arrange
+		k, ctx := keepertest.AuthorityKeeper(t)
+
+		k.SetAuthorizationList(ctx, types.AuthorizationList{Authorizations: []types.Authorization{
+			{
+				MsgUrl:           "ABC",
+				AuthorizedPolicy: types.PolicyType_groupEmergency,
+			},
+			{
+				MsgUrl:           "ABC",
+				AuthorizedPolicy: types.PolicyType_groupEmergency,
+			},
+		}})
+
+		// Act
+		err := v3.MigrateStore(ctx, *k)
+
+		// Assert
+		require.Error(t, err)
 	})
 }
