97c82cc63d21a8ac91ee79575e6eea9c9621c7e4
Alex Gartner <alexg@zetachain.com>
2025-02-07 14:15:06 -0800

    feat(e2e): setup sui (#3485)
    * feat(e2e): setup sui
    
    * fund deployer account
    
    * secp256k1 signer and gateway deployment
    
    * more signer refactoring
    
    * more signer test refactoring
    
    * use github.com/decred/dcrd/dcrec/secp256k1/v4/ecdsa directly
    
    * a bit more signer refactoring and coderabbit feedback
    
    * updates
    
    * fix deployment with ShowObjectChanges: true

--- cmd/zetae2e/local/local.go
+++ cmd/zetae2e/local/local.go
@@ -36,6 +36,7 @@ const (
 	flagTestPerformance   = "test-performance"
 	flagTestSolana        = "test-solana"
 	flagTestTON           = "test-ton"
+	flagTestSui           = "test-sui"
 	flagSkipRegular       = "skip-regular"
 	flagLight             = "light"
 	flagSetupOnly         = "setup-only"
@@ -72,6 +73,7 @@ func NewLocalCmd() *cobra.Command {
 	cmd.Flags().Bool(flagTestPerformance, false, "set to true to run performance tests")
 	cmd.Flags().Bool(flagTestSolana, false, "set to true to run solana tests")
 	cmd.Flags().Bool(flagTestTON, false, "set to true to run TON tests")
+	cmd.Flags().Bool(flagTestSui, false, "set to true to run Sui tests")
 	cmd.Flags().Bool(flagSkipRegular, false, "set to true to skip regular tests")
 	cmd.Flags().Bool(flagLight, false, "run the most basic regular tests, useful for quick checks")
 	cmd.Flags().Bool(flagSetupOnly, false, "set to true to only setup the networks")
@@ -104,6 +106,7 @@ func localE2ETest(cmd *cobra.Command, _ []string) {
 		testPerformance   = must(cmd.Flags().GetBool(flagTestPerformance))
 		testSolana        = must(cmd.Flags().GetBool(flagTestSolana))
 		testTON           = must(cmd.Flags().GetBool(flagTestTON))
+		testSui           = must(cmd.Flags().GetBool(flagTestSui))
 		skipRegular       = must(cmd.Flags().GetBool(flagSkipRegular))
 		light             = must(cmd.Flags().GetBool(flagLight))
 		setupOnly         = must(cmd.Flags().GetBool(flagSetupOnly))
@@ -193,6 +196,10 @@ func localE2ETest(cmd *cobra.Command, _ []string) {
 
 	// monitor block production to ensure we fail fast if there are consensus failures
 	go monitorBlockProductionCancel(ctx, cancel, conf)
+
+	if testSui && !skipSetup {
+		deployerRunner.SetupSui(conf.RPCs.SuiFaucet)
+	}
 
 	// set the authority client to the zeta tx server to be able to query message permissions
 	deployerRunner.ZetaTxServer.SetAuthorityClient(deployerRunner.AuthorityClient)
