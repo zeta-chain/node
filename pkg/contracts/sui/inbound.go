aa221b8c015d8a226be22b42a6e75fc9ce363391
lumtis <lucas.bertrand.22@gmail.com>
2025-02-07 18:57:11 +0100

    Improve data

--- pkg/contracts/sui/inbound.go
+++ pkg/contracts/sui/inbound.go
@@ -1,6 +1,7 @@
 package sui
 
 import (
+	ethcommon "github.com/ethereum/go-ethereum/common"
 	"strconv"
 
 	"github.com/block-vision/sui-go-sdk/models"
@@ -12,12 +13,13 @@ const CoinTypeSUI = "00000000000000000000000000000000000000000000000000000000000
 
 // Inbound represents data for a Sui inbound, it is parsed from a deposit or depositAndCall event
 type Inbound struct {
-	TxHash           string
-	EventIndex       uint64
+	TxHash     string
+	EventIndex uint64
+	// Note: CoinType is what is used as Asset field in the ForeignCoin object
 	CoinType         string
-	Amount           string
+	Amount           uint64
 	Sender           string
-	Receiver         string
+	Receiver         ethcommon.Address
 	Payload          []byte
 	IsCrossChainCall bool
 }
@@ -41,9 +43,13 @@ func parseInbound(event models.SuiEventResponse, depositAndCall bool) (Inbound, 
 		return Inbound{}, errors.New("invalid coin type")
 	}
 
-	amount, ok := parsedJSON["amount"].(string)
+	parsedAmount, ok := parsedJSON["amount"].(string)
 	if !ok {
 		return Inbound{}, errors.New("invalid amount")
+	}
+	amount, err := strconv.ParseUint(parsedAmount, 10, 64)
+	if err != nil {
+		return Inbound{}, errors.Wrap(err, "failed to parse amount")
 	}
 
 	sender, ok := parsedJSON["sender"].(string)
@@ -51,9 +57,13 @@ func parseInbound(event models.SuiEventResponse, depositAndCall bool) (Inbound, 
 		return Inbound{}, errors.New("invalid sender")
 	}
 
-	receiver, ok := parsedJSON["receiver"].(string)
+	parsedReceiver, ok := parsedJSON["receiver"].(string)
 	if !ok {
 		return Inbound{}, errors.New("invalid receiver")
+	}
+	receiver := ethcommon.HexToAddress(parsedReceiver)
+	if receiver == (ethcommon.Address{}) {
+		return Inbound{}, errors.New("invalid receiver address")
 	}
 
 	payload := []byte{}
