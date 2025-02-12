74e15186cd44eda889b081bb2710cc1fec6a9529
lumtis <lucas.bertrand.22@gmail.com>
2025-02-12 16:14:08 +0100

    generate

--- x/crosschain/keeper/abort.go
+++ x/crosschain/keeper/abort.go
@@ -25,7 +25,8 @@ func (k Keeper) ProcessAbort(
 ) {
 	// only support cctx with v2 and with a defined abort address
 	// also abort can't be processed if the abort amount is already refunded
-	if cctx.ProtocolContractVersion != types.ProtocolContractVersion_V2 || cctx.RevertOptions.AbortAddress == "" || cctx.CctxStatus.IsAbortRefunded {
+	if cctx.ProtocolContractVersion != types.ProtocolContractVersion_V2 || cctx.RevertOptions.AbortAddress == "" ||
+		cctx.CctxStatus.IsAbortRefunded {
 		cctx.CctxStatus.UpdateStatusAndErrorMessages(types.CctxStatus_Aborted, messages)
 		return
 	}
