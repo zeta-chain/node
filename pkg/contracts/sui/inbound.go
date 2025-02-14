16e06985a7615a945a18cc866004b41a0b8b4b12
Dmitry S <11892559+swift1337@users.noreply.github.com>
2025-02-14 15:03:12 +0100

    Refactor and improve gateway event parsing

--- pkg/contracts/sui/inbound.go
+++ pkg/contracts/sui/inbound.go
@@ -1,21 +1,18 @@
 package sui
 
 import (
-	"fmt"
-	"strconv"
-
+	"cosmossdk.io/math"
 	"github.com/block-vision/sui-go-sdk/models"
 	ethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/pkg/errors"
 )
 
-// Inbound represents data for a Sui inbound, it is parsed from a deposit or depositAndCall event
+// Inbound represents data for a Sui inbound,
+// it is parsed from a deposit/depositAndCall event
 type Inbound struct {
-	TxHash     string
-	EventIndex uint64
 	// Note: CoinType is what is used as Asset field in the ForeignCoin object
 	CoinType         CoinType
-	Amount           uint64
+	Amount           math.Uint
 	Sender           string
 	Receiver         ethcommon.Address
 	Payload          []byte
@@ -26,66 +23,57 @@ func (d *Inbound) IsGasDeposit() bool {
 	return d.CoinType == SUI
 }
 
-// parseInbound parses an inbound from a JSON read in the Sui event
-// depositAndCall is a flag to indicate if the event is a depositAndCall event otherwise deposit event
-// TODO: add specific error that can be handled when the event data is invalid
-// https://github.com/zeta-chain/node/issues/3502
-func parseInbound(event models.SuiEventResponse, eventType string) (Inbound, error) {
-	eventIndex, err := strconv.ParseUint(event.Id.EventSeq, 10, 64)
-	if err != nil {
-		return Inbound{}, errors.Wrap(err, "failed to parse event index")
-	}
-
+func parseInbound(event models.SuiEventResponse, eventType EventType) (Inbound, error) {
 	parsedJSON := event.ParsedJson
 
-	coinType, ok := parsedJSON["coin_type"].(string)
-	if !ok {
-		return Inbound{}, errors.New("invalid coin type")
-	}
-
-	parsedAmount, ok := parsedJSON["amount"].(string)
-	if !ok {
-		return Inbound{}, errors.New("invalid amount")
-	}
-	amount, err := strconv.ParseUint(parsedAmount, 10, 64)
+	coinType, err := extractStr(parsedJSON, "coin_type")
 	if err != nil {
-		return Inbound{}, errors.Wrap(err, "failed to parse amount")
+		return Inbound{}, err
 	}
 
-	sender, ok := parsedJSON["sender"].(string)
-	if !ok {
-		return Inbound{}, errors.New("invalid sender")
+	amountRaw, err := extractStr(parsedJSON, "amount")
+	if err != nil {
+		return Inbound{}, err
 	}
 
-	parsedReceiver, ok := parsedJSON["receiver"].(string)
-	if !ok {
-		return Inbound{}, errors.New("invalid receiver")
+	amount, err := math.ParseUint(amountRaw)
+	if err != nil {
+		return Inbound{}, errors.Wrap(err, "unable to parse amount")
 	}
 
-	receiver := ethcommon.HexToAddress(parsedReceiver)
+	sender, err := extractStr(parsedJSON, "sender")
+	if err != nil {
+		return Inbound{}, err
+	}
+
+	receiverRaw, err := extractStr(parsedJSON, "receiver")
+	if err != nil {
+		return Inbound{}, err
+	}
+
+	receiver := ethcommon.HexToAddress(receiverRaw)
 	if receiver == (ethcommon.Address{}) {
-		return Inbound{}, errors.New("can't parse receiver address")
+		return Inbound{}, errors.Errorf("invalid receiver address %q", receiverRaw)
 	}
 
-	isCrosschainCall := false
-
+	var isCrosschainCall bool
 	var payload []byte
-	if eventType == eventDepositAndCall {
+
+	if eventType == DepositAndCall {
 		isCrosschainCall = true
 
-		parsedPayload, ok := parsedJSON["payload"].([]interface{})
+		payloadRaw, ok := parsedJSON["payload"].([]any)
 		if !ok {
 			return Inbound{}, errors.New("invalid payload")
 		}
-		payload, err = convertPayload(parsedPayload)
+
+		payload, err = convertPayload(payloadRaw)
 		if err != nil {
-			return Inbound{}, errors.Wrap(err, "failed to convert payload")
+			return Inbound{}, errors.Wrap(err, "unable to convert payload")
 		}
 	}
 
 	return Inbound{
-		TxHash:           event.Id.TxDigest,
-		EventIndex:       eventIndex,
 		CoinType:         CoinType(coinType),
 		Amount:           amount,
 		Sender:           sender,
@@ -93,21 +81,4 @@ func parseInbound(event models.SuiEventResponse, eventType string) (Inbound, err
 		Payload:          payload,
 		IsCrossChainCall: isCrosschainCall,
 	}, nil
-}
-
-// convertPayload
-func convertPayload(data []interface{}) ([]byte, error) {
-	payload := make([]byte, 0, len(data))
-	for i, d := range data {
-		// parsed bytes are represented as float64
-		if b, ok := d.(float64); ok {
-			if b < 0 || b > 255 {
-				return nil, fmt.Errorf("invalid payload data at index %d, not a byte (%f)", i, b)
-			}
-			payload = append(payload, byte(b))
-		} else {
-			return nil, fmt.Errorf("invalid payload data at index %d, not a float64", i)
-		}
-	}
-	return payload, nil
 }
