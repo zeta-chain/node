967fff431fdc78a7eb83bc01c58f19848664c436
lumtis <lucas.bertrand.22@gmail.com>
2025-02-19 13:35:45 +0100

    zevm evm revert

---
+++ e2e/e2etests/test_zevm_to_evm_call_revert.go
@@ -0,0 +1,48 @@
+package e2etests
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/stretchr/testify/require"
+	"github.com/zeta-chain/protocol-contracts/pkg/gatewayzevm.sol"
+
+	"github.com/zeta-chain/node/e2e/runner"
+	"github.com/zeta-chain/node/e2e/utils"
+	crosschaintypes "github.com/zeta-chain/node/x/crosschain/types"
+)
+
+func TestZEVMToEVMCallRevert(r *runner.E2ERunner, args []string) {
+	require.Len(r, args, 0)
+
+	payload := randomPayload(r)
+
+	r.ApproveETHZRC20(r.GatewayZEVMAddr)
+
+	// perform the withdraw
+	tx := r.ZEVMToEMVCall(
+		r.TestDAppV2EVMAddr,
+		[]byte("revert"),
+		gatewayzevm.RevertOptions{
+			RevertAddress:    r.TestDAppV2ZEVMAddr,
+			CallOnRevert:     true,
+			RevertMessage:    []byte(payload),
+			OnRevertGasLimit: big.NewInt(200000),
+		},
+	)
+
+	// wait for the cctx to be mined
+	cctx := utils.WaitCctxMinedByInboundHash(r.Ctx, tx.Hash().Hex(), r.CctxClient, r.Logger, r.CctxTimeout)
+	r.Logger.CCTX(*cctx, "call")
+	require.Equal(r, crosschaintypes.CctxStatus_Reverted, cctx.CctxStatus.Status)
+
+	r.AssertTestDAppZEVMCalled(true, payload, big.NewInt(0))
+
+	// check expected sender was used
+	senderForMsg, err := r.TestDAppV2ZEVM.SenderWithMessage(
+		&bind.CallOpts{},
+		[]byte(payload),
+	)
+	require.NoError(r, err)
+	require.Equal(r, r.ZEVMAuth.From, senderForMsg)
+}
