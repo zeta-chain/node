3a1bcf026a0775175c47cd9316382f2d2845a0e2
lumtis <lucas.bertrand.22@gmail.com>
2025-02-20 19:16:34 +0100

    some testing

--- e2e/runner/sui.go
+++ e2e/runner/sui.go
@@ -1,7 +1,9 @@
 package runner
 
 import (
-	"fmt"
+	"cosmossdk.io/math"
+	"github.com/pkg/errors"
+	"github.com/zeta-chain/node/e2e/utils/sui"
 
 	"github.com/block-vision/sui-go-sdk/models"
 	ethcommon "github.com/ethereum/go-ethereum/common"
@@ -19,13 +21,15 @@ func (r *E2ERunner) SUIDeposit(
 
 	signerAddr := signer.Address()
 
-	r.Logger.Print("SUI Address:")
+	r.Logger.Print("address")
 	r.Logger.Print(signerAddr)
 
 	// retrieve SUI coin object to deposit
-	// TODO: split coin with an amountto not deposit all tokens
-	coinObjectId, err := r.filterOwnedObject(signerAddr, string(zetasui.SUI))
+	coinObjectId, err := r.splitSui(signer, math.NewUint(100000))
 	require.NoError(r, err)
+
+	r.Logger.Print("coin object id")
+	r.Logger.Print(coinObjectId)
 
 	// create the tx
 	tx, err := r.Clients.Sui.MoveCall(r.Ctx, models.MoveCallRequest{
@@ -47,36 +51,76 @@ func (r *E2ERunner) SUIDeposit(
 	r.Logger.Print(resp)
 }
 
-// filterOwnedObject finds one object owned by the address and has the type of typeName
-func (r *E2ERunner) filterOwnedObject(address, typeName string) (objId string, err error) {
-	// see https://docs.sui.io/sui-api-ref#suix_getownedobjects
-	suiObjectResponseQuery := models.SuiObjectResponseQuery{
-		Filter: models.SuiObjectDataFilter{
-			"StructType": typeName,
-		},
-		Options: models.SuiObjectDataOptions{
-			ShowType:    true,
-			ShowContent: true,
-			ShowBcs:     true,
-			ShowOwner:   true,
-		},
-	}
+// splitSUI splits SUI coin and obtain a SUI coin object with the wanted balance
+func (r *E2ERunner) splitSui(signer *sui.SignerSecp256k1, balance math.Uint) (objId string, err error) {
+	return r.findSUIWithBalanceAbove(signer.Address(), balance)
 
-	resp, err := r.Clients.Sui.SuiXGetOwnedObjects(r.Ctx, models.SuiXGetOwnedObjectsRequest{
-		Address: address,
-		Query:   suiObjectResponseQuery,
-		Limit:   50,
+	// TODO: there is an issue splitting the coin -> fix this
+	//// find the coin to split
+	//originalCoin, err := r.findSUIWithBalanceAbove(signer.Address(), balance)
+	//if err != nil {
+	//	return "", err
+	//}
+	//
+	//// split the coin using the PaySui API
+	//tx, err := r.Clients.Sui.PaySui(r.Ctx, models.PaySuiRequest{
+	//	Signer:      signer.Address(),
+	//	SuiObjectId: []string{originalCoin},
+	//	Recipient:   []string{signer.Address()},
+	//	Amount:      []string{balance.String()},
+	//	GasBudget:   "5000000000",
+	//})
+	//
+	//// sign the split tx
+	//_, err = signer.SignTransactionBlock(tx.TxBytes)
+	//require.NoError(r, err)
+	//
+	//// find the split coin
+	//return r.findSUIWithBalance(signer.Address(), balance)
+}
+
+func (r *E2ERunner) findSUIWithBalance(
+	address string,
+	balance math.Uint,
+) (coinId string, err error) {
+	return r.findSUI(address, balance, func(a, b math.Uint) bool {
+		return a.Equal(b)
+	})
+}
+
+func (r *E2ERunner) findSUIWithBalanceAbove(
+	address string,
+	balanceAbove math.Uint,
+) (coinId string, err error) {
+	return r.findSUI(address, balanceAbove, func(a, b math.Uint) bool {
+		return a.GTE(b)
+	})
+}
+
+type compFunc func(a, b math.Uint) bool
+
+func (r *E2ERunner) findSUI(
+	address string,
+	balance math.Uint,
+	comp compFunc,
+) (coinId string, err error) {
+	res, err := r.Clients.Sui.SuiXGetCoins(r.Ctx, models.SuiXGetCoinsRequest{
+		Owner:    address,
+		CoinType: string(zetasui.SUI),
 	})
 	if err != nil {
 		return "", err
 	}
 
-	for _, data := range resp.Data {
-		if data.Data.Type == typeName {
-			objId = data.Data.ObjectId
-			return objId, nil
+	for _, data := range res.Data {
+		coinBalance, err := math.ParseUint(data.Balance)
+		if err != nil {
+			return "", err
+		}
+		if comp(coinBalance, balance) {
+			return data.CoinObjectId, nil
 		}
 	}
 
-	return "", fmt.Errorf("no object of type %s found", typeName)
+	return "", errors.New("no coin found")
 }
