cd7db53574206ed8dbed7987d1d11b155767784e
skosito <skostic9242@gmail.com>
2025-02-20 15:37:21 +0100

    add missing gateway msg tests

--- pkg/contracts/solana/gateway_message_test.go
+++ pkg/contracts/solana/gateway_message_test.go
@@ -3,6 +3,7 @@ package solana_test
 import (
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/require"
 
 	"github.com/gagliardetto/solana-go"
@@ -71,6 +72,73 @@ func Test_MsgWithdrawSPLHash(t *testing.T) {
 		// ACT
 		// create new withdraw message
 		hash := contracts.NewMsgWithdrawSPL(chainID, nonce, amount, 8, mintAccount, to, toAta).Hash()
+
+		// ASSERT
+		require.EqualValues(t, hash[:], wantHashBytes)
+	})
+}
+
+func Test_MsgIncrementNonceHash(t *testing.T) {
+	t.Run("should calculate expected hash", func(t *testing.T) {
+		// ARRANGE
+		// #nosec G115 always positive
+		chainID := uint64(chains.SolanaLocalnet.ChainId)
+		nonce := uint64(0)
+		amount := uint64(1336000)
+
+		wantHash := "7acd44905f89cc36b4542b103a25f2110d25ca469dee854fbf342f1f3ee8ba90"
+		wantHashBytes := testutil.HexToBytes(t, wantHash)
+
+		// ACT
+		// create new increment nonce message
+		hash := contracts.NewMsgIncrementNonce(chainID, nonce, amount).Hash()
+
+		// ASSERT
+		require.EqualValues(t, hash[:], wantHashBytes)
+	})
+}
+
+func Test_MsgExecuteHash(t *testing.T) {
+	t.Run("should calculate expected hash", func(t *testing.T) {
+		// ARRANGE
+		// #nosec G115 always positive
+		chainID := uint64(chains.SolanaLocalnet.ChainId)
+		nonce := uint64(0)
+		amount := uint64(1336000)
+		to := solana.MustPublicKeyFromBase58("37yGiHAnLvWZUNVwu9esp74YQFqxU1qHCbABkDvRddUQ")
+		sender := common.HexToAddress("0x42bd6E2ce4CDb2F58Ed0A0E427F011A0645D5E33")
+
+		wantHash := "7391cf357fd80e7cb3d2a9758932fdea4988d03c87210a2632f03b467728d199"
+		wantHashBytes := testutil.HexToBytes(t, wantHash)
+
+		// ACT
+		// create new execute message
+		hash := contracts.NewMsgExecute(chainID, nonce, amount, to, sender, []byte("hello"), []*solana.AccountMeta{}).Hash()
+
+		// ASSERT
+		require.EqualValues(t, hash[:], wantHashBytes)
+	})
+}
+
+func Test_MsgExecuteSPLHash(t *testing.T) {
+	t.Run("should calculate expected hash", func(t *testing.T) {
+		// ARRANGE
+		// #nosec G115 always positive
+		chainID := uint64(chains.SolanaLocalnet.ChainId)
+		nonce := uint64(0)
+		amount := uint64(1336000)
+		mintAccount := solana.MustPublicKeyFromBase58("AS48jKNQsDGkEdDvfwu1QpqjtqbCadrAq9nGXjFmdX3Z")
+		to := solana.MustPublicKeyFromBase58("37yGiHAnLvWZUNVwu9esp74YQFqxU1qHCbABkDvRddUQ")
+		toAta, _, err := solana.FindAssociatedTokenAddress(to, mintAccount)
+		require.NoError(t, err)
+		sender := common.HexToAddress("0x42bd6E2ce4CDb2F58Ed0A0E427F011A0645D5E33")
+
+		wantHash := "d90f9640faecd76509b4e88fa7d18f130918130b8666179f1597f185a828d3a5"
+		wantHashBytes := testutil.HexToBytes(t, wantHash)
+
+		// ACT
+		// create new execute message
+		hash := contracts.NewMsgExecuteSPL(chainID, nonce, amount, 8, mintAccount, to, toAta, sender, []byte("hello"), []*solana.AccountMeta{}).Hash()
 
 		// ASSERT
 		require.EqualValues(t, hash[:], wantHashBytes)
