7b68aa7d2b4a534e25cae992a45ef9252c7aaa97
Julian Rubino <julian.rubino0@gmail.com>
2025-03-19 17:41:44 -0300

    print ton wallet address

--- e2e/runner/balances.go
+++ e2e/runner/balances.go
@@ -209,6 +209,13 @@ func (r *E2ERunner) PrintAccountBalances(balances AccountBalances) {
 		r.Logger.Print("* SUI: %s", signer.Address())
 	}
 
+	tonWallet, err := r.Account.AsTONWallet(r.Clients.TON)
+	if err != nil {
+		r.Logger.Print("Error getting TON address: %s", err.Error())
+	} else {
+		r.Logger.Print("* TON: %s", tonWallet.GetAddress().ToRaw())
+	}
+
 	// zevm
 	r.Logger.Print("ZetaChain:")
 	r.Logger.Print("* ZETA balance:  %s", balances.ZetaZETA.String())
