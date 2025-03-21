3ca8c49a4fb7b997fc3c44576f676d519f3c4f42
Boris Quiroz <boris@zetachain.com>
2025-03-21 07:21:27 -0300

    Add TestWIP

--- e2e/e2etests/e2etests.go
+++ e2e/e2etests/e2etests.go
@@ -84,6 +84,7 @@ const (
 	TestTONDepositAndCallRefundName = "ton_deposit_refund"
 	TestTONWithdrawName             = "ton_withdraw"
 	TestTONWithdrawConcurrentName   = "ton_withdraw_concurrent"
+	TestWIP                         = "ton_wip"
 
 	/*
 	 Sui tests
