5f3d62fdde95f432b7929eada32bc1792e92b5d2
Alex Gartner <git@agartner.com>
2025-04-10 11:09:39 -0700

    tolerate deposit fee differences on testnet and mainnet

--- e2e/e2etests/test_bitcoin_deposit.go
+++ e2e/e2etests/test_bitcoin_deposit.go
@@ -8,6 +8,7 @@ import (
 	"github.com/zeta-chain/node/e2e/runner"
 	"github.com/zeta-chain/node/e2e/utils"
 	crosschaintypes "github.com/zeta-chain/node/x/crosschain/types"
+	"github.com/zeta-chain/node/zetaclient/chains/bitcoin/common"
 )
 
 func TestBitcoinDeposit(r *runner.E2ERunner, args []string) {
@@ -20,19 +21,30 @@ func TestBitcoinDeposit(r *runner.E2ERunner, args []string) {
 	startingBalance, err := r.BTCZRC20.BalanceOf(&bind.CallOpts{}, r.ZEVMAuth.From)
 	require.NoError(r, err)
 
-	txHash := r.DepositBTCWithExactAmount(depositAmount, nil)
+	txHash := r.DepositBTCWithAmount(depositAmount, nil)
 
 	// wait for the cctx to be mined
 	cctx := utils.WaitCctxMinedByInboundHash(r.Ctx, txHash.String(), r.CctxClient, r.Logger, r.CctxTimeout)
 	r.Logger.CCTX(*cctx, "deposit")
 	utils.RequireCCTXStatus(r, cctx, crosschaintypes.CctxStatus_OutboundMined)
 
+	// calculate fee
+	tx, err := r.BtcRPCClient.GetTransaction(r.Ctx, txHash)
+	require.NoError(r, err)
+	rawTx, err := r.BtcRPCClient.GetRawTransactionResult(r.Ctx, txHash, tx)
+	require.NoError(r, err)
+	fee, err := common.CalcDepositorFee(r.Ctx, r.BtcRPCClient, &rawTx, r.BitcoinParams)
+	require.NoError(r, err)
+	feeSatoshis := uint64(fee * btcutil.SatoshiPerBitcoin)
+
+	expectedAmount := depositAmountZRC20 - feeSatoshis
+
 	// assert that the inbound amount is expected
-	require.InDelta(r, depositAmountZRC20, cctx.InboundParams.Amount.Uint64(), 100)
+	require.InDelta(r, expectedAmount, cctx.InboundParams.Amount.Uint64(), 100)
 
 	// assert that the balance increases by the expected amount
 	endingBalance, err := r.BTCZRC20.BalanceOf(&bind.CallOpts{}, r.ZEVMAuth.From)
 	require.NoError(r, err)
 	balanceDiff := bigSub(endingBalance, startingBalance)
-	require.InDelta(r, depositAmountZRC20, balanceDiff.Uint64(), 100)
+	require.InDelta(r, expectedAmount, balanceDiff.Uint64(), 100)
 }
