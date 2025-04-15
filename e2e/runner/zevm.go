58c74342b1157252b403e894edb9021eb4baa97e
Dmitry S <11892559+swift1337@users.noreply.github.com>
2025-04-15 15:49:37 +0200

    Fix cctx list order

--- e2e/runner/zevm.go
+++ e2e/runner/zevm.go
@@ -415,7 +415,7 @@ func (r *E2ERunner) WaitForSpecificCCTX(
 		ctx      = r.Ctx
 		start    = time.Now()
 		reqQuery = &types.QueryAllCctxRequest{
-			Pagination: &query.PageRequest{Reverse: true},
+			Pagination: &query.PageRequest{Reverse: false},
 		}
 	)
 
