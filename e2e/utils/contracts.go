01abdcad8eec2fb12c4dd5aa8e241354e9e7800e
Boris Quiroz <boris@zetachain.com>
2025-04-29 16:22:27 -0400

    fix: remove unused code

--- e2e/utils/contracts.go
+++ e2e/utils/contracts.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -32,17 +31,7 @@ func MustHaveCalledExampleContract(
 
 	actualSender, err := contract.LastSender(&bind.CallOpts{})
 	require.NoError(t, err)
-
-	// Allow for environments where the sender might not be empty (like localnet)
-	// Only strictly check the sender if we expect a specific non-empty value
-	if len(sender) > 0 {
-		// We expect a specific sender
-		require.EqualValues(t, sender, actualSender)
-	} else if len(actualSender) > 0 {
-		// If we expect empty but got non-empty, just log it (don't fail)
-		// This handles localnet vs testnet differences
-		fmt.Printf("Got non-empty sender (%x) when empty was expected. This is normal in some environments.\n", actualSender)
-	}
+	require.EqualValues(t, sender, actualSender)
 }
 
 // MustHaveCalledExampleContractWithMsg checks if the contract has been called correctly with correct amount and msg
@@ -67,15 +56,5 @@ func MustHaveCalledExampleContractWithMsg(
 
 	actualSender, err := contract.LastSender(&bind.CallOpts{})
 	require.NoError(t, err)
-
-	// Allow for environments where the sender might not be empty (like localnet)
-	// Only strictly check the sender if we expect a specific non-empty value
-	if len(sender) > 0 {
-		// We expect a specific sender
-		require.EqualValues(t, sender, actualSender)
-	} else if len(actualSender) > 0 {
-		// If we expect empty but got non-empty, just log it (don't fail)
-		// This handles localnet vs testnet differences
-		fmt.Printf("Got non-empty sender (%x) when empty was expected. This is normal in some environments.\n", actualSender)
-	}
+	require.EqualValues(t, sender, actualSender)
 }
