12c00a445cbd3e58ddec998c8976ae9f01dd97e2
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2025-05-08 11:49:48 -0400

    generate files

--- cmd/zetae2e/local/local.go
+++ cmd/zetae2e/local/local.go
@@ -14,6 +14,7 @@ import (
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/require"
+
 	zetae2econfig "github.com/zeta-chain/node/cmd/zetae2e/config"
 	"github.com/zeta-chain/node/e2e/config"
 	"github.com/zeta-chain/node/e2e/e2etests"
