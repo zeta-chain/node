cd2d5248b426d88acdad40c430c0689adfeb053e
Charlie Chen <charliec@zetachain.com>
2025-06-11 12:15:55 -0500

    add gas limit argument to erc20 withdrawAndCall E2E test

--- e2e/e2etests/test_erc20_withdraw_and_call_no_message.go
+++ e2e/e2etests/test_erc20_withdraw_and_call_no_message.go
@@ -13,7 +13,7 @@ import (
 )
 
 func TestERC20WithdrawAndCallNoMessage(r *runner.E2ERunner, args []string) {
-	require.Len(r, args, 1)
+	require.Len(r, args, 2)
 
 	previousGasLimit := r.ZEVMAuth.GasLimit
 	r.ZEVMAuth.GasLimit = 10000000
@@ -22,6 +22,7 @@ func TestERC20WithdrawAndCallNoMessage(r *runner.E2ERunner, args []string) {
 	}()
 
 	amount := utils.ParseBigInt(r, args[0])
+	gasLimit := utils.ParseBigInt(r, args[1])
 
 	r.ApproveERC20ZRC20(r.GatewayZEVMAddr)
 	r.ApproveETHZRC20(r.GatewayZEVMAddr)
@@ -32,6 +33,7 @@ func TestERC20WithdrawAndCallNoMessage(r *runner.E2ERunner, args []string) {
 		amount,
 		[]byte{},
 		gatewayzevm.RevertOptions{OnRevertGasLimit: big.NewInt(0)},
+		gasLimit,
 	)
 
 	// wait for the cctx to be mined
