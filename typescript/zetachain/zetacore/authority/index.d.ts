d8a60bdcb9517e531763045c2ce4d106998264ba
Denis Fadeev <denis@fadeev.org>
2025-06-18 15:06:40 +0300

    ci: generate TypeScript types (#3978)

--- typescript/zetachain/zetacore/authority/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./authorization_pb";
-export * from "./chain_info_pb";
-export * from "./genesis_pb";
-export * from "./policies_pb";
-export * from "./query_pb";
-export * from "./tx_pb";
--- typescript/zetachain/zetacore/fungible/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./events_pb";
-export * from "./foreign_coins_pb";
-export * from "./genesis_pb";
-export * from "./query_pb";
-export * from "./system_contract_pb";
-export * from "./tx_pb";
