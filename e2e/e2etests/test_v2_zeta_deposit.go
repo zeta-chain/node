13d2a2885b93733868a47508782cf0b7fd4e1197
Tanmay <tanmay@zetachain.com>
2025-06-19 13:18:34 -0400

    test: add connector fund migration e2e test using contracts only  (#3976)
    * update go mod
    
    * add a new message for migrating funds
    
    * add migrate funds to e2e
    
    * rebase from develop
    
    * add v2 e2e test to check flow
    
    * add zeta gateway deposit to zetaclient
    
    * add github workflow
    
    * update comments
    
    * fix code formating
    
    * fix code formating
    
    * fix code formating
    
    * fix unit tests
    
    * fix unit tests
    
    * revert to old command to start e2e test
    
    * remove message for migration and refactor to using contract directly
    
    * add changelog
    
    * generate files after removing new message
    
    * update generated files
    
    * update generated files
    
    * resolve comments 1
    
    * remove v2 from naming
    
    * generate files

---
+++ e2e/e2etests/test_v2_zeta_deposit.go
@@ -0,0 +1,35 @@
+package e2etests
+
+import (
+	"math/big"
+
+	"github.com/stretchr/testify/require"
+	"github.com/zeta-chain/protocol-contracts/pkg/gatewayevm.sol"
+
+	"github.com/zeta-chain/node/e2e/runner"
+	"github.com/zeta-chain/node/e2e/utils"
+	crosschaintypes "github.com/zeta-chain/node/x/crosschain/types"
+)
+
+func TestZetaDeposit(r *runner.E2ERunner, args []string) {
+	require.Len(r, args, 1)
+
+	// NOTE, updating the chain params disables the V1 flow and enables the V2 flow.
+	chainID, err := r.EVMClient.ChainID(r.Ctx)
+	require.NoError(r, err)
+	updateChainParams(r, chainID.Int64())
+
+	amount := utils.ParseBigInt(r, args[0])
+
+	r.ApproveZetaOnEVM(r.GatewayEVMAddr)
+	// perform the deposit
+	tx := r.ZETADeposit(r.EVMAddress(), amount, gatewayevm.RevertOptions{OnRevertGasLimit: big.NewInt(0)})
+
+	// wait for the cctx to be mined
+	cctx := utils.WaitCctxMinedByInboundHash(r.Ctx, tx.Hash().Hex(), r.CctxClient, r.Logger, r.CctxTimeout)
+	r.Logger.CCTX(*cctx, "deposit")
+	require.Equal(r, crosschaintypes.CctxStatus_OutboundMined, cctx.CctxStatus.Status)
+
+	// CoinType_Zeta is not supported in V2 by the protocol yet.Add assertions when adding support for Zeta in V2
+	// https://github.com/zeta-chain/node/issues/3212
+}
--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -22,7 +22,6 @@ const (
 
 	// CmdMigrateTssFunds is used for CCTX of type cmd to give the instruction to the TSS to transfer its funds on a new address
 	CmdMigrateTssFunds = "cmd_migrate_tss_funds"
-
 	// BTCWithdrawalDustAmount is the minimum satoshis that can be withdrawn from zEVM to avoid outbound dust output
 	// The Bitcoin protocol sets a minimum output value to 546 satoshis (dust limit) but we set it to 1000 satoshis
 	BTCWithdrawalDustAmount = 1000
