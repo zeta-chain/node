2d825a67d581a6cc9f21a529da89ef3ef90975cb
Dmitry S <11892559+swift1337@users.noreply.github.com>
2025-07-08 16:56:04 +0200

    minor style fix

--- app/ante/sig_verification.go
+++ app/ante/sig_verification.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	secp256k1VerifyCost uint64 = 21000
+	secp256k1VerifyCost uint64 = 21_000
 )
 
 var _ authante.SignatureVerificationGasConsumer = DefaultSigVerificationGasConsumer
@@ -21,31 +21,34 @@ var _ authante.SignatureVerificationGasConsumer = DefaultSigVerificationGasConsu
 // for signature verification based upon the public key type. The cost is fetched from the given params and is matched
 // by the concrete type.
 func DefaultSigVerificationGasConsumer(
-	meter storetypes.GasMeter, sig signing.SignatureV2, params authtypes.Params,
+	meter storetypes.GasMeter,
+	sig signing.SignatureV2,
+	params authtypes.Params,
 ) error {
 	pubkey := sig.PubKey
 	switch pubkey := pubkey.(type) {
 	case *ethsecp256k1.PubKey:
 		meter.ConsumeGas(secp256k1VerifyCost, "ante verify: eth_secp256k1")
 		return nil
-
 	case multisig.PubKey:
 		// Multisig keys
-		multisignature, ok := sig.Data.(*signing.MultiSignatureData)
+		multiSig, ok := sig.Data.(*signing.MultiSignatureData)
 		if !ok {
 			return fmt.Errorf("expected %T, got, %T", &signing.MultiSignatureData{}, sig.Data)
 		}
-		return ConsumeMultisignatureVerificationGas(meter, multisignature, pubkey, params, sig.Sequence)
-
+		return ConsumeMultiSignatureVerificationGas(meter, multiSig, pubkey, params, sig.Sequence)
 	default:
 		return authante.DefaultSigVerificationGasConsumer(meter, sig, params)
 	}
 }
 
-// ConsumeMultisignatureVerificationGas consumes gas from a GasMeter for verifying a multisig pubkey signature
-func ConsumeMultisignatureVerificationGas(
-	meter storetypes.GasMeter, sig *signing.MultiSignatureData, pubkey multisig.PubKey,
-	params authtypes.Params, accSeq uint64,
+// ConsumeMultiSignatureVerificationGas consumes gas from a GasMeter for verifying a multisig pubkey signature
+func ConsumeMultiSignatureVerificationGas(
+	meter storetypes.GasMeter,
+	sig *signing.MultiSignatureData,
+	pubkey multisig.PubKey,
+	params authtypes.Params,
+	accSeq uint64,
 ) error {
 	size := sig.BitArray.Count()
 	sigIndex := 0
@@ -54,15 +57,18 @@ func ConsumeMultisignatureVerificationGas(
 		if !sig.BitArray.GetIndex(i) {
 			continue
 		}
+
 		sigV2 := signing.SignatureV2{
 			PubKey:   pubkey.GetPubKeys()[i],
 			Data:     sig.Signatures[sigIndex],
 			Sequence: accSeq,
 		}
+
 		err := DefaultSigVerificationGasConsumer(meter, sigV2, params)
 		if err != nil {
 			return err
 		}
+
 		sigIndex++
 	}
 
