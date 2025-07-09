b18be3ddeaae7ea3ba3de3ffc7ecae3d0f6df35b
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2025-07-09 11:46:43 -0400

    update to checkAccounting to the end of E2E run instead of every test

--- cmd/zetae2e/local/local.go
+++ cmd/zetae2e/local/local.go
@@ -569,6 +569,8 @@ func localE2ETest(cmd *cobra.Command, _ []string) {
 		os.Exit(1)
 	}
 
+	deployerRunner.CheckZRC20BalanceAndSupply()
+
 	// Default ballot maturity is set to 30 blocks.
 	// We can wait for 31 blocks to ensure that all ballots created during the test are matured, as emission rewards may be slashed for some observers based on their vote.
 	// This seems to be a problem only in performance tests where we are creating a lot of ballots in a short time. We do not need to slow down regular tests for this check as we expect all observers to vote correctly.
