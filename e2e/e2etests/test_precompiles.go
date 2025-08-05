c12427c7ef39cdf1ea0ac32cbf5021b193441dbc
lumtis <lucas.bertrand.22@gmail.com>
2025-08-05 15:36:50 +0200

    add precompile test

---
+++ e2e/e2etests/test_precompiles.go
@@ -0,0 +1,76 @@
+package e2etests
+
+import (
+	"math/big"
+	"strings"
+
+	"cosmossdk.io/math"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+
+	"github.com/zeta-chain/node/e2e/contracts/istaking"
+	"github.com/zeta-chain/node/e2e/runner"
+)
+
+// source: https://github.com/cosmos/evm/blob/main/precompiles/staking/StakingI.sol
+const stakingAddr = "0x0000000000000000000000000000000000000800"
+
+func TestPrecompiles(r *runner.E2ERunner, _ []string) {
+	precompile, err := istaking.NewIStaking(ethcommon.HexToAddress(stakingAddr), r.ZEVMClient)
+	require.NoError(r, err)
+
+	r.Logger.Print("Trying to perform staking precompile test...")
+
+	validator := getValidatorOpAddress(r)
+
+	// Get the delegation to the first validator
+	delegationBefore := getDelegation(r)
+
+	// Deledate 1 ZETA
+	tx, err := precompile.Delegate(r.ZEVMAuth, r.ZEVMAuth.From, validator, big.NewInt(1e18))
+	require.NoError(r, err)
+	r.WaitForTxReceiptOnZEVM(tx)
+
+	// Get the delegation to the first validator after delegation
+	// The share denomination of the delegation should increase
+	delegationAfter := getDelegation(r)
+	require.True(
+		r,
+		delegationAfter.GT(delegationBefore),
+		"Delegation shares should increase after delegation, before: %s, after: %s",
+		delegationBefore.String(),
+		delegationAfter.String(),
+	)
+
+	r.Logger.Print("Staking precompile test passed successfully!")
+}
+
+// getDelegation returns the delegation to the first validator from the active E2E account
+func getDelegation(r *runner.E2ERunner) math.LegacyDec {
+	// Get the first validator's operator address
+	opAddress := getValidatorOpAddress(r)
+
+	// Get the delegation address for the first validator
+	res, err := r.StakingClient.Delegation(r.Ctx, &stakingtypes.QueryDelegationRequest{
+		DelegatorAddr: r.Account.RawBech32Address.String(),
+		ValidatorAddr: opAddress,
+	})
+	if err != nil {
+		// no delegation found, return zero shares
+		if strings.Contains(err.Error(), "not found") {
+			return math.LegacyZeroDec()
+		}
+		require.Fail(r, "Failed to get delegation", "error: %v", err)
+	}
+
+	return res.DelegationResponse.Delegation.Shares
+}
+
+// getValidatorOpAddress returns the operator address of the first validator to perform staking tests
+func getValidatorOpAddress(r *runner.E2ERunner) string {
+	res, err := r.StakingClient.Validators(r.Ctx, &stakingtypes.QueryValidatorsRequest{})
+	require.NoError(r, err)
+	require.NotEmpty(r, res.Validators)
+	return res.Validators[0].OperatorAddress
+}
