fe89771763071750a2e25bbf5f5629d85a34e7b5
Charlie Chen <34498985+ws4charlie@users.noreply.github.com>
2025-08-19 22:45:43 -0500

    fix(v34-only): use SAFE confirmation mode for all inbound votes (#4117)
    * use SAFE confirmation mode for all inbound votes
    
    * move confirmation mode update logic right before signing the inbound vote message

--- e2e/e2etests/test_eth_deposit_fast_confirmation.go
+++ e2e/e2etests/test_eth_deposit_fast_confirmation.go
@@ -76,7 +76,10 @@ func TestETHDepositFastConfirmation(r *runner.E2ERunner, args []string) {
 	timeStart := time.Now()
 	cctx := utils.WaitCctxMinedByInboundHash(r.Ctx, tx.Hash().Hex(), r.CctxClient, r.Logger, r.CctxTimeout)
 	require.Equal(r, crosschaintypes.CctxStatus_OutboundMined, cctx.CctxStatus.Status)
-	require.Equal(r, crosschaintypes.ConfirmationMode_FAST, cctx.InboundParams.ConfirmationMode)
+
+	// zetaclient is patched
+	// SAFE mode is always used for all inbound votes (both fast and slow votes)
+	require.Equal(r, crosschaintypes.ConfirmationMode_SAFE, cctx.InboundParams.ConfirmationMode)
 	fastConfirmTime := time.Since(timeStart)
 
 	r.Logger.Info("FAST confirmed deposit succeeded in %f seconds", fastConfirmTime.Seconds())
