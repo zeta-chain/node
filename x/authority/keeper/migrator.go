b9f17b2c79e892ff3714dba4d732375e585a4375
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2025-09-03 11:39:14 -0400

    update setup handler to remove setting erc20 module

--- x/authority/keeper/migrator.go
+++ x/authority/keeper/migrator.go
@@ -2,8 +2,8 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	v4 "github.com/zeta-chain/node/x/authority/migrations/v4"
 
+	v4 "github.com/zeta-chain/node/x/authority/migrations/v4"
 	v5 "github.com/zeta-chain/node/x/authority/migrations/v5"
 )
 
