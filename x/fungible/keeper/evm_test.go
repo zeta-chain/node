9058c8dbe83804c8e0cb19ee636c22fdfc2d47f2
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2025-09-02 21:15:03 -0400

    update unit tests

--- x/fungible/keeper/evm_test.go
+++ x/fungible/keeper/evm_test.go
@@ -135,7 +135,7 @@ func deploySystemContracts(
 	require.NotEmpty(t, systemContract)
 	assertContractDeployment(t, evmk, ctx, systemContract)
 
-	require.NoError(t, k.SetGatewayGasLimit(ctx, sdkmath.NewIntFromBigInt(types.GatewayGasLimit)))
+	k.SetGatewayGasLimit(ctx, sdkmath.NewIntFromBigInt(types.GatewayGasLimit))
 
 	// deploy the gateway contract
 	contract := deployGatewayContract(t, ctx, k, evmk, wzeta, sample.EthAddress())
