d8d8971ca20234c08886e9d871901a50c988bc8b
Charlie Chen <charliec@zetachain.com>
2025-09-04 00:07:37 -0500

    do not deploy example contract in e2e tests, use pre-deployed example dapp V2 instead

--- e2e/e2etests/test_solana_to_zevm_call.go
+++ e2e/e2etests/test_solana_to_zevm_call.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	testcontract "github.com/zeta-chain/node/e2e/contracts/example"
 	"github.com/zeta-chain/node/e2e/runner"
 	"github.com/zeta-chain/node/e2e/utils"
 	crosschaintypes "github.com/zeta-chain/node/x/crosschain/types"
@@ -15,27 +14,24 @@ import (
 func TestSolanaToZEVMCall(r *runner.E2ERunner, args []string) {
 	require.Len(r, args, 0)
 
-	// deploy an example contract in ZEVM
-	contractAddr, _, contract, err := testcontract.DeployExample(r.ZEVMAuth, r.ZEVMClient)
-	require.NoError(r, err)
-	r.Logger.Info("Example contract deployed at: %s", contractAddr.String())
+	// ARRANGE
+	// Given payload and ZEVM contract address
+	contractAddr := r.TestDAppV2ZEVMAddr
+	payload := randomPayload(r)
+	r.AssertTestDAppZEVMCalled(false, payload, nil)
 
+	// ACT
 	// execute call transaction
-	data := []byte("hello")
-	sig := r.SOLCall(nil, contractAddr, data, nil)
+	// data := []byte("hello")
+	sig := r.SOLCall(nil, contractAddr, []byte(payload), nil)
 
+	// ASSERT
 	// wait for the cctx to be mined
 	cctx := utils.WaitCctxMinedByInboundHash(r.Ctx, sig.String(), r.CctxClient, r.Logger, r.CctxTimeout)
 	r.Logger.CCTX(*cctx, "solana_call")
 	utils.RequireCCTXStatus(r, cctx, crosschaintypes.CctxStatus_OutboundMined)
 	require.Equal(r, cctx.GetCurrentOutboundParam().Receiver, contractAddr.Hex())
 
-	// check if example contract has been called, bar value should be set to amount
-	utils.WaitAndVerifyExampleContractCallWithMsg(
-		r,
-		contract,
-		big.NewInt(0),
-		data,
-		[]byte(r.GetSolanaPrivKey().PublicKey().String()),
-	)
+	// check the payload was received on the contract
+	r.AssertTestDAppZEVMCalled(true, payload, big.NewInt(0))
 }
--- e2e/e2etests/test_ton_deposit_and_call.go
+++ e2e/e2etests/test_ton_deposit_and_call.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	testcontract "github.com/zeta-chain/node/e2e/contracts/example"
 	"github.com/zeta-chain/node/e2e/runner"
 	"github.com/zeta-chain/node/e2e/utils"
 	toncontracts "github.com/zeta-chain/node/pkg/contracts/ton"
@@ -30,16 +29,13 @@ func TestTONDepositAndCall(r *runner.E2ERunner, args []string) {
 	_, sender, err := r.Account.AsTONWallet(r.Clients.TON)
 	require.NoError(r, err)
 
-	// Given sample zEVM contract deployed by userTON account
-	contractAddr, _, contract, err := testcontract.DeployExample(r.ZEVMAuth, r.ZEVMClient)
-	require.NoError(r, err, "unable to deploy example contract")
-	r.Logger.Info("Example zevm contract deployed at: %s", contractAddr.String())
-
-	// Given call data
-	callData := []byte("hello from TON!")
+	// Given payload and a ZEVM contract
+	contractAddr := r.TestDAppV2ZEVMAddr
+	payload := randomPayload(r)
+	r.AssertTestDAppZEVMCalled(false, payload, big.NewInt(0))
 
 	// ACT
-	_, err = r.TONDepositAndCall(gw, sender, amount, contractAddr, callData)
+	_, err = r.TONDepositAndCall(gw, sender, amount, contractAddr, []byte(payload))
 
 	// ASSERT
 	require.NoError(r, err)
@@ -50,6 +46,6 @@ func TestTONDepositAndCall(r *runner.E2ERunner, args []string) {
 	change := utils.NewExactChange(expectedDeposit.BigInt())
 	utils.WaitAndVerifyZRC20BalanceChange(r, r.TONZRC20, contractAddr, big.NewInt(0), change, r.Logger)
 
-	// check if example contract has been called, bar value should be set to amount
-	utils.WaitAndVerifyExampleContractCall(r, contract, expectedDeposit.BigInt(), []byte(sender.GetAddress().ToRaw()))
+	// check the payload was received on the contract
+	r.AssertTestDAppZEVMCalled(true, payload, expectedDeposit.BigInt())
 }
