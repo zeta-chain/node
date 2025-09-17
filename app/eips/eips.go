f09cb7b20f0e9d8cc125fff32336c8e894a36e86
zhoujiaweii <zjwustc@outlook.com>
2025-09-17 22:52:45 +0800

    chore: fix some comments (#4208)
    Signed-off-by: zhoujiaweii <zjwustc@outlook.com>

--- app/eips/eips.go
+++ app/eips/eips.go
@@ -11,7 +11,7 @@ var (
 
 // NOTE: copied from cosmos evm to avoid importing evmd
 
-// enable0000 contains the logic to modify the CREATE and CREATE2 opcodes
+// Enable0000 contains the logic to modify the CREATE and CREATE2 opcodes
 // constant gas value.
 func Enable0000(jt *vm.JumpTable) {
 	currentValCreate := jt[vm.CREATE].GetConstantGas()
@@ -21,14 +21,14 @@ func Enable0000(jt *vm.JumpTable) {
 	jt[vm.CREATE2].SetConstantGas(currentValCreate2 * Multiplier)
 }
 
-// enable0001 contains the logic to modify the CALL opcode
+// Enable0001 contains the logic to modify the CALL opcode
 // constant gas value.
 func Enable0001(jt *vm.JumpTable) {
 	currentVal := jt[vm.CALL].GetConstantGas()
 	jt[vm.CALL].SetConstantGas(currentVal * Multiplier)
 }
 
-// enable0002 contains the logic to modify the SSTORE opcode
+// Enable0002 contains the logic to modify the SSTORE opcode
 // constant gas value.
 func Enable0002(jt *vm.JumpTable) {
 	jt[vm.SSTORE].SetConstantGas(SstoreConstantGas)
