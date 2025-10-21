677ee66ddcf7723e69a148507a571c742325beb7
Charlie Chen <34498985+ws4charlie@users.noreply.github.com>
2025-10-21 14:25:25 -0500

    feat: observe inbound from previous gateway package after upgrade (#4182)
    * backport PR 4087 to v35
    
    * fix CI upgrade test
    
    * fix unit test
    
    * avoid wrapping nil error
    
    * backport PR 4138
    
    * fix compile error
    
    * add support for previous package deprecation; fix upgrade test and unit test
    
    * add a note to clarify the PackageID argument used for event query
    
    * add documents to explain Sui gateway address format in the chain params
    
    * update the TODO comment for broken Sui e2e test in the upgrade

--- zetaclient/chains/base/observer_test.go
+++ zetaclient/chains/base/observer_test.go
@@ -511,6 +511,75 @@ func TestReadWriteDBLastTxScanned(t *testing.T) {
 	})
 }
 
+func Test_GetSetAuxString(t *testing.T) {
+	chain := chains.SuiMainnet
+
+	t.Run("should be able to update auxiliary string value", func(t *testing.T) {
+		ob := newTestSuite(t, chain)
+
+		// should return empty value if not set
+		key := "test key"
+		require.Empty(t, ob.GetAuxString(key))
+
+		// update auxiliary string value
+
+		value := "test value"
+		ob.Observer.WithAuxString(key, value)
+		require.Equal(t, value, ob.GetAuxString(key))
+	})
+}
+
+func Test_LoadAuxString(t *testing.T) {
+	chain := chains.SuiMainnet
+	key := "test key"
+	envvar := EnvVarLatestAuxStringByChain(chain, key)
+
+	t.Run("should be able to load/update auxiliary string value", func(t *testing.T) {
+		// create observer and open db
+		ob := newTestSuite(t, chain)
+
+		// create db and write auxiliary string value
+		err := ob.WriteAuxStringToDB(key, "test value")
+		require.NoError(t, err)
+
+		// read auxiliary string value
+		ob.LoadAuxString(key)
+		require.EqualValues(t, "test value", ob.GetAuxString(key))
+
+		// update auxiliary string value
+		err = ob.WriteAuxStringToDB(key, "test value 2")
+		require.NoError(t, err)
+
+		// read again
+		ob.LoadAuxString(key)
+		require.EqualValues(t, "test value 2", ob.GetAuxString(key))
+	})
+
+	t.Run("should return empty value if not found in db", func(t *testing.T) {
+		// create observer and open db
+		ob := newTestSuite(t, chain)
+
+		// read auxiliary string value
+		ob.LoadAuxString(key)
+		require.Empty(t, ob.GetAuxString(key))
+	})
+
+	t.Run("should overwrite string value if env var is set", func(t *testing.T) {
+		// create observer and open db
+		ob := newTestSuite(t, chain)
+
+		// create db and write auxiliary string value
+		ob.WriteAuxStringToDB(key, "test value 1")
+
+		// set env var
+		os.Setenv(envvar, "test value 2")
+
+		// read auxiliary string value
+		ob.LoadAuxString(key)
+		require.EqualValues(t, "test value 2", ob.GetAuxString(key))
+	})
+}
+
 func createDatabase(t *testing.T) *db.DB {
 	sqlDatabase, err := db.NewFromSqliteInMemory(true)
 	require.NoError(t, err)
