b509825cccedc1b5d31dca0409a08a46dadf8380
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2025-12-08 19:25:19 +0530

    add check for len of observer set in e2e test

--- e2e/runner/observer_set.go
+++ e2e/runner/observer_set.go
@@ -1,12 +1,17 @@
 package runner
 
-import observertypes "github.com/zeta-chain/node/x/observer/types"
+import (
+	"github.com/stretchr/testify/require"
+
+	observertypes "github.com/zeta-chain/node/x/observer/types"
+)
 
 func (r *E2ERunner) RemoveObserver() error {
 	observerSet, err := r.ObserverClient.ObserverSet(r.Ctx, &observertypes.QueryObserverSet{})
 	if err != nil {
 		return err
 	}
+	require.GreaterOrEqual(r, len(observerSet.Observers), 1)
 	r.Logger.Print("🏃 Removing observer from the set : %s", observerSet.Observers[len(observerSet.Observers)-1])
 	err = r.ZetaTxServer.RemoveObserver(observerSet.Observers[len(observerSet.Observers)-1])
 	return err
