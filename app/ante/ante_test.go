278761cae15a5be17060465b61554e7b9ac28f04
Tanmay <tanmay@zetachain.com>
2026-02-25 16:45:26 -0500

    fix: check grantee for IsSystemTx (backport #4548) (#4554)

--- app/ante/ante_test.go
+++ app/ante/ante_test.go
@@ -34,124 +34,92 @@ func (mah *MockAnteHandler) AnteHandle(ctx sdk.Context, _ sdk.Tx, _ bool) (sdk.C
 }
 
 func TestIsSystemTx(t *testing.T) {
-	// system tx types:
-	//      *cctxtypes.MsgVoteGasPrice,
-	//		*cctxtypes.MsgVoteInbound,
-	//		*cctxtypes.MsgVoteOutbound,
-	//		*cctxtypes.MsgAddOutboundTracker,
-	//		*cctxtypes.MsgAddInboundTracker,
-	//		*observertypes.MsgVoteBlockHeader,
-	//		*observertypes.MsgVoteTSS,
-	//		*observertypes.MsgVoteBlame:
+	encodingConfig := app.MakeEncodingConfig(serverconfig.DefaultEVMChainID)
+
 	buildTxFromMsg := func(msg sdk.Msg) sdk.Tx {
-		txBuilder := app.MakeEncodingConfig(serverconfig.DefaultEVMChainID).TxConfig.NewTxBuilder()
+		txBuilder := encodingConfig.TxConfig.NewTxBuilder()
 		txBuilder.SetMsgs(msg)
 		return txBuilder.GetTx()
 	}
-	buildAuthzTxFromMsg := func(msg sdk.Msg) sdk.Tx {
-		txBuilder := app.MakeEncodingConfig(serverconfig.DefaultEVMChainID).TxConfig.NewTxBuilder()
-		msgExec := authz.NewMsgExec(sample.Bech32AccAddress(), []sdk.Msg{msg})
+	buildAuthzTxFromMsgWithGrantee := func(grantee sdk.AccAddress, msg sdk.Msg) sdk.Tx {
+		txBuilder := encodingConfig.TxConfig.NewTxBuilder()
+		msgExec := authz.NewMsgExec(grantee, []sdk.Msg{msg})
 		txBuilder.SetMsgs(&msgExec)
 		return txBuilder.GetTx()
 	}
-	isAuthorized := func(_ string) error {
+	buildMultiMsgTx := func(msgs ...sdk.Msg) sdk.Tx {
+		txBuilder := encodingConfig.TxConfig.NewTxBuilder()
+		txBuilder.SetMsgs(msgs...)
+		return txBuilder.GetTx()
+	}
+
+	// ARRANGE: fixed addresses for grantee verification tests
+	observerAddr := sample.AccAddress()
+	registeredGrantee := sample.Bech32AccAddress()
+	attackerGrantee := sample.Bech32AccAddress()
+
+	// isAuthorized mocks CheckSystemTxAuthorization: observer always authorized,
+	// and if msgExecSigner is non-empty it must match the registeredGrantee.
+	isAuthorized := func(observer string, msgExecSigner string) error {
+		if msgExecSigner != "" && msgExecSigner != registeredGrantee.String() {
+			return errors.New("unauthorized grantee")
+		}
 		return nil
 	}
-	isAuthorizedFalse := func(_ string) error {
+	isAuthorizedNoExec := func(_ string, _ string) error {
+		return nil
+	}
+	isUnauthorized := func(_ string, _ string) error {
 		return errors.New("not authorized")
 	}
 
 	tests := []struct {
-		name         string
-		tx           sdk.Tx
-		isAuthorized func(string) error
-		wantIs       bool
+		name   string
+		tx     sdk.Tx
+		isAuth func(string, string) error
+		wantIs bool
 	}{
+		// --- direct system messages (no MsgExec) ---
 		{
-			"MsgVoteTSS",
+			"MsgVoteTSS unauthorized observer",
 			buildTxFromMsg(&observertypes.MsgVoteTSS{
 				Creator:   sample.AccAddress(),
 				TssPubkey: "pubkey1234",
 			}),
-			isAuthorizedFalse,
+			isUnauthorized,
 			false,
 		},
 		{
-			"MsgVoteTSS",
+			"MsgVoteTSS authorized observer",
 			buildTxFromMsg(&observertypes.MsgVoteTSS{
 				Creator:   sample.AccAddress(),
 				TssPubkey: "pubkey1234",
 			}),
-			isAuthorized,
+			isAuthorizedNoExec,
 			true,
 		},
-		{
-			"MsgExec{MsgVoteTSS}",
-			buildAuthzTxFromMsg(&observertypes.MsgVoteTSS{
-				Creator:   sample.AccAddress(),
-				TssPubkey: "pubkey1234",
-			}),
-			isAuthorized,
-
-			true,
-		},
-		{
-			"MsgSend",
-			buildTxFromMsg(&banktypes.MsgSend{}),
-			isAuthorized,
-
-			false,
-		},
-		{
-			"MsgExec{MsgSend}",
-			buildAuthzTxFromMsg(&banktypes.MsgSend{}),
-			isAuthorized,
-
-			false,
-		},
-		{
-			"MsgCreateValidator",
-			buildTxFromMsg(&stakingtypes.MsgCreateValidator{}),
-			isAuthorized,
-
-			false,
-		},
-
 		{
 			"MsgVoteInbound",
 			buildTxFromMsg(&crosschaintypes.MsgVoteInbound{
 				Creator: sample.AccAddress(),
 			}),
-			isAuthorized,
-
+			isAuthorizedNoExec,
 			true,
 		},
-		{
-			"MsgExec{MsgVoteInbound}",
-			buildAuthzTxFromMsg(&crosschaintypes.MsgVoteInbound{
-				Creator: sample.AccAddress(),
-			}),
-			isAuthorized,
-
-			true,
-		},
-
 		{
 			"MsgVoteOutbound",
 			buildTxFromMsg(&crosschaintypes.MsgVoteOutbound{
 				Creator: sample.AccAddress(),
 			}),
-			isAuthorized,
-
+			isAuthorizedNoExec,
 			true,
 		},
 		{
-			"MsgExec{MsgVoteOutbound}",
-			buildAuthzTxFromMsg(&crosschaintypes.MsgVoteOutbound{
+			"MsgVoteGasPrice",
+			buildTxFromMsg(&crosschaintypes.MsgVoteGasPrice{
 				Creator: sample.AccAddress(),
 			}),
-			isAuthorized,
-
+			isAuthorizedNoExec,
 			true,
 		},
 		{
@@ -159,17 +127,7 @@ func TestIsSystemTx(t *testing.T) {
 			buildTxFromMsg(&crosschaintypes.MsgAddOutboundTracker{
 				Creator: sample.AccAddress(),
 			}),
-			isAuthorized,
-
-			true,
-		},
-		{
-			"MsgExec{MsgAddOutboundTracker}",
-			buildAuthzTxFromMsg(&crosschaintypes.MsgAddOutboundTracker{
-				Creator: sample.AccAddress(),
-			}),
-			isAuthorized,
-
+			isAuthorizedNoExec,
 			true,
 		},
 		{
@@ -177,35 +135,7 @@ func TestIsSystemTx(t *testing.T) {
 			buildTxFromMsg(&crosschaintypes.MsgAddInboundTracker{
 				Creator: sample.AccAddress(),
 			}),
-			isAuthorized,
-
-			true,
-		},
-		{
-			"MsgExec{MsgAddInboundTracker}",
-			buildAuthzTxFromMsg(&crosschaintypes.MsgAddInboundTracker{
-				Creator: sample.AccAddress(),
-			}),
-			isAuthorized,
-
-			true,
-		},
-		{
-			"MsgVoteTSS",
-			buildTxFromMsg(&observertypes.MsgVoteTSS{
-				Creator: sample.AccAddress(),
-			}),
-			isAuthorized,
-
-			true,
-		},
-		{
-			"MsgExec{MsgVoteTSS}",
-			buildAuthzTxFromMsg(&observertypes.MsgVoteTSS{
-				Creator: sample.AccAddress(),
-			}),
-			isAuthorized,
-
+			isAuthorizedNoExec,
 			true,
 		},
 		{
@@ -213,17 +143,7 @@ func TestIsSystemTx(t *testing.T) {
 			buildTxFromMsg(&observertypes.MsgVoteBlockHeader{
 				Creator: sample.AccAddress(),
 			}),
-			isAuthorized,
-
-			true,
-		},
-		{
-			"MsgExec{MsgVoteBlockHeader}",
-			buildAuthzTxFromMsg(&observertypes.MsgVoteBlockHeader{
-				Creator: sample.AccAddress(),
-			}),
-			isAuthorized,
-
+			isAuthorizedNoExec,
 			true,
 		},
 		{
@@ -231,23 +151,174 @@ func TestIsSystemTx(t *testing.T) {
 			buildTxFromMsg(&observertypes.MsgVoteBlame{
 				Creator: sample.AccAddress(),
 			}),
-			isAuthorized,
+			isAuthorizedNoExec,
+			true,
+		},
 
+		// --- non-system messages ---
+		{
+			"MsgSend is not system tx",
+			buildTxFromMsg(&banktypes.MsgSend{}),
+			isAuthorizedNoExec,
+			false,
+		},
+		{
+			"MsgCreateValidator is not system tx",
+			buildTxFromMsg(&stakingtypes.MsgCreateValidator{}),
+			isAuthorizedNoExec,
+			false,
+		},
+		{
+			"MsgExec{MsgSend} is not system tx",
+			buildAuthzTxFromMsgWithGrantee(registeredGrantee, &banktypes.MsgSend{}),
+			isAuthorized,
+			false,
+		},
+
+		// --- MsgExec with registered hotkey (legitimate) ---
+		{
+			"MsgExec{MsgVoteTSS} with registered hotkey",
+			buildAuthzTxFromMsgWithGrantee(registeredGrantee, &observertypes.MsgVoteTSS{
+				Creator:   observerAddr,
+				TssPubkey: "pubkey1234",
+			}),
+			isAuthorized,
 			true,
 		},
 		{
-			"MsgExec{MsgVoteBlame}",
-			buildAuthzTxFromMsg(&observertypes.MsgVoteBlame{
-				Creator: sample.AccAddress(),
+			"MsgExec{MsgVoteInbound} with registered hotkey",
+			buildAuthzTxFromMsgWithGrantee(registeredGrantee, &crosschaintypes.MsgVoteInbound{
+				Creator: observerAddr,
 			}),
 			isAuthorized,
-
 			true,
+		},
+		{
+			"MsgExec{MsgVoteOutbound} with registered hotkey",
+			buildAuthzTxFromMsgWithGrantee(registeredGrantee, &crosschaintypes.MsgVoteOutbound{
+				Creator: observerAddr,
+			}),
+			isAuthorized,
+			true,
+		},
+		{
+			"MsgExec{MsgAddOutboundTracker} with registered hotkey",
+			buildAuthzTxFromMsgWithGrantee(registeredGrantee, &crosschaintypes.MsgAddOutboundTracker{
+				Creator: observerAddr,
+			}),
+			isAuthorized,
+			true,
+		},
+		{
+			"MsgExec{MsgAddInboundTracker} with registered hotkey",
+			buildAuthzTxFromMsgWithGrantee(registeredGrantee, &crosschaintypes.MsgAddInboundTracker{
+				Creator: observerAddr,
+			}),
+			isAuthorized,
+			true,
+		},
+		{
+			"MsgExec{MsgVoteBlockHeader} with registered hotkey",
+			buildAuthzTxFromMsgWithGrantee(registeredGrantee, &observertypes.MsgVoteBlockHeader{
+				Creator: observerAddr,
+			}),
+			isAuthorized,
+			true,
+		},
+		{
+			"MsgExec{MsgVoteBlame} with registered hotkey",
+			buildAuthzTxFromMsgWithGrantee(registeredGrantee, &observertypes.MsgVoteBlame{
+				Creator: observerAddr,
+			}),
+			isAuthorized,
+			true,
+		},
+
+		// --- MsgExec attack: grantee is not registered hotkey ---
+		{
+			"MsgExec{MsgVoteTSS} with attacker grantee",
+			buildAuthzTxFromMsgWithGrantee(attackerGrantee, &observertypes.MsgVoteTSS{
+				Creator:   observerAddr,
+				TssPubkey: "pubkey1234",
+			}),
+			isAuthorized,
+			false,
+		},
+		{
+			"MsgExec{MsgVoteInbound} with attacker grantee",
+			buildAuthzTxFromMsgWithGrantee(attackerGrantee, &crosschaintypes.MsgVoteInbound{
+				Creator: observerAddr,
+			}),
+			isAuthorized,
+			false,
+		},
+		{
+			"MsgExec{MsgVoteGasPrice} with attacker grantee",
+			buildAuthzTxFromMsgWithGrantee(attackerGrantee, &crosschaintypes.MsgVoteGasPrice{
+				Creator: observerAddr,
+			}),
+			isAuthorized,
+			false,
+		},
+
+		// --- MsgExec with unauthorized observer ---
+		{
+			"MsgExec{MsgVoteTSS} with unauthorized observer",
+			buildAuthzTxFromMsgWithGrantee(registeredGrantee, &observertypes.MsgVoteTSS{
+				Creator:   observerAddr,
+				TssPubkey: "pubkey1234",
+			}),
+			isUnauthorized,
+			false,
+		},
+
+		// --- multi-msg tx ---
+		{
+			"multi-msg tx is not system tx",
+			buildMultiMsgTx(
+				&crosschaintypes.MsgVoteInbound{Creator: sample.AccAddress()},
+				&crosschaintypes.MsgVoteOutbound{Creator: sample.AccAddress()},
+			),
+			isAuthorizedNoExec,
+			false,
+		},
+
+		// --- nested MsgExec ---
+		{
+			"MsgExec{MsgExec{MsgVoteTSS}} nested exec rejected",
+			func() sdk.Tx {
+				txBuilder := encodingConfig.TxConfig.NewTxBuilder()
+				innerExec := authz.NewMsgExec(registeredGrantee, []sdk.Msg{&observertypes.MsgVoteTSS{
+					Creator:   observerAddr,
+					TssPubkey: "pubkey1234",
+				}})
+				outerExec := authz.NewMsgExec(attackerGrantee, []sdk.Msg{&innerExec})
+				txBuilder.SetMsgs(&outerExec)
+				return txBuilder.GetTx()
+			}(),
+			isAuthorized,
+			false,
+		},
+
+		// --- MsgExec with multiple inner messages ---
+		{
+			"MsgExec with multiple inner messages rejected",
+			func() sdk.Tx {
+				txBuilder := encodingConfig.TxConfig.NewTxBuilder()
+				msgExec := authz.NewMsgExec(registeredGrantee, []sdk.Msg{
+					&observertypes.MsgVoteTSS{Creator: observerAddr, TssPubkey: "pubkey1234"},
+					&observertypes.MsgVoteBlame{Creator: observerAddr},
+				})
+				txBuilder.SetMsgs(&msgExec)
+				return txBuilder.GetTx()
+			}(),
+			isAuthorized,
+			false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			is := ante.IsSystemTx(tt.tx, tt.isAuthorized)
+			is := ante.IsSystemTx(tt.tx, tt.isAuth)
 			require.Equal(t, tt.wantIs, is)
 		})
 	}
